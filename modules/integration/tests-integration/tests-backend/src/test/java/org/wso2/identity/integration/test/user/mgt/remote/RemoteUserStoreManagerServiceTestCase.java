555c45bfe8eff71f911bd760feada38ab2caab02
pushpalanka <lanka@wso2.com>
2015-02-02 10:33:14 +0530

    Fresh branch with new test migration, after carbon version migration - 4.3.1 test framework - enabled testng.xml

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/user/mgt/remote/RemoteUserStoreManagerServiceTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/user/mgt/remote/RemoteUserStoreManagerServiceTestCase.java
@@ -16,7 +16,7 @@
 * under the License.
 */
 
-package org.wso2.identity.integration.test.usermgt.remote;
+package org.wso2.identity.integration.test.user.mgt.remote;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
