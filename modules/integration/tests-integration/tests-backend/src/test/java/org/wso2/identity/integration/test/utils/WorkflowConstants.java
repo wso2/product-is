d5eedd7fb217fca9ccbe48a8363404b37e9c68a5
Harsha Thirimanna <harsha.thirimanna@gmail.com>
2015-09-12 20:40:53 +0530

    change profile name in test cases

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/utils/WorkflowConstants.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/utils/WorkflowConstants.java
@@ -39,7 +39,7 @@ public class WorkflowConstants {
     public static final String DELETE_MULTIPLE_USER_CLAIMS_EVENT = "DELETE_MULTIPLE_USER_CLAIMS";
     public static final String UPDATE_USER_ROLES_EVENT = "UPDATE_USER_ROLES";
     public static final String UPDATE_ROLE_USERS_EVENT = "UPDATE_ROLE_USERS";
-    public static final String WORKFLOW_PROFILE = "all";
+    public static final String WORKFLOW_PROFILE = "full";
 
 
 }
