027c0d4281bb83be444eaccab3b9720559ee076b
lanka@wso2.com <Russian5Kusum#>
2016-02-25 11:11:23 +0530

    Modifying mobile grant sample to demonstrate custom headers + improve dataExtractUtils code + move IDP test case to relevant package.

--- modules/samples/oauth2/custom-grant/src/main/java/org/wso2/sample/identity/oauth2/grant/mobile/MobileGrant.java
+++ modules/samples/oauth2/custom-grant/src/main/java/org/wso2/sample/identity/oauth2/grant/mobile/MobileGrant.java
@@ -22,6 +22,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
+import org.wso2.carbon.identity.oauth2.ResponseHeader;
 import org.wso2.carbon.identity.oauth2.model.RequestParameter;
 import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
 import org.wso2.carbon.identity.oauth2.token.handlers.grant.AbstractAuthorizationGrantHandler;
@@ -67,7 +68,13 @@ public class MobileGrant extends AbstractAuthorizationGrantHandler  {
                 mobileUser.setUserName(mobileNumber);
                 oAuthTokenReqMessageContext.setAuthorizedUser(mobileUser);
                 oAuthTokenReqMessageContext.setScope(oAuthTokenReqMessageContext.getOauth2AccessTokenReqDTO().getScope());
+            } else{
+                ResponseHeader responseHeader = new ResponseHeader();
+                responseHeader.setKey("SampleHeader-999");
+                responseHeader.setValue("Provided Mobile Number is Invalid.");
+                oAuthTokenReqMessageContext.addProperty("RESPONSE_HEADERS", new ResponseHeader[]{responseHeader});
             }
+
         }
 
         return authStatus;
