328080c2ffbf8990e4976898f95a15a148be117a
Sumedha Kodithuwakku <sumedhassk@gmail.com>
2016-04-11 11:13:39 +0530

    Improvements suggested; Copyright update and removing un-necessary tests

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/oauth2/OAuth2ServiceRegexCallbackUrlTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/oauth2/OAuth2ServiceRegexCallbackUrlTestCase.java
@@ -1,20 +1,21 @@
 /*
- * Copyright (c) WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- * 
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
+ * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.wso2.identity.integration.test.oauth2;
 
 import org.apache.catalina.startup.Tomcat;
@@ -68,6 +69,12 @@ public class OAuth2ServiceRegexCallbackUrlTestCase extends OAuth2ServiceAbstract
 
 		setSystemproperties();
 		client = new DefaultHttpClient();
+
+		tomcat = getTomcat();
+		URL resourceUrl =
+				getClass().getResource(File.separator + "samples" + File.separator +
+						"playground2.war");
+		startTomcat(tomcat, OAuth2Constant.PLAYGROUND_APP_CONTEXT_ROOT, resourceUrl.getPath());
 	}
 
 	@AfterClass(alwaysRun = true)
@@ -81,21 +88,7 @@ public class OAuth2ServiceRegexCallbackUrlTestCase extends OAuth2ServiceAbstract
 		accessToken = null;
 	}
 
-	@Test(alwaysRun = true, description = "Deploy playground application")
-	public void testDeployPlaygroundApp() {
-		try {
-			tomcat = getTomcat();
-			URL resourceUrl =
-					getClass().getResource(File.separator + "samples" + File.separator +
-							"playground2.war");
-			startTomcat(tomcat, OAuth2Constant.PLAYGROUND_APP_CONTEXT_ROOT, resourceUrl.getPath());
-
-		} catch (Exception e) {
-			Assert.fail("Playground application deployment failed.", e);
-		}
-	}
-
-	@Test(groups = "wso2.is", description = "Check Oauth2 application registration", dependsOnMethods = "testDeployPlaygroundApp")
+	@Test(groups = "wso2.is", description = "Check Oauth2 application registration")
 	public void testRegisterApplication() throws Exception {
 
 		OAuthConsumerAppDTO appConfigData = new OAuthConsumerAppDTO();
