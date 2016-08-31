faa7a8018b7ae150a075ac27d2401ec3259d71dc
madurangasiriwardena <maduranga.siriwardena@gmail.com>
2016-08-31 15:38:39 +0530

    Fixing test cases

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/oauth2/OAuth2ServiceErrorResponseTest.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/oauth2/OAuth2ServiceErrorResponseTest.java
@@ -85,8 +85,9 @@ public class OAuth2ServiceErrorResponseTest extends OAuth2ServiceAbstractIntegra
 
         String locationURI = locationHeader.getValue();
 
-        Assert.assertTrue(locationURI.contains(OAuth2Constant.UNSUPPORTED_GRANT_TYPE));
-        Assert.assertTrue(locationURI.contains("Requested+Grant+type+is+not+supported."));
+        Assert.assertTrue(locationURI.contains(OAuth2Constant.UNAUTHORIZED_CLIENT));
+        Assert.assertTrue(locationURI.contains("The authenticated client is not authorized to use this authorization " +
+                "grant type"));
     }
 
     public OAuthConsumerAppDTO createApplication() throws Exception {
