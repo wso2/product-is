3fde6bb6e5a1a691e5d00303a487d4e8b36a51b9
minoli <minoliperera91@gmail.com>
2017-01-04 17:18:45 +0530

    Account settings - Append selected tab to the url
    Conflicts:
    	portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/public/css/user-portal.css
    	portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/public/js/account-setting.js

--- portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/public/js/account-setting.js
+++ portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/public/js/account-setting.js
@@ -66,11 +66,36 @@ $(window).load(function(){
 
 =======
 $(window).load(function(){
-    var selector = '.settings-nav li';
 
-    $(selector).click(function () {
+    var selector = '.settings-nav li';
+    $(selector).not(".collapse-li").click(function () {
         $(selector).not(".collapse-li").removeClass('active');
     });
+<<<<<<< HEAD
 >>>>>>> c06d2f5... Adding Account settings template
+=======
+
+    // Show the relevant tab from url
+    var url = document.location.toString();
+    if (url.match('#')) {
+        $('.settings-nav a[href="#' + url.split('#')[1] + '"]').tab('show');
+    } else {
+        $('.settings-content > .tab-pane:first-child').addClass('active');
+        $('.settings-nav > ul > li:first-child a').removeClass('collapsed');
+        $('#profile-menu li:first-child').addClass('active');
+    }
+
+    $( ".sub-menu" ).each(function( index ) {
+        if ($(this).children('li').hasClass('active')) {
+            $(this).addClass('in');
+        }
+    });
+
+    // Change hash for select tab
+    $('.settings-nav a, .sub-menu a').on('shown.bs.tab', function (e) {
+        window.location.hash = e.target.hash;
+    });
+
+>>>>>>> e4b1725... Account settings - Append selected tab to the url
 });
 
