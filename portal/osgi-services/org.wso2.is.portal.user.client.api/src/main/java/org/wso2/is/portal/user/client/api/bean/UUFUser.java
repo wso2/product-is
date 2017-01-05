6d67e240278c44063b529c54ef9023a247b58843
Kasun Gajasinghe <kasunbg@gmail.com>
2017-01-05 18:30:29 +0530

    Adding UUF based user to work with User Portal login
    Conflicts:
    	portal/osgi-services/org.wso2.is.portal.user.client.api/src/main/java/org/wso2/is/portal/user/client/api/bean/UUFUser.java

--- portal/osgi-services/org.wso2.is.portal.user.client.api/src/main/java/org/wso2/is/portal/user/client/api/bean/UUFUser.java
+++ portal/osgi-services/org.wso2.is.portal.user.client.api/src/main/java/org/wso2/is/portal/user/client/api/bean/UUFUser.java
@@ -1,4 +1,5 @@
 /*
+<<<<<<< HEAD
  * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,12 +19,36 @@ package org.wso2.is.portal.user.client.api.bean;
 
 import org.wso2.carbon.uuf.spi.auth.User;
 
+=======
+*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*/
+package org.wso2.is.portal.user.client.api.bean;
+
+>>>>>>> ea073eb... Adding UUF based user to work with User Portal login
 /**
  * Temporary UUFUser to login to user portal.
  *
  * //todo enable cs
  */
+<<<<<<< HEAD
 public class UUFUser implements User {
+=======
+public class UUFUser implements org.wso2.carbon.uuf.spi.auth.User {
+>>>>>>> ea073eb... Adding UUF based user to work with User Portal login
 
     private static final long serialVersionUID = 34114512424L;
 
