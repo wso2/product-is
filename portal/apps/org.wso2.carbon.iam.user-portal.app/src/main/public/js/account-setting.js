f8a65fbcdfad451a8720929ba15ddf00c27c0236
minoli <minoliperera91@gmail.com>
2017-01-10 11:50:41 +0530

    Adding password strength to password input
    Conflicts:
    	portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/fragments/reset-password/reset-password.hbs
    	portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/public/css/user-portal.css
    	portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/public/js/account-setting.js

--- portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/public/js/account-setting.js
+++ portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/public/js/account-setting.js
@@ -96,6 +96,81 @@ $(window).load(function(){
         window.location.hash = e.target.hash;
     });
 
+<<<<<<< HEAD
 >>>>>>> e4b1725... Account settings - Append selected tab to the url
+=======
+    //password strength meter logic
+    $("#newPassword").on("focus keyup", function () {
+        var score = 0;
+        var a = $(this).val();
+        var desc = new Array();
+
+        // strength desc
+        desc[0] = "Too short";
+        desc[1] = "Weak";
+        desc[2] = "Good";
+        desc[3] = "Strong";
+        desc[4] = "Best";
+
+        // password length
+        var valid = '<i class="fw fw-success"></i>';
+        var invalid = '<i class="fw fw-error"></i>';
+
+        if (a.length >= 6) {
+            $("#length").removeClass("invalid").addClass("valid");
+            $("#length .status_icon").html(valid);
+            score++;
+        } else {
+            $("#length").removeClass("valid").addClass("invalid");
+            $("#length .status_icon").html(invalid);
+        }
+
+        // at least 1 digit in password
+        if (a.match(/\d/)) {
+            $("#pnum").removeClass("invalid").addClass("valid");
+            $("#pnum .status_icon").html(valid);
+            score++;
+        } else {
+            $("#pnum").removeClass("valid").addClass("invalid");
+            $("#pnum .status_icon").html(invalid);
+        }
+
+        // at least 1 capital & lower letter in password
+        if (a.match(/[A-Z]/) && a.match(/[a-z]/)) {
+            $("#capital").removeClass("invalid").addClass("valid");
+            $("#capital .status_icon").html(valid);
+            score++;
+        } else {
+            $("#capital").removeClass("valid").addClass("invalid");
+            $("#capital .status_icon").html(invalid);
+        }
+
+        // at least 1 special character in password {
+        if ( a.match(/.[!,@,#,$,%,^,&,*,?,_,~,-,(,)]/) ) {
+            $("#spchar").removeClass("invalid").addClass("valid");
+            $("#spchar .status_icon").html(valid);
+            score++;
+        } else {
+            $("#spchar").removeClass("valid").addClass("invalid");
+            $("#spchar .status_icon").html(invalid);
+        }
+
+        if(a.length > 0) {
+            //show strength text
+            $("#passwordDescription").text(desc[score]);
+            // show indicator
+            $("#passwordStrength").removeClass().addClass("strength"+score);
+        } else {
+            $("#passwordDescription").text("Password not entered");
+            $("#passwordStrength").removeClass().addClass("strength"+score);
+        }
+    });
+
+    $("#newPassword").popover({ html:true, content: $("#password_strength_wrap").html(), placement: 'top', trigger:'focus keypress' });
+    $("#newPassword").blur(function () {
+        $(".password_strength_meter .popover").popover("hide");
+    });
+    
+>>>>>>> ec888d6... Adding password strength to password input
 });
 
