cd1cabdccc7e1f2e8c5479415fe12095ea9da8f7
thanujalk <jatljayasinghe@gmail.com>
2017-01-12 13:32:34 +0530

    Adding temporary fix for post issue in fragments.

--- portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/pages/account/account-setting.js
+++ portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/pages/account/account-setting.js
@@ -1,9 +1,19 @@
 function onRequest(env) {
 
+    var session = getSession();
+    if (!session || !session.getUser()) {
+        sendRedirect(env.contextPath + env.config['loginPageUri']);
+    }
+
+    var formId = "";
+    if (env.request.method == "POST") {
+        formId = env.request.queryString;
+    }
+
     var result = getProfileNames();
 
     if (result.success) {
-        return {"profiles": result.profiles}
+        return {profiles: result.profiles, actionId: formId}
     } else {
         return {errorMessage: result.message};
     }
