0b23f892894683ddd1c6c1fcdfe15d3aa3b17362
indunil@wso2.com <indunil@wso2.com>
2017-01-20 16:04:49 +0530

    Add more osgi test cases for user portal
    Conflicts:
    	tests/osgi-tests/src/test/java/org/wso2/is/portal/user/client/api/unit/test/IdentityStoreClientServiceTest.java

--- tests/osgi-tests/src/test/java/org/wso2/is/portal/user/client/api/unit/test/IdentityStoreClientServiceTest.java
+++ tests/osgi-tests/src/test/java/org/wso2/is/portal/user/client/api/unit/test/IdentityStoreClientServiceTest.java
@@ -28,6 +28,7 @@ import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import org.wso2.carbon.identity.mgt.claim.Claim;
+import org.wso2.carbon.identity.mgt.claim.MetaClaim;
 import org.wso2.carbon.identity.mgt.exception.UserNotFoundException;
 import org.wso2.carbon.kernel.utils.CarbonServerInfo;
 import org.wso2.is.portal.user.client.api.IdentityStoreClientService;
@@ -35,12 +36,12 @@ import org.wso2.is.portal.user.client.api.bean.UUFUser;
 import org.wso2.is.portal.user.client.api.exception.UserPortalUIException;
 import org.wso2.is.portal.user.client.api.unit.test.util.UserPortalOSGiTestUtils;
 
+import javax.inject.Inject;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javax.inject.Inject;
 
 import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 
@@ -49,6 +50,7 @@ import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 public class IdentityStoreClientServiceTest {
 
     private static List<UUFUser> users = new ArrayList<>();
+    private static String PRIMARY_DOMAIN = "PRIMARY";
 
     @Inject
     private BundleContext bundleContext;
@@ -81,7 +83,7 @@ public class IdentityStoreClientServiceTest {
         Map<String, String> userClaims = new HashMap<>();
         Map<String, String> credentials = new HashMap<>();
         userClaims.put("http://wso2.org/claims/username", "user1");
-        userClaims.put("http://wso2.org/claims/firstName", "user1_firstName");
+        userClaims.put("http://wso2.org/claims/givenname", "user1_firstName");
         userClaims.put("http://wso2.org/claims/lastName", "user1_lastName");
         userClaims.put("http://wso2.org/claims/email", "user1@wso2.com");
 
@@ -113,6 +115,29 @@ public class IdentityStoreClientServiceTest {
 >>>>>>> 1fe77ad... Uncommented all the tests which passes
     }
 
+    @Test(groups = "addUsers")
+    public void testAddUserToDomain() throws UserPortalUIException {
+        IdentityStoreClientService identityStoreClientService =
+                bundleContext.getService(bundleContext.getServiceReference(IdentityStoreClientService.class));
+        Assert.assertNotNull(identityStoreClientService, "Failed to get IdentityStoreClientService instance");
+
+        Map<String, String> userClaims = new HashMap<>();
+        Map<String, String> credentials = new HashMap<>();
+        userClaims.put("http://wso2.org/claims/username", "user2");
+        userClaims.put("http://wso2.org/claims/givenname", "user2_firstName");
+        userClaims.put("http://wso2.org/claims/lastName", "user2_lastName");
+        userClaims.put("http://wso2.org/claims/email", "user2@wso2.com");
+
+        credentials.put("password", "admin");
+
+        UUFUser user = identityStoreClientService.addUser(userClaims, credentials, PRIMARY_DOMAIN);
+
+        Assert.assertNotNull(user, "Failed to add the user.");
+        Assert.assertNotNull(user.getUserId(), "Invalid user unique id.");
+
+        users.add(user);
+    }
+
     @Test(groups = "authentication", dependsOnGroups = {"addUsers"})
     public void testAuthenticate() throws UserPortalUIException {
 
@@ -134,19 +159,23 @@ public class IdentityStoreClientServiceTest {
         Assert.assertNotNull(identityStoreClientService, "Failed to get IdentityStoreClientService instance");
 
         Map<String, String> updatedClaims = new HashMap<>();
-        updatedClaims.put("http://wso2.org/claims/firstName", "user1_firstNameUpdated");
+        updatedClaims.put("http://wso2.org/claims/givenname", "user1_firstNameUpdated");
 
-        //identityStoreClientService.updateUserProfile(users.get(0).getUserId(), updatedClaims);
+        identityStoreClientService.updateUserProfile(users.get(0).getUserId(), updatedClaims);
 
-        /*List<MetaClaim> metaClaims = new ArrayList<>();
+        List<MetaClaim> metaClaims = new ArrayList<>();
         List<Claim> userClaims;
-        MetaClaim metaClaim1 = new MetaClaim("http://wso2.org/claims", "http://wso2.org/claims/firstName");
+        MetaClaim metaClaim1 = new MetaClaim("http://wso2.org/claims", "http://wso2.org/claims/givenname");
         metaClaims.add(metaClaim1);
 
         userClaims = identityStoreClientService.getClaimsOfUser(users.get(0).getUserId(), metaClaims);
         Assert.assertNotNull(userClaims, "Failed to get the user claims.");
+<<<<<<< HEAD
         Assert.assertNotEquals(userClaims.get(0).getValue(),"user1_firstNameUpdated", "Fail to update the user profile");*/
 >>>>>>> 59b7147... Added test cases for all osgi client services in user portal:tests/osgi-tests/src/test/java/org.wso2.is.portal.user.client.api.unit.test/IdentityStoreClientServiceTest.java
+=======
+        Assert.assertEquals(userClaims.get(0).getValue(), "user1_firstNameUpdated", "Fail to update the user profile");
+>>>>>>> 9780254... Add more osgi test cases for user portal
     }
 
     @Test(dependsOnGroups = {"addUsers", "update", "authentication"})
