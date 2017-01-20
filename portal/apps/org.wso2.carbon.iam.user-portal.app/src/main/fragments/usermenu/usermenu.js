2e90f0bdb6fe9b04b088b47a2a24d316a43dda10
denuwanthi <denuwanthi.hasanthika@gmail.com>
2017-01-20 12:52:01 +0530

    add profile image to user menu fragment

--- portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/fragments/usermenu/usermenu.js
+++ portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/fragments/usermenu/usermenu.js
@@ -4,5 +4,34 @@ function onRequest(env) {
     if (!session) {
         sendRedirect(env.contextPath + env.config['loginPageUri']);
     }
-    return {username: session.getUser().getUsername()};
+    var profileImageResult = isProfileImageAvailbale(session);
+    return {
+        username: session.getUser().getUsername(), profileImage: profileImageResult.profileImage,
+        userId: profileImageResult.userId
+    };
+}
+
+function isProfileImageAvailbale(session) {
+    var Paths = Java.type('java.nio.file.Paths');
+    var System = Java.type('java.lang.System');
+    var Files = Java.type('java.nio.file.Files');
+    var File = Java.type('java.io.File');
+    var imageDirPath = Paths.get(System.getProperty('user.dir'), "images");
+    if (!Files.exists(imageDirPath)) {
+        return {profileImage: false};
+    }
+    else {
+        var file = new File(imageDirPath.toString());
+        var names = file.list();
+        if (names) {
+            for (var i = 0; i < names.length; i++) {
+                var imageName = names[i].toString();
+                if (imageName.indexOf(session.getUser().getUserId()) !== -1) {
+                    return {profileImage: true, userId: session.getUser().getUserId()};
+                }
+            }
+        }
+
+    }
+    return {profileImage: false};
 }
