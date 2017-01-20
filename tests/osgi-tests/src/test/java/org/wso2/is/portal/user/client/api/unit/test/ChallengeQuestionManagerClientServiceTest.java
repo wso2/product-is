fb482c5838a9d92a13fbc58bc5bbb52610b1cf24
indunil@wso2.com <indunil@wso2.com>
2017-01-20 08:58:38 +0530

    Change test case name for ChallengeQuestionManagerClientService
    Conflicts:
    	tests/osgi-tests/src/test/resources/testng.xml

--- tests/osgi-tests/src/test/java/org/wso2/is/portal/user/client/api/unit/test/ChallengeQuestionManagerClientServiceTest.java
+++ tests/osgi-tests/src/test/java/org/wso2/is/portal/user/client/api/unit/test/ChallengeQuestionManagerClientServiceTest.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.is.portal.user.client.api.unit.test;
@@ -22,15 +24,18 @@ import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerSuite;
 import org.ops4j.pax.exam.testng.listener.PaxExam;
 import org.osgi.framework.BundleContext;
+import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import org.wso2.carbon.identity.recovery.model.ChallengeQuestion;
 import org.wso2.carbon.kernel.utils.CarbonServerInfo;
+import org.wso2.is.portal.user.client.api.ChallengeQuestionManagerClientService;
 import org.wso2.is.portal.user.client.api.exception.UserPortalUIException;
 import org.wso2.is.portal.user.client.api.unit.test.util.UserPortalOSGiTestUtils;
 
+import javax.inject.Inject;
 import java.nio.file.Paths;
 import java.util.List;
-import javax.inject.Inject;
 
 import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 
@@ -63,11 +68,8 @@ public class ChallengeQuestionManagerClientServiceTest {
     @Test(groups = "getChallengeQuestion")
     public void testGetChallengeQuestionList() throws UserPortalUIException {
         /*ChallengeQuestionManagerClientService challengeQuestionManagerService =
-                bundleContext.getService(bundleContext.getServiceReference
-                (ChallengeQuestionManagerClientService.class));
-        Assert.assertNotNull(challengeQuestionManagerService,
-        "Failed to get ChallengeQuestionManagerClientService instance");*/
-
+                bundleContext.getService(bundleContext.getServiceReference(ChallengeQuestionManagerClientService.class));
+        Assert.assertNotNull(challengeQuestionManagerService, "Failed to get ChallengeQuestionManagerClientService instance");*/
 
         /*List<ChallengeQuestion> challengeQuestions = challengeQuestionManagerService.getChallengeQuestionList();
         Assert.assertNotNull(challengeQuestions, "Failed to retrieve the challenge question list.");*/
