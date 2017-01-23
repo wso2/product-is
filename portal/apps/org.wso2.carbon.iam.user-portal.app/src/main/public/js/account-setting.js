00cb1df566071a76d824a04f7a26fb7aa1acc51c
minoli <minoliperera91@gmail.com>
2017-01-23 09:58:45 +0530

    UI fixes
    Conflicts:
    	portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/pages/signup.hbs

--- portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/public/js/account-setting.js
+++ portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/public/js/account-setting.js
@@ -94,6 +94,7 @@ $(window).load(function(){
     // Change hash for select tab
     $('.settings-nav a, .sub-menu a').on('shown.bs.tab', function (e) {
         window.location.hash = e.target.hash;
+        $(window).scrollTop(0);
     });
 <<<<<<< HEAD
 
