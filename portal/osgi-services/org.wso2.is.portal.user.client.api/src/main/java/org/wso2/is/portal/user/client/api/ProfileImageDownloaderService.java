b8255cb8e04422e95cb36194c57d3857c265c0f9
indunil@wso2.com <indunil@wso2.com>
2017-01-26 22:45:39 +0530

    Added license headers

--- portal/osgi-services/org.wso2.is.portal.user.client.api/src/main/java/org/wso2/is/portal/user/client/api/ProfileImageDownloaderService.java
+++ portal/osgi-services/org.wso2.is.portal.user.client.api/src/main/java/org/wso2/is/portal/user/client/api/ProfileImageDownloaderService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.wso2.is.portal.user.client.api;
 
 import org.wso2.msf4j.Microservice;
