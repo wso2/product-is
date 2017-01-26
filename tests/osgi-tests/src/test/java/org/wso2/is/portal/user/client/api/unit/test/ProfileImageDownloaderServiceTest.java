6cb7ce0601332d22f3083b11e60e3614e065bb89
indunil@wso2.com <indunil@wso2.com>
2017-01-26 06:10:17 +0530

    Resolved checkstyle issues
    Conflicts:
    	tests/osgi-tests/src/test/java/org/wso2/is/portal/user/client/api/unit/test/ProfileImageDownloaderServiceTest.java

--- tests/osgi-tests/src/test/java/org/wso2/is/portal/user/client/api/unit/test/ProfileImageDownloaderServiceTest.java
+++ tests/osgi-tests/src/test/java/org/wso2/is/portal/user/client/api/unit/test/ProfileImageDownloaderServiceTest.java
@@ -33,9 +33,12 @@ package org.wso2.is.portal.user.client.api.unit.test;
  */
 package org.wso2.is.portal.user.client.api.unit.test;
 
+<<<<<<< HEAD
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 >>>>>>> f61b1d5... Add initial test case for profile downloader microservice
+=======
+>>>>>>> b3abc2e... Resolved checkstyle issues
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerSuite;
 import org.ops4j.pax.exam.testng.listener.PaxExam;
@@ -44,24 +47,30 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 <<<<<<< HEAD
+<<<<<<< HEAD
 import org.wso2.is.portal.user.client.api.ProfileImageDownloaderService;
 import org.wso2.msf4j.MicroservicesRunner;
 
 =======
 import org.wso2.carbon.kernel.utils.CarbonServerInfo;
+=======
+>>>>>>> b3abc2e... Resolved checkstyle issues
 import org.wso2.is.portal.user.client.api.ProfileImageDownloaderService;
-import org.wso2.is.portal.user.client.api.unit.test.util.UserPortalOSGiTestUtils;
 import org.wso2.msf4j.MicroservicesRunner;
 
+<<<<<<< HEAD
 import javax.inject.Inject;
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 >>>>>>> f61b1d5... Add initial test case for profile downloader microservice
+=======
+>>>>>>> b3abc2e... Resolved checkstyle issues
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
+<<<<<<< HEAD
 <<<<<<< HEAD
 import javax.ws.rs.HttpMethod;
 import javax.ws.rs.core.Response;
@@ -70,11 +79,17 @@ import static org.testng.AssertJUnit.assertEquals;
 =======
 import java.nio.file.Paths;
 import java.util.List;
+=======
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.core.Response;
+>>>>>>> b3abc2e... Resolved checkstyle issues
 
-import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 import static org.testng.AssertJUnit.assertEquals;
+<<<<<<< HEAD
 import static org.testng.AssertJUnit.assertTrue;
 >>>>>>> f61b1d5... Add initial test case for profile downloader microservice
+=======
+>>>>>>> b3abc2e... Resolved checkstyle issues
 
 @Listeners(PaxExam.class)
 @ExamReactorStrategy(PerSuite.class)
@@ -103,11 +118,16 @@ public class ProfileImageDownloaderServiceTest {
     @Test(groups = "getProfileImage")
     public void testGetProfileImage() throws IOException {
 <<<<<<< HEAD
+<<<<<<< HEAD
         HttpURLConnection urlConn = request("/DynamicPath/image?userid=1.0a8faaa2-4091-4000-bdd4-9c417798e47c",
                 HttpMethod.GET);
 =======
         HttpURLConnection urlConn = request("/DynamicPath/image?userid=1.0a8faaa2-4091-4000-bdd4-9c417798e47c", HttpMethod.GET);
 >>>>>>> f61b1d5... Add initial test case for profile downloader microservice
+=======
+        HttpURLConnection urlConn = request("/DynamicPath/image?userid=1.0a8faaa2-4091-4000-bdd4-9c417798e47c",
+                HttpMethod.GET);
+>>>>>>> b3abc2e... Resolved checkstyle issues
         assertEquals(Response.Status.OK.getStatusCode(), urlConn.getResponseCode());
     }
 
