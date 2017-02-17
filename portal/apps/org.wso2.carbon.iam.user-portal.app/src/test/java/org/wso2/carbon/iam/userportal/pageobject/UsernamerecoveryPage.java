388f043762fd20361e4df9beb85b85d84d1d048d
GDRDABARERA <gdrdabarera@gmail.com>
2017-02-17 16:25:02 +0530

    Building a Test UI objects and tests

---
+++ portal/apps/org.wso2.carbon.iam.user-portal.app/src/test/java/org/wso2/carbon/iam/userportal/pageobject/UsernamerecoveryPage.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wso2.carbon.iam.userportal.pageobject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Represents username recovery page.
+ */
+public class UsernamerecoveryPage {
+
+    private static WebElement element = null;
+
+    public static WebElement btn_BackSignIn(WebDriver driver) {
+        element = driver.findElement(By.id("back-sign-in"));
+        return element;
+    }
+
+    public static WebElement btn_Recover(WebDriver driver) {
+        element = driver.findElement(By.id("recover"));
+        return element;
+    }
+
+    public static WebElement txtbx_FirstName(WebDriver driver, String label) {
+        element = driver.findElement(By.id(label));
+        return element;
+    }
+}
