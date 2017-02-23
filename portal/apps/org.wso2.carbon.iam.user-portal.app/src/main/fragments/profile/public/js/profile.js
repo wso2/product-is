cab1cf44b5e77efcc3ea0e7be4b6d9bfde987cf0
denuwanthi <denuwanthi.hasanthika@gmail.com>
2017-02-23 16:32:14 +0530

    IDENTITY-5712 close the successful messages within 5 seconds time

--- portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/fragments/profile/public/js/profile.js
+++ portal/apps/org.wso2.carbon.iam.user-portal.app/src/main/fragments/profile/public/js/profile.js
@@ -45,4 +45,12 @@ $(window).load(function () {
         }
     })
 
+
+
 });
+
+window.setTimeout(function() {
+    $(".alert-success").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove();
+    });
+}, 5000);
