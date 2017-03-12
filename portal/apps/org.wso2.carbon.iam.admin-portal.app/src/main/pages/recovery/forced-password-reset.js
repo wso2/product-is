f6cd9a94e4221d9488dcea1ad02fffdf48e5fb3a
hasanthi <hasanthidisanayakka@yahoo.com>
2017-03-12 16:00:31 +0530

    Improving texts

--- portal/apps/org.wso2.carbon.iam.admin-portal.app/src/main/pages/recovery/forced-password-reset.js
+++ portal/apps/org.wso2.carbon.iam.admin-portal.app/src/main/pages/recovery/forced-password-reset.js
@@ -1,8 +1,5 @@
 
 function onGet(env) {
-    var session = getSession();
-    if (!session || !session.getUser()) {
-        sendRedirect(env.contextPath + env.config['loginPageUri']);
-    }
+
 }
 
