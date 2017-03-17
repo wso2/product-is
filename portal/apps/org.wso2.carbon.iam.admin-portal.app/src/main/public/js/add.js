4320f51e3e93c65dc2c959ae1793dad6dc6034d5
denuwanthi <denuwanthi.hasanthika@gmail.com>
2017-03-17 11:40:15 +0530

    Add user existence validation from UI

--- portal/apps/org.wso2.carbon.iam.admin-portal.app/src/main/public/js/add.js
+++ portal/apps/org.wso2.carbon.iam.admin-portal.app/src/main/public/js/add.js
@@ -44,7 +44,50 @@ function displayVals() {
             "accountVerificationMethod-area", "OVERWRITE", callbacks);
     }
 }
-
+document.getElementById("inputUsername").onblur = function() {userNameExists()};
+function userNameExists() {
+    var username = document.getElementById('inputUsername').value;
+    if (!username) {
+        return;
+    }
+    var usernameClaimUri = "http://wso2.org/claims/username";
+    var domain = null;
+    if (document.getElementById('domainSelector')) {
+        domain = document.getElementById('domainSelector').value;
+    }
+    $.ajax({
+        type: "GET",
+        url: "/admin-portal/root/apis/identityStore-micro-service/userExists",
+        data: {username: username, usernameClaimUri: usernameClaimUri, domain: domain},
+        success: function (data) {
+            if (data === "true") {
+                var fillingObject = {
+                    "userExists": true
+                };
+                var callbacks = {
+                    onSuccess: function () {
+                        $("#addUser").prop('disabled', true);
+                    },
+                    onFailure: function (e) {
+                    }
+                };
+                UUFClient.renderFragment("user-existance", fillingObject,
+                    "userExistsError-area", "OVERWRITE", callbacks);
+            } else {
+                var fillingObject = {};
+                var callbacks = {
+                    onSuccess: function () {
+                        $("#addUser").prop('disabled', false);
+                    },
+                    onFailure: function (e) {
+                    }
+                };
+                UUFClient.renderFragment("user-existance", fillingObject,
+                    "userExistsError-area", "OVERWRITE", callbacks);
+            }
+        }
+    });
+}
 $("#addUserForm").validate({
     rules: {
         confirmPassword: {
