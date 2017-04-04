59a07bde7f47a6095288a7905f77d04bbf78b013
hasanthi <hasanthidisanayakka@yahoo.com>
2017-04-04 09:14:56 +0530

    Adding success/danger alerts

--- portal/apps/org.wso2.carbon.iam.admin-portal.app/src/main/pages/reset/admin-initiated-password-reset.js
+++ portal/apps/org.wso2.carbon.iam.admin-portal.app/src/main/pages/reset/admin-initiated-password-reset.js
@@ -1,22 +1,20 @@
 function getPasscode(env) {
     try {
         var passcode = callOSGiService("org.wso2.is.portal.user.client.api.RecoveryMgtService", "persistPasscode",
-            ["user1"]);
+            ["user1"]);;
     } catch (e) {
-        return {success: false, errorMessage: 'Error while persisting the passcode'};
+        return {errorMessage: 'Error while persisting the passcode'};
     }
-    return passcode;
+    return {sucess: true, code: passcode};
 }
 
 function onPost(env) {
-    try {
-        var passcode = getPasscode(env);
-        sendToClient("passcode", passcode);
-        return {success: true, message: 'Succefully persisted the passcode.'};
-
-    } catch (e) {
-        return {success: false, errorMessage: 'Error while persisting the passcode'};
+    var passcode = getPasscode(env);
+    if (passcode.errorMessage) {
+        return {success: false, message: 'error.while.persisting'};
+    } else {
+        sendToClient("passcode", passcode.code);
+        return {success: true, message: 'successfully.persisted'};
     }
-
 }
 
