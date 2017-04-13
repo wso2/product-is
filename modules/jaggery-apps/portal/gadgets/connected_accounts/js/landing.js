465144d12558f9d179cefb556c5ed2b6cca33a9d
nuwandiw <nuwandiw@wso2.com>
2017-04-13 13:48:02 +0530

    IDENTITY-5835: Making POST calls to portal gadgets and adding cookie as post param

--- modules/jaggery-apps/portal/gadgets/connected_accounts/js/landing.js
+++ modules/jaggery-apps/portal/gadgets/connected_accounts/js/landing.js
@@ -156,7 +156,7 @@ function drawAddFedAccountPopup() {
 
     $.ajax({
         url: "/portal/gadgets/connected_accounts/index.jag",
-        type: "GET",
+        type: "POST",
         data: "&cookie=" + cookie + "&action=idPList",
         success: function (data) {
             var resp = $.parseJSON(data);
