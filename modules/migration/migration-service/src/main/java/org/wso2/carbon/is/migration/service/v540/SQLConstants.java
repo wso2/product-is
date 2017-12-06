6baba228b55179844283d0375158a45eb6c74e43
Thanuja Jayasinghe <thanuja@Thanujas-MacBook-Pro.local>
2017-12-06 17:22:03 +0530

    Updating IS 5.4.0 migration client.

--- modules/migration/migration-service/src/main/java/org/wso2/carbon/is/migration/service/v540/SQLConstants.java
+++ modules/migration/migration-service/src/main/java/org/wso2/carbon/is/migration/service/v540/SQLConstants.java
@@ -21,64 +21,17 @@ package org.wso2.carbon.is.migration.service.v540;
  */
 public class SQLConstants {
 
-    public static final String ID_COLUMN = "ID";
-    public static final String GET_SCOPE_ROLES = "SELECT * FROM IDN_OAUTH2_SCOPE";
-    public static final String ADD_SCOPE_BINDINGS = "INSERT INTO IDN_OAUTH2_SCOPE_BINDING (SCOPE_ID, SCOPE_BINDING) " +
-                                                    "VALUES (?, ?)";
-    public static final String UPDATE_SCOPE_ROLES = "UPDATE IDN_OAUTH2_SCOPE SET NAME = ?, DESCRIPTION = ? "
-                                                    + " WHERE SCOPE_ID = ? ";
-    // Claim Dialect SQLs
-    public static final String ADD_CLAIM_DIALECT = "INSERT INTO IDN_CLAIM_DIALECT (DIALECT_URI, TENANT_ID) " +
-                                                   "VALUES (?, ?)";
-    public static final String GET_CLAIM_DIALECT
-            = "SELECT ID FROM IDN_CLAIM_DIALECT WHERE DIALECT_URI= ? AND TENANT_ID = ? ";
-    // Claim SQLs
-    public static final String GET_CLAIM_ID = "SELECT ID FROM IDN_CLAIM WHERE DIALECT_ID=(SELECT ID FROM " +
-                                              "IDN_CLAIM_DIALECT WHERE DIALECT_URI=? AND TENANT_ID=?) AND CLAIM_URI=?"
-                                              + " AND TENANT_ID=?";
-    // Claim SQLs
-    public static final String GET_CLAIM_URI = "SELECT CLAIM_URI FROM IDN_CLAIM WHERE DIALECT_ID=(SELECT ID FROM " +
-                                               "IDN_CLAIM_DIALECT WHERE DIALECT_URI=? AND TENANT_ID=?) AND ID=? AND "
-                                               + "TENANT_ID=?";
-    public static final String ADD_CLAIM = "INSERT INTO IDN_CLAIM (DIALECT_ID, CLAIM_URI, TENANT_ID) VALUES ((SELECT " +
-                                           "ID FROM IDN_CLAIM_DIALECT WHERE DIALECT_URI=? AND TENANT_ID=?), ?, ?)";
-    // Local Claim Attributes SQLs
-    public static final String ADD_CLAIM_MAPPED_ATTRIBUTE = "INSERT INTO IDN_CLAIM_MAPPED_ATTRIBUTE (LOCAL_CLAIM_ID, " +
-                                                            "USER_STORE_DOMAIN_NAME, ATTRIBUTE_NAME, TENANT_ID) "
-                                                            + "VALUES (?, ?, ?, ?)";
-    public static final String GET_CLAIM_FROM_MAPPED_ATTRIBUTE =
-            "SELECT LOCAL_CLAIM_ID FROM IDN_CLAIM_MAPPED_ATTRIBUTE " +
-            "WHERE USER_STORE_DOMAIN_NAME = ? AND ATTRIBUTE_NAME =? AND TENANT_ID=?";
-    // Local Claim Properties SQLs
-    public static final String ADD_CLAIM_PROPERTY = "INSERT INTO IDN_CLAIM_PROPERTY (LOCAL_CLAIM_ID, PROPERTY_NAME, " +
-                                                    "PROPERTY_VALUE, TENANT_ID) VALUES (?, ?, ?, ?)";
-    // External Claim Mapping SQLs
-    public static final String ADD_CLAIM_MAPPING = "INSERT INTO IDN_CLAIM_MAPPING (MAPPED_LOCAL_CLAIM_ID, " +
-                                                   "EXT_CLAIM_ID, TENANT_ID) VALUES (?, ?, ?)";
-    //Select all entries for given permission string
-    public static final String SELECT_PERMISSION = "SELECT UM_ID, UM_RESOURCE_ID, UM_ACTION , UM_TENANT_ID, " +
-                                                   "UM_MODULE_ID FROM UM_PERMISSION WHERE UM_RESOURCE_ID=?";
-    //Select permission entry count
-    public static final String SELECT_PERMISSION_COUNT = "SELECT COUNT(1) FROM UM_PERMISSION WHERE UM_RESOURCE_ID=? " +
-                                                         "AND UM_ACTION=? AND UM_TENANT_ID=? AND UM_MODULE_ID=?";
-    //Select role count with permission
-    public static final String SELECT_ROLE_PERMISSION_COUNT = "SELECT COUNT(1) FROM UM_ROLE_PERMISSION WHERE " +
-                                                              "UM_PERMISSION_ID=? AND UM_ROLE_NAME=? AND "
-                                                              + "UM_IS_ALLOWED=? AND UM_TENANT_ID=? AND UM_DOMAIN_ID=?";
-    //insert permission
-    public static final String INSERT_PERMISSION = "INSERT INTO UM_PERMISSION (UM_RESOURCE_ID, UM_ACTION , " +
-                                                   "UM_TENANT_ID, UM_MODULE_ID) VALUES (?, ?, ?, ?)";
-    //Select role entries with permission
-    public static final String SELECT_ROLES_WITH_PERMISSION = "SELECT UM_PERMISSION_ID, UM_ROLE_NAME, UM_IS_ALLOWED, " +
-                                                              "UM_TENANT_ID, UM_DOMAIN_ID FROM UM_ROLE_PERMISSION "
-                                                              + "WHERE UM_PERMISSION_ID=?";
-    //Select permission entries with permission string in tenant
-    public static final String SELECT_PERMISSION_IN_TENANT = "SELECT UM_ID, UM_RESOURCE_ID, UM_ACTION , " +
-                                                             "UM_TENANT_ID, UM_MODULE_ID FROM UM_PERMISSION WHERE "
-                                                             + "UM_RESOURCE_ID=? AND UM_TENANT_ID=?";
-    //Insert permission for role
-    public static final String INSERT_ROLES_WITH_PERMISSION = "INSERT INTO UM_ROLE_PERMISSION (UM_PERMISSION_ID," +
-                                                              "UM_ROLE_NAME, UM_IS_ALLOWED, UM_TENANT_ID, "
-                                                              + "UM_DOMAIN_ID) VALUES (?, ?, ?,?, ?)";
+    public static final String GET_ALL_OAUTH2_SCOPES = "SELECT SCOPE_ID, SCOPE_KEY, NAME, ROLES FROM IDN_OAUTH2_SCOPE";
 
+    public static final String ADD_SCOPE_BINDINGS = "INSERT INTO IDN_OAUTH2_SCOPE_BINDING (SCOPE_ID, SCOPE_BINDING) " +
+            "VALUES (?, ?)";
+
+    public static final String UPDATE_OAUTH2_SCOPES = "UPDATE IDN_OAUTH2_SCOPE SET NAME = ? WHERE SCOPE_ID = ? ";
+
+    public static final String RETRIEVE_ALL_CONSUMER_APPS = "SELECT CONSUMER_KEY, TENANT_ID FROM " +
+            "IDN_OAUTH_CONSUMER_APPS";
+
+    public static final String UPDATE_EXPIRY_TIMES_IN_CONSUMER_APPS = "UPDATE IDN_OAUTH_CONSUMER_APPS SET " +
+            "USER_ACCESS_TOKEN_EXPIRE_TIME=?, APP_ACCESS_TOKEN_EXPIRE_TIME=?, REFRESH_TOKEN_EXPIRE_TIME=? WHERE " +
+            "CONSUMER_KEY=?";
 }
--- modules/migration/migration-service/src/main/java/org/wso2/carbon/is/migration/service/v540/bean/OAuth2Scope.java
+++ modules/migration/migration-service/src/main/java/org/wso2/carbon/is/migration/service/v540/bean/OAuth2Scope.java
@@ -13,65 +13,67 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
+
 package org.wso2.carbon.is.migration.service.v540.bean;
 
 /**
- * Bean class for OAuth2 Scope
+ * Bean class for OAuth2 Scope.
  */
 public class OAuth2Scope {
 
-    private String scopeId;
-    private String roleString;
+    private int scopeId;
 
     private String scopeKey;
-    private String name;
-    private String description;
 
-    public OAuth2Scope(String scopeId, String roleString, String scopeKey, String name, String description) {
+    private String name;
+
+    private String roles;
+
+    public OAuth2Scope(int scopeId, String scopeKey, String name, String roles) {
+
         this.scopeId = scopeId;
-        this.roleString = roleString;
         this.scopeKey = scopeKey;
         this.name = name;
-        this.description = description;
+        this.roles = roles;
     }
 
-    public String getDescription() {
-        return description;
-    }
+    public int getScopeId() {
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRoleString() {
-        return roleString;
-    }
-
-    public void setRoleString(String roleString) {
-        this.roleString = roleString;
-    }
-
-    public String getScopeId() {
         return scopeId;
     }
 
-    public void setScopeId(String scopeId) {
+    public void setScopeId(int scopeId) {
+
         this.scopeId = scopeId;
     }
 
     public String getScopeKey() {
+
         return scopeKey;
     }
 
     public void setScopeKey(String scopeKey) {
+
         this.scopeKey = scopeKey;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public String getRoles() {
+
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+
+        this.roles = roles;
     }
 }
