d8df8cbbcb492126e167021a19615089d46fb142
emsw.bandara <emsw.bandara@gmail.com>
2018-01-12 17:10:57 +0530

    Adding integration tests for Conditional Authentication Support using JavaScript feature

---
+++ modules/integration/tests-integration/tests-backend/src/test/resources/script.js
@@ -0,0 +1,21 @@
+function test(context) {
+    var acr = selectAcrFrom(context, ["acr1", "acr2"]);
+    Log.info("--------------- ACR selected "+ acr);
+    //comment
+    switch(acr) {
+        case "acr1" : executeStep({id :'1',
+            on : {
+                success : function(context) {
+                    var isAdmin = hasRole(context, 'admin');
+                    Log.info("--------------- Has Admin "+isAdmin);
+                    if(isAdmin) {
+                        executeStep({id :'2'});
+                    }
+                }
+            }
+        });
+            break;
+        case "acr2" : executeStep({id :'1'}); executeStep({id :'2'});  break;
+        default :  executeStep({id :'1'});  executeStep({id :'2'});
+    }
+}
