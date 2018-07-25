76e9dabd867d16331799a06a7519a7b56cfd58c1
madurangasiriwardena <maduranga.siriwardena@gmail.com>
2018-07-25 18:09:55 +0530

    Format DataExtractUtil

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/utils/DataExtractUtil.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/utils/DataExtractUtil.java
@@ -1,13 +1,13 @@
 /*
  * Copyright (c) WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- * 
+ *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -39,263 +39,273 @@ import java.util.regex.Pattern;
  */
 public class DataExtractUtil {
 
-	/**
-	 * Extract data from http response with the given keywords
-	 * 
-	 * @param response
-	 * @param keyPositionMap
-	 * @return values
-	 * @throws java.io.IOException
-	 */
-	public static List<KeyValue> extractDataFromResponse(HttpResponse response,
-														 Map<String, Integer> keyPositionMap)
-			throws IOException {
+    /**
+     * Extract data from http response with the given keywords
+     *
+     * @param response
+     * @param keyPositionMap
+     * @return values
+     * @throws java.io.IOException
+     */
+    public static List<KeyValue> extractDataFromResponse(HttpResponse response,
+                                                         Map<String, Integer> keyPositionMap)
+            throws IOException {
 
-		//todo extracting sessionDataKey using this method required key and value to be in the same line
-		//todo ex. <input type="hidden" name="sessionDataKey"  value='8a433378-6d1f-434b-b574-a143dbb1a508'/>
-		//todo if the jsp page formatted and value moved to the next line this will break. nice to have this fixed
+        //todo extracting sessionDataKey using this method required key and value to be in the same line
+        //todo ex. <input type="hidden" name="sessionDataKey"  value='8a433378-6d1f-434b-b574-a143dbb1a508'/>
+        //todo if the jsp page formatted and value moved to the next line this will break. nice to have this fixed
 
-		List<KeyValue> keyValues = new ArrayList<>();
-		BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-		String line;
-		while ((line = rd.readLine()) != null) {
-			for (String key : keyPositionMap.keySet()) {
-				if (line.contains(key)) {
-					String[] tokens;
-					if (line.contains("'")) {
-						tokens = line.split("'");
-						KeyValue keyValue = new KeyValue(key, tokens[keyPositionMap.get(key)]);
-						keyValues.add(keyValue);
-						return keyValues;
-					} else {
-						String regexString = Pattern.quote(key + " value=\"") + "(.*?)" + Pattern.quote("\"");
-						Pattern pattern = Pattern.compile(regexString);
-						Matcher matcher = pattern.matcher(line);
-						if (matcher.find()) {
-							KeyValue keyValue = new KeyValue(key, matcher.group(1));
-							keyValues.add(keyValue);
-							return keyValues;
-						}
-						return null;
-					}
-				}
-			}
-		}
-		rd.close();
-		return null;
-	}
+        List<KeyValue> keyValues = new ArrayList<>();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            for (String key : keyPositionMap.keySet()) {
+                if (line.contains(key)) {
+                    String[] tokens;
+                    if (line.contains("'")) {
+                        tokens = line.split("'");
+                        KeyValue keyValue = new KeyValue(key, tokens[keyPositionMap.get(key)]);
+                        keyValues.add(keyValue);
+                        return keyValues;
+                    } else {
+                        String regexString = Pattern.quote(key + " value=\"") + "(.*?)" + Pattern.quote("\"");
+                        Pattern pattern = Pattern.compile(regexString);
+                        Matcher matcher = pattern.matcher(line);
+                        if (matcher.find()) {
+                            KeyValue keyValue = new KeyValue(key, matcher.group(1));
+                            keyValues.add(keyValue);
+                            return keyValues;
+                        }
+                        return null;
+                    }
+                }
+            }
+        }
+        rd.close();
+        return null;
+    }
 
-	/**
-	 * Extract input values from http response
-	 * 
-	 * @param response
-	 * @param keyPositionMap
-	 * @return values
-	 * @throws java.io.IOException
-	 */
-	public static List<KeyValue> extractInputValueFromResponse(HttpResponse response,
-	                                                           Map<String, Integer> keyPositionMap)
-	                                                                                               throws IOException {
-		List<KeyValue> keyValues = new ArrayList<KeyValue>();
-		BufferedReader rd =
-		                    new BufferedReader(new InputStreamReader(response.getEntity()
-		                                                                     .getContent()));
-		String line;
+    /**
+     * Extract input values from http response
+     *
+     * @param response
+     * @param keyPositionMap
+     * @return values
+     * @throws java.io.IOException
+     */
+    public static List<KeyValue> extractInputValueFromResponse(HttpResponse response,
+                                                               Map<String, Integer> keyPositionMap)
+            throws IOException {
 
-		while ((line = rd.readLine()) != null) {
-			for (String key : keyPositionMap.keySet()) {
-				if (line.contains(key)) {
-					Pattern p = Pattern.compile("value=\"(\\S+)\"");
-					Matcher m = p.matcher(line);
-					if (m.find()) {
-						KeyValue keyValue = new KeyValue(key, m.group(1));
-						keyValues.add(keyValue);
-						return keyValues;
-					}
-				}
-			}
-		}
-		rd.close();
+        List<KeyValue> keyValues = new ArrayList<KeyValue>();
+        BufferedReader rd =
+                new BufferedReader(new InputStreamReader(response.getEntity()
+                        .getContent()));
+        String line;
 
-		return null;
-	}
+        while ((line = rd.readLine()) != null) {
+            for (String key : keyPositionMap.keySet()) {
+                if (line.contains(key)) {
+                    Pattern p = Pattern.compile("value=\"(\\S+)\"");
+                    Matcher m = p.matcher(line);
+                    if (m.find()) {
+                        KeyValue keyValue = new KeyValue(key, m.group(1));
+                        keyValues.add(keyValue);
+                        return keyValues;
+                    }
+                }
+            }
+        }
+        rd.close();
 
-	/**
-	 * Extract label values from response
-	 * 
-	 * @param response
-	 * @param keyPositionMap
-	 * @return values
-	 * @throws java.io.IOException
-	 */
-	public static List<KeyValue> extractLabelValueFromResponse(HttpResponse response,
-	                                                           Map<String, Integer> keyPositionMap)
-	                                                                                               throws IOException {
-		List<KeyValue> keyValues = new ArrayList<KeyValue>();
-		BufferedReader rd =
-		                    new BufferedReader(new InputStreamReader(response.getEntity()
-		                                                                     .getContent()));
+        return null;
+    }
 
-		String line;
+    /**
+     * Extract label values from response
+     *
+     * @param response
+     * @param keyPositionMap
+     * @return values
+     * @throws java.io.IOException
+     */
+    public static List<KeyValue> extractLabelValueFromResponse(HttpResponse response,
+                                                               Map<String, Integer> keyPositionMap)
+            throws IOException {
 
-		while ((line = rd.readLine()) != null) {
-			for (String key : keyPositionMap.keySet()) {
-				if (line.contains(key)) {
-					Pattern p = Pattern.compile(">(\\S+)</label>");
-					Matcher m = p.matcher(line);
-					if (m.find()) {
-						KeyValue keyValue = new KeyValue(key, m.group(1));
-						keyValues.add(keyValue);
-						return keyValues;
-					}
-				}
-			}
-		}
-		rd.close();
+        List<KeyValue> keyValues = new ArrayList<KeyValue>();
+        BufferedReader rd =
+                new BufferedReader(new InputStreamReader(response.getEntity()
+                        .getContent()));
 
-		return null;
-	}
+        String line;
 
-	/**
-	 * Extract table row data from http response
-	 * 
-	 * @param response
-	 * @param keyPositionMap
-	 * @return values
-	 * @throws java.io.IOException
-	 */
-	public static List<KeyValue> extractTableRowDataFromResponse(HttpResponse response,
-	                                                             Map<String, Integer> keyPositionMap)
-	                                                                                                 throws IOException {
-		boolean lineReached = false;
-		List<KeyValue> keyValues = new ArrayList<KeyValue>();
-		BufferedReader rd =
-		                    new BufferedReader(new InputStreamReader(response.getEntity()
-		                                                                     .getContent()));
-		String line;
-		while ((line = rd.readLine()) != null) {
-			for (String key : keyPositionMap.keySet()) {
-				if (line.contains(key)) {
-					lineReached = true;
-					continue;
-				}
-				if (lineReached) {
-					if (line.contains("<td>")) {
-						Pattern p = Pattern.compile("<td>(\\S+)</td>");
-						Matcher m = p.matcher(line);
-						if (m.find()) {
-							KeyValue keyValue = new KeyValue(key, m.group(1));
-							keyValues.add(keyValue);
-							return keyValues;
-						}
-					}
-				}
-			}
-		}
-		rd.close();
+        while ((line = rd.readLine()) != null) {
+            for (String key : keyPositionMap.keySet()) {
+                if (line.contains(key)) {
+                    Pattern p = Pattern.compile(">(\\S+)</label>");
+                    Matcher m = p.matcher(line);
+                    if (m.find()) {
+                        KeyValue keyValue = new KeyValue(key, m.group(1));
+                        keyValues.add(keyValue);
+                        return keyValues;
+                    }
+                }
+            }
+        }
+        rd.close();
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * Extract session consent data from response
-	 * 
-	 * @param response
-	 * @param keyPositionMap
-	 * @return
-	 * @throws java.io.IOException
-	 */
-	public static List<KeyValue> extractSessionConsentDataFromResponse(HttpResponse response,
-	                                                                   Map<String, Integer> keyPositionMap)
-	                                                                                                       throws IOException {
-		boolean lineReached = false;
-		List<KeyValue> keyValues = new ArrayList<KeyValue>();
-		BufferedReader rd =
-		                    new BufferedReader(new InputStreamReader(response.getEntity()
-		                                                                     .getContent()));
-		String line;
-		while ((line = rd.readLine()) != null) {
-			for (String key : keyPositionMap.keySet()) {
-				if (line.contains(key)) {
-					lineReached = true;
-				}
-				if (lineReached) {
-					if (line.contains("value")) {
-						String[] tokens = line.split("\"");
-						KeyValue keyValue = new KeyValue(key, tokens[1]);
-						keyValues.add(keyValue);
-						return keyValues;
-					}
-				}
-			}
-		}
-		rd.close();
+    /**
+     * Extract table row data from http response
+     *
+     * @param response
+     * @param keyPositionMap
+     * @return values
+     * @throws java.io.IOException
+     */
+    public static List<KeyValue> extractTableRowDataFromResponse(HttpResponse response,
+                                                                 Map<String, Integer> keyPositionMap)
+            throws IOException {
 
-		return null;
-	}
+        boolean lineReached = false;
+        List<KeyValue> keyValues = new ArrayList<KeyValue>();
+        BufferedReader rd =
+                new BufferedReader(new InputStreamReader(response.getEntity()
+                        .getContent()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            for (String key : keyPositionMap.keySet()) {
+                if (line.contains(key)) {
+                    lineReached = true;
+                    continue;
+                }
+                if (lineReached) {
+                    if (line.contains("<td>")) {
+                        Pattern p = Pattern.compile("<td>(\\S+)</td>");
+                        Matcher m = p.matcher(line);
+                        if (m.find()) {
+                            KeyValue keyValue = new KeyValue(key, m.group(1));
+                            keyValues.add(keyValue);
+                            return keyValues;
+                        }
+                    }
+                }
+            }
+        }
+        rd.close();
 
-	/**
-	 * Extract a value corresponds to a given key from Query String.
-	 *
-	 * @param query Query string that holds the key value pair.
-	 * @param key   String key to be extracted.
-	 * @return Extracted string value from the query.
-	 */
-	public static String extractParamFromURIFragment(String query, String key) {
-		String fragment = query.substring(query.indexOf("#") + 1);
-		String[] params = fragment.split("&");
-		for (String param : params) {
-			String name = param.split("=")[0];
-			if (name.contains(key)) {
-				return param.split("=")[1];
-			}
-		}
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * Return a query param value of a given key, from a URI string.
-	 *
-	 * @param URIString URI as a string   .
-	 * @param key       Key that needs to be extracted.
-	 * @throws URISyntaxException
-	 * @return String value corresponds to the key.
-	 */
-	public static String getParamFromURIString(String URIString, String key) throws URISyntaxException {
+    /**
+     * Extract session consent data from response
+     *
+     * @param response
+     * @param keyPositionMap
+     * @return
+     * @throws java.io.IOException
+     */
+    public static List<KeyValue> extractSessionConsentDataFromResponse(HttpResponse response,
+                                                                       Map<String, Integer> keyPositionMap)
+            throws IOException {
 
-		String param = null;
-		List<NameValuePair> params = URLEncodedUtils.parse(new URI(URIString),
-				String.valueOf(StandardCharsets.UTF_8));
-		for (NameValuePair param1 : params) {
-			if (StringUtils.equals(param1.getName(), key)) {
-				param = param1.getValue();
-				break;
-			}
-		}
-		return param;
-	}
+        boolean lineReached = false;
+        List<KeyValue> keyValues = new ArrayList<KeyValue>();
+        BufferedReader rd =
+                new BufferedReader(new InputStreamReader(response.getEntity()
+                        .getContent()));
+        String line;
+        while ((line = rd.readLine()) != null) {
+            for (String key : keyPositionMap.keySet()) {
+                if (line.contains(key)) {
+                    lineReached = true;
+                }
+                if (lineReached) {
+                    if (line.contains("value")) {
+                        String[] tokens = line.split("\"");
+                        KeyValue keyValue = new KeyValue(key, tokens[1]);
+                        keyValues.add(keyValue);
+                        return keyValues;
+                    }
+                }
+            }
+        }
+        rd.close();
 
-	public static class KeyValue {
-		private String key;
-		private String value;
+        return null;
+    }
 
-		KeyValue(String key, String value) {
-			this.key = key;
-			this.value = value;
-		}
+    /**
+     * Extract a value corresponds to a given key from Query String.
+     *
+     * @param query Query string that holds the key value pair.
+     * @param key   String key to be extracted.
+     * @return Extracted string value from the query.
+     */
+    public static String extractParamFromURIFragment(String query, String key) {
 
-		public String getValue() {
-			return value;
-		}
+        String fragment = query.substring(query.indexOf("#") + 1);
+        String[] params = fragment.split("&");
+        for (String param : params) {
+            String name = param.split("=")[0];
+            if (name.contains(key)) {
+                return param.split("=")[1];
+            }
+        }
+        return null;
+    }
 
-		public String getKey() {
-			return key;
-		}
-	}
+    /**
+     * Return a query param value of a given key, from a URI string.
+     *
+     * @param URIString URI as a string   .
+     * @param key       Key that needs to be extracted.
+     * @return String value corresponds to the key.
+     * @throws URISyntaxException
+     */
+    public static String getParamFromURIString(String URIString, String key) throws URISyntaxException {
 
-    public static String getContentData(HttpResponse response) throws IOException{
+        String param = null;
+        List<NameValuePair> params = URLEncodedUtils.parse(new URI(URIString),
+                String.valueOf(StandardCharsets.UTF_8));
+        for (NameValuePair param1 : params) {
+            if (StringUtils.equals(param1.getName(), key)) {
+                param = param1.getValue();
+                break;
+            }
+        }
+        return param;
+    }
+
+    public static class KeyValue {
+
+        private String key;
+        private String value;
+
+        KeyValue(String key, String value) {
+
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getValue() {
+
+            return value;
+        }
+
+        public String getKey() {
+
+            return key;
+        }
+    }
+
+    public static String getContentData(HttpResponse response) throws IOException {
+
         BufferedReader rd = new BufferedReader(new InputStreamReader(response
-                                                                             .getEntity().getContent()));
+                .getEntity().getContent()));
         String line;
         StringBuffer result = new StringBuffer();
         while ((line = rd.readLine()) != null) {
