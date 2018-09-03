c8cd953515c6386ece06d85d265dc41f58d1e2ae
senthalan <senthu18>
2018-09-03 16:14:56 +0530

    Specify truststore location

--- modules/integration/tests-common/integration-test-utils/src/main/java/org/wso2/identity/integration/common/utils/ISIntegrationTest.java
+++ modules/integration/tests-common/integration-test-utils/src/main/java/org/wso2/identity/integration/common/utils/ISIntegrationTest.java
@@ -129,9 +129,9 @@ public class ISIntegrationTest {
 //    }
 
     public void setSystemproperties() {
-        URL resourceUrl = getClass().getResource(URL_SEPARATOR + "keystores" + URL_SEPARATOR
-                + "products" + URL_SEPARATOR + "wso2carbon.jks");
-        System.setProperty("javax.net.ssl.trustStore", resourceUrl.getPath());
+
+        System.setProperty("javax.net.ssl.trustStore", FrameworkPathUtil.getSystemResourceLocation() + File.separator +
+                "keystores" + File.separator  + "products" + File.separator + "wso2carbon.jks");
         System.setProperty("javax.net.ssl.trustStorePassword",
                 "wso2carbon");
         System.setProperty("javax.net.ssl.trustStoreType", "JKS");
