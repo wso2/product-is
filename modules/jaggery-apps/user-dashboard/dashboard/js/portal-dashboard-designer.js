f8e94e00488c854de27bc6327c392922a3bd26b6
dilin993 <dilin.dampahalage@gmail.com>
2018-10-08 14:27:56 +0530

    Fix https://github.com/wso2/product-is/issues/3691

--- modules/jaggery-apps/user-dashboard/dashboard/js/portal-dashboard-designer.js
+++ modules/jaggery-apps/user-dashboard/dashboard/js/portal-dashboard-designer.js
@@ -24,6 +24,10 @@ $(function() {
 
 				$.each(userWidgets, function(i, w) {
 
+                    if (w.authorized !== 'true') {
+                        return;
+                    }
+
                             var el = $('<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">' +
 								'<div id="'+w.id+'" class="layout_block gs_w" ' +
 								'data-title="'+w.title+'" data-wid="'+w.wid+'" data-url="'+w.url+'" data-prefs="'+w.prefs+'"> ' +
