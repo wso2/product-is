7da247c0bfbe34e15cb1b776d48baa8cf61d5b62
Ashen De Silva <desilvaashen.15@cse.mrt.ac.lk>
2018-11-14 09:57:39 +0530

    Frontchannel logout functionality added to playground2 app.
    PR issues fixed.
    
    PR issues fixed and log4j appender added.

--- modules/samples/oauth2/playground2/src/main/org/wso2/sample/identity/oauth2/SessionIdStore.java
+++ modules/samples/oauth2/playground2/src/main/org/wso2/sample/identity/oauth2/SessionIdStore.java
@@ -37,18 +37,25 @@ public class SessionIdStore {
     private static Map<String, HttpSession> sessionMap = new HashMap<>();
 
     public static void storeSession(String sid, HttpSession session) {
+
         log.info("Storing session: " + session.getId() + " against the sid: " + sid);
         sessionMap.put(sid, session);
     }
 
     public static String getSid(String idToken) throws ParseException {
+
         String sid = (String) SignedJWT.parse(idToken).getJWTClaimsSet().getClaim("sid");
         return idToken != null ? sid : null;
     }
 
     public static HttpSession getSession(String sid) {
+
         log.info("Retrieving session: " + sessionMap.get(sid).getId() + " for the sid: " + sid);
         return sessionMap.get(sid);
     }
 
+    public static void removeSession(String sid) {
+
+        sessionMap.remove(sid);
+    }
 }
--- modules/samples/oauth2/playground2/src/main/webapp/index.jsp
+++ modules/samples/oauth2/playground2/src/main/webapp/index.jsp
@@ -1,11 +1,4 @@
 <%@page import="org.wso2.sample.identity.oauth2.OAuth2Constants"%>
-<%--<%--%>
-
-    <%--session.removeAttribute(OAuth2Constants.OAUTH2_GRANT_TYPE);--%>
-    <%--session.removeAttribute(OAuth2Constants.ACCESS_TOKEN);--%>
-    <%--session.removeAttribute(OAuth2Constants.CODE);--%>
-
- <%--%>--%>
 
 <!DOCTYPE html>
 <html><head>
