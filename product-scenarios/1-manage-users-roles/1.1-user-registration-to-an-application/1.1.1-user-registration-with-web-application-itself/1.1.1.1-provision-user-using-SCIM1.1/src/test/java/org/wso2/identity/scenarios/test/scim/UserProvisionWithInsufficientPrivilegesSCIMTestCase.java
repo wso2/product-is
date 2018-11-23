20901f53d9f942fb4b9d7b7c2230e9842feb3a40
shavantha <shavantha@gmail.com>
2018-11-23 17:57:22 +0530

    moved the first method as a private method to be called from the init

--- product-scenarios/1-manage-users-roles/1.1-user-registration-to-an-application/1.1.1-user-registration-with-web-application-itself/1.1.1.1-provision-user-using-SCIM1.1/src/test/java/org/wso2/identity/scenarios/test/scim/UserProvisionWithInsufficientPrivilegesSCIMTestCase.java
+++ product-scenarios/1-manage-users-roles/1.1-user-registration-to-an-application/1.1.1-user-registration-with-web-application-itself/1.1.1.1-provision-user-using-SCIM1.1/src/test/java/org/wso2/identity/scenarios/test/scim/UserProvisionWithInsufficientPrivilegesSCIMTestCase.java
@@ -55,10 +55,10 @@ public class UserProvisionWithInsufficientPrivilegesSCIMTestCase extends Scenari
         client = HttpClients.createDefault();
         super.init();
         scimUsersEndpoint = backendURL + SCIMConstants.SCIM_ENDPOINT + SEPERATOR + Constants.SCIM_ENDPOINT_USER;
+        testSCIMCreateFirstUser();
     }
 
-    @Test
-    public void testSCIMCreateFirstUser() throws Exception {
+    private void testSCIMCreateFirstUser() throws Exception {
 
         JSONObject userObject = new JSONObject();
         JSONArray schemas = new JSONArray();
@@ -71,11 +71,9 @@ public class UserProvisionWithInsufficientPrivilegesSCIMTestCase extends Scenari
 
         response = SCIMProvisioningUtil.provisionUserSCIM(backendURL, userObject, Constants.SCIM1_USERS_ENDPOINT, ADMIN_USERNAME, ADMIN_PASSWORD);
         assertEquals(response.getStatusLine().getStatusCode(), HttpStatus.SC_CREATED, "User has not been created successfully");
-
-
     }
 
-    @Test(description = "1.1.1.1.11", dependsOnMethods = "testSCIMCreateFirstUser")
+    @Test(description = "1.1.1.1.11")
     public void testSCIMCreateSecondUser() throws Exception {
 
         JSONObject rootObject = new JSONObject();
@@ -103,7 +101,7 @@ public class UserProvisionWithInsufficientPrivilegesSCIMTestCase extends Scenari
         assertEquals(response.getStatusLine().getStatusCode(), HttpStatus.SC_OK, "User has not been deleted successfully");
 
     }
-    
+
     private Header getFaultyAuthzHeader() {
 
         return new BasicHeader(HttpHeaders.AUTHORIZATION, constructBasicAuthzHeader(SCIMConstants.USERNAME, SCIMConstants.PASSWORD));
