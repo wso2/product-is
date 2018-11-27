a370484b62c60a0dba57f70ae6ebe74c7cb07d8b
tharindu1992 <41312286+tharindu1992@users.noreply.github.com>
2018-11-27 13:55:23 +0530

    Update test.sh

--- product-scenarios/test.sh
+++ product-scenarios/test.sh
@@ -95,4 +95,5 @@ mvn clean install -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf
 
 echo "Copying surefire-reports to ${OUTPUT_DIR}"
 
+mkdir -p ${OUTPUT_DIR}
 find ./* -name "surefire-reports" -exec cp --parents -r {} ${OUTPUT_DIR} \;
