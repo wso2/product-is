bef840b0c59dbd4e08cc93fd16c9606aa39f39a4
shavantha <shavantha@gmail.com>
2018-12-03 12:07:32 +0530

    removed duplicate code related to json object

--- product-scenarios/1-manage-users-roles/1.1-user-registration-to-an-application/1.1.1-user-registration-with-web-application-itself/1.1.1.1-provision-user-using-SCIM1.1/src/test/java/org/wso2/identity/scenarios/test/scim/UserProvisionWithInsufficientPrivilegesSCIMTestCase.java
+++ product-scenarios/1-manage-users-roles/1.1-user-registration-to-an-application/1.1.1-user-registration-with-web-application-itself/1.1.1.1-provision-user-using-SCIM1.1/src/test/java/org/wso2/identity/scenarios/test/scim/UserProvisionWithInsufficientPrivilegesSCIMTestCase.java
@@ -47,13 +47,13 @@ public class UserProvisionWithInsufficientPrivilegesSCIMTestCase extends Scenari
     private String newPass = "newpassword";
     private String userId;
     HttpResponse response;
+    JSONObject rootObject;
 
     @BeforeClass(alwaysRun = true)
     public void testInit() throws Exception {
 
         setKeyStoreProperties();
         client = HttpClients.createDefault();
-
         super.init();
         scimUsersEndpoint = backendURL + SEPERATOR +  Constants.SCIMEndpoints.SCIM1_ENDPOINT + SEPERATOR + Constants.SCIMEndpoints.SCIM_ENDPOINT_USER;
         testSCIMCreateFirstUser();
@@ -61,28 +61,23 @@ public class UserProvisionWithInsufficientPrivilegesSCIMTestCase extends Scenari
 
     private void testSCIMCreateFirstUser() throws Exception {
 
-        JSONObject userObject = new JSONObject();
-        JSONArray schemas = new JSONArray();
-        userObject.put(SCIMConstants.SCHEMAS_ATTRIBUTE, schemas);
-        JSONObject names = new JSONObject();
-        names.put(SCIMConstants.GIVEN_NAME_ATTRIBUTE, SCIMConstants.GIVEN_NAME_CLAIM_VALUE);
-        userObject.put(SCIMConstants.NAME_ATTRIBUTE, names);
-        userObject.put(SCIMConstants.USER_NAME_ATTRIBUTE, SCIMConstants.USERNAME);
-        userObject.put(SCIMConstants.PASSWORD_ATTRIBUTE, SCIMConstants.PASSWORD);
-
-        response = SCIMProvisioningUtil.provisionUserSCIM(backendURL, userObject, Constants.SCIMEndpoints.SCIM1_ENDPOINT, Constants.SCIMEndpoints.SCIM_ENDPOINT_USER , ADMIN_USERNAME, ADMIN_PASSWORD);
-        assertEquals(response.getStatusLine().getStatusCode(), HttpStatus.SC_CREATED, "User has not been created successfully");
-    }
-
-    @Test(description = "1.1.2.1.1.11")
-    public void testSCIMCreateSecondUser() throws Exception {
-
-        JSONObject rootObject = new JSONObject();
+        rootObject = new JSONObject();
         JSONArray schemas = new JSONArray();
         rootObject.put(SCIMConstants.SCHEMAS_ATTRIBUTE, schemas);
         JSONObject names = new JSONObject();
         names.put(SCIMConstants.GIVEN_NAME_ATTRIBUTE, SCIMConstants.GIVEN_NAME_CLAIM_VALUE);
         rootObject.put(SCIMConstants.NAME_ATTRIBUTE, names);
+        rootObject.put(SCIMConstants.USER_NAME_ATTRIBUTE, SCIMConstants.USERNAME);
+        rootObject.put(SCIMConstants.PASSWORD_ATTRIBUTE, SCIMConstants.PASSWORD);
+
+        response = SCIMProvisioningUtil.provisionUserSCIM(backendURL, rootObject, Constants.SCIMEndpoints.SCIM1_ENDPOINT, Constants.SCIMEndpoints.SCIM_ENDPOINT_USER , ADMIN_USERNAME, ADMIN_PASSWORD);
+        assertEquals(response.getStatusLine().getStatusCode(), HttpStatus.SC_OK, "User has not been created successfully");
+    }
+
+    @Test(description = "1.1.2.1.1.11")
+    public void testSCIMCreateSecondUser() throws Exception {
+
+
         rootObject.put(SCIMConstants.USER_NAME_ATTRIBUTE, newUser);
         rootObject.put(SCIMConstants.PASSWORD_ATTRIBUTE, newPass);
 
