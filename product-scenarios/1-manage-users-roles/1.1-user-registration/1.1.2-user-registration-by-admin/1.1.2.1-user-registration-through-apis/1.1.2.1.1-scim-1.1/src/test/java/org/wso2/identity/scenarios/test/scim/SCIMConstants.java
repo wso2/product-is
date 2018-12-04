a35a2a555fdebb3cf6a708afafe22802bf594b61
shavantha <shavantha@gmail.com>
2018-12-04 17:13:45 +0530

    adding te scimconstant class

--- product-scenarios/1-manage-users-roles/1.1-user-registration/1.1.2-user-registration-by-admin/1.1.2.1-user-registration-through-apis/1.1.2.1.1-scim-1.1/src/test/java/org/wso2/identity/scenarios/test/scim/SCIMConstants.java
+++ product-scenarios/1-manage-users-roles/1.1-user-registration/1.1.2-user-registration-by-admin/1.1.2.1-user-registration-through-apis/1.1.2.1.1-scim-1.1/src/test/java/org/wso2/identity/scenarios/test/scim/SCIMConstants.java
@@ -20,14 +20,30 @@ package org.wso2.identity.scenarios.test.scim;
 
 public class SCIMConstants {
 
-    static final String SCIM_ENDPOINT = "/wso2/scim";
-    static final String SCHEMAS_ATTRIBUTE = "schemas";
-    static final String GIVEN_NAME_ATTRIBUTE = "givenName";
-    static final String NAME_ATTRIBUTE = "name";
-    static final String USER_NAME_ATTRIBUTE = "userName";
-    static final String PASSWORD_ATTRIBUTE = "password";
-    static final String ID_ATTRIBUTE = "id";
-    static final String GIVEN_NAME_CLAIM_VALUE = "user1";
-    static final String USERNAME = "scim1user";
-    static final String PASSWORD = "scim1pwd";
+    public static final String SCIM_ENDPOINT = "/wso2/scim";
+    public static final String SCHEMAS_ATTRIBUTE = "schemas";
+    public static final String GIVEN_NAME_ATTRIBUTE = "givenName";
+    public static final String NAME_ATTRIBUTE = "name";
+    public static final String USER_NAME_ATTRIBUTE = "userName";
+    public static final String PASSWORD_ATTRIBUTE = "password";
+    public static final String ID_ATTRIBUTE = "id";
+    public static final String GIVEN_NAME_CLAIM_VALUE = "user1";
+    public static final String USERNAME = "scim1user";
+    public static final String PASSWORD = "scim1pwd";
+    public static final String SCIM2USER = "scim2user";
+    public static final String SCIM2PASSWORD = "scim2pwd";
+    public static final String DISPLAY_NAME = "displayName";
+    public static final String DISPLAY = "display";
+    public static final String MEMBERS = "members";
+    public static final String VALUE = "value";
+    public static final String ROLE_NAME = "TestRole";
+    public static final String FAMILY_NAME_ATTRIBUTE = "familyName";
+    public static final String FAMILY_NAME_CLAIM_VALUE = "scim2";
+    public static final String TYPE_PARAM = "type";
+    public static final String EMAIL_TYPE_WORK_ATTRIBUTE = "work";
+    public static final String EMAIL_TYPE_HOME_ATTRIBUTE = "home";
+    public static final String VALUE_PARAM = "value";
+    public static final String PRIMARY_PARAM = "primary";
+    public static final String EMAILS_ATTRIBUTE = "emails";
+    public static final String ERROR_SCHEMA = "urn:ietf:params:scim:api:messages:2.0:Error";
 }
