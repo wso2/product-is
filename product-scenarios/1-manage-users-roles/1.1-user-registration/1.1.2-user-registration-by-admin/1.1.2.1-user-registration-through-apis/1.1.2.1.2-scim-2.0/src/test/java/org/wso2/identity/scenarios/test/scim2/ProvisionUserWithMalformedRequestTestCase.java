625c6328945dfe57e91f0a5ffa7a08f3767e8a62
shavantha <shavantha@gmail.com>
2018-12-05 15:06:26 +0530

    updated the SCHEMA_ERROR assertion

--- product-scenarios/1-manage-users-roles/1.1-user-registration/1.1.2-user-registration-by-admin/1.1.2.1-user-registration-through-apis/1.1.2.1.2-scim-2.0/src/test/java/org/wso2/identity/scenarios/test/scim2/ProvisionUserWithMalformedRequestTestCase.java
+++ product-scenarios/1-manage-users-roles/1.1-user-registration/1.1.2-user-registration-by-admin/1.1.2.1-user-registration-through-apis/1.1.2.1.2-scim-2.0/src/test/java/org/wso2/identity/scenarios/test/scim2/ProvisionUserWithMalformedRequestTestCase.java
@@ -80,7 +80,8 @@ public class ProvisionUserWithMalformedRequestTestCase extends ScenarioTestBase 
         JSONArray schemasArray = new JSONArray();
         schemasArray.add(responseObj);
 
-        assertEquals(((JSONObject) responseObj).get("schemas"), SCIMConstants.ERROR_SCHEMA);
+        assertEquals(((JSONObject) responseObj).get("schemas"), SCIMConstants.ERROR_SCHEMA,"Expected ERROR_SCHEMA not" +
+                " returned");
     }
 
 }
