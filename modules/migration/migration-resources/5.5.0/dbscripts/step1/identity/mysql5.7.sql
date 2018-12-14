c1ed3875b5d0144b6515856378ce7952f89401aa
omindu <omindu@wso2.com>
2018-12-14 11:39:52 +0530

    Fix wso2/product-is#4191: Illegal mix of collations errors when dropping indexes from a mysql stored procedure

--- modules/migration/migration-resources/5.5.0/dbscripts/step1/identity/mysql5.7.sql
+++ modules/migration/migration-resources/5.5.0/dbscripts/step1/identity/mysql5.7.sql
@@ -1,7 +1,7 @@
 DROP INDEX IDX_AT ON IDN_OAUTH2_ACCESS_TOKEN;
 DROP PROCEDURE IF EXISTS drop_index_if_exists;
-CREATE PROCEDURE drop_index_if_exists(in theTable varchar(128), in theIndexName varchar(128) ) BEGIN IF((SELECT COUNT(*) AS index_exists FROM information_schema.statistics WHERE TABLE_SCHEMA = DATABASE() and table_name = theTable AND index_name = theIndexName) > 0) THEN SET @s = CONCAT('DROP INDEX ' , theIndexName , ' ON ' , theTable); PREPARE stmt FROM @s; EXECUTE stmt; END IF; END;
-CALL drop_index_if_exists("IDN_OAUTH2_AUTHORIZATION_CODE", "IDX_AUTHORIZATION_CODE");
+CREATE PROCEDURE drop_index_if_exists() BEGIN IF((SELECT COUNT(*) AS index_exists FROM information_schema.statistics WHERE TABLE_SCHEMA = DATABASE() and table_name = 'IDN_OAUTH2_AUTHORIZATION_CODE' AND index_name = 'IDX_AUTHORIZATION_CODE') > 0) THEN SET @s = CONCAT('DROP INDEX ' , 'IDX_AUTHORIZATION_CODE' , ' ON ' , 'IDN_OAUTH2_AUTHORIZATION_CODE'); PREPARE stmt FROM @s; EXECUTE stmt; END IF; END;
+CALL drop_index_if_exists();
 
 ALTER TABLE IDN_OAUTH2_ACCESS_TOKEN modify REFRESH_TOKEN VARCHAR(2048);
 ALTER TABLE IDN_OAUTH2_ACCESS_TOKEN modify ACCESS_TOKEN VARCHAR(2048);
