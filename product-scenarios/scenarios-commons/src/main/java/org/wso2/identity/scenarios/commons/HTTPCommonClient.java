a5743e38ba976dc3df5b2597ec0756a2abd90dbf
thanujalk <thanuja@wso2.com>
2019-02-05 19:05:30 +0530

    Adding '2.1 User self registration via end user portal' test case.

--- product-scenarios/scenarios-commons/src/main/java/org/wso2/identity/scenarios/commons/HTTPCommonClient.java
+++ product-scenarios/scenarios-commons/src/main/java/org/wso2/identity/scenarios/commons/HTTPCommonClient.java
@@ -16,6 +16,7 @@
 
 package org.wso2.identity.scenarios.commons;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpDelete;
@@ -27,8 +28,11 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.util.Map;
 
@@ -132,5 +136,30 @@ public class HTTPCommonClient {
     public void closeHttpClient() throws IOException {
 
         this.client.close();
+    }
+
+    /**
+     * Read JSON payload from the response.
+     *
+     * @param response HttpResponse with data.
+     * @return JSON parsed payload.
+     * @throws IOException If error occurs while parsing JSON payload.
+     */
+    public JSONObject getJSONFromResponse(HttpResponse response) throws IOException {
+
+        BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+        return (JSONObject) JSONValue.parse(rd);
+    }
+
+    /**
+     * Read String payload from the response.
+     *
+     * @param response HttpResponse with data.
+     * @return String payload.
+     * @throws IOException If error occurs while building string payload.
+     */
+    public String getStringFromResponse(HttpResponse response) throws IOException {
+
+        return IOUtils.toString(new InputStreamReader(response.getEntity().getContent()));
     }
 }
