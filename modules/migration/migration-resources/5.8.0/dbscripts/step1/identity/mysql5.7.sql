2fee619dd31493b25f83f56493ccffa2c7c4a786
tharindu-bandara <bandara14@live.com>
2019-04-30 17:38:43 +0530

    Move uma index

--- modules/migration/migration-resources/5.8.0/dbscripts/step1/identity/mysql5.7.sql
+++ modules/migration/migration-resources/5.8.0/dbscripts/step1/identity/mysql5.7.sql
@@ -6,7 +6,6 @@ ALTER TABLE IDN_OAUTH1A_REQUEST_TOKEN MODIFY CALLBACK_URL VARCHAR(2048);
 
 ALTER TABLE IDN_OAUTH2_AUTHORIZATION_CODE MODIFY CALLBACK_URL VARCHAR(2048);
 
-CREATE INDEX IDX_USER_RID ON IDN_UMA_RESOURCE (RESOURCE_ID, RESOURCE_OWNER_NAME, USER_DOMAIN, CLIENT_ID);
 DROP PROCEDURE IF EXISTS skip_index_if_exists;
 
 CREATE PROCEDURE skip_index_if_exists(indexName varchar(64),tableName varchar(64), tableColumns varchar(64)) BEGIN  IF((SELECT COUNT(*) AS index_exists FROM information_schema.statistics WHERE TABLE_SCHEMA = DATABASE() and table_name = tableName AND index_name = indexName) = 0) THEN SET @s = CONCAT('CREATE INDEX ' , indexName , ' ON ' , tableName, tableColumns); PREPARE stmt FROM @s; EXECUTE stmt; END IF; END;
