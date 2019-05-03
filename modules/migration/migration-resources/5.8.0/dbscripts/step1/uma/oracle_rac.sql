351b7cb7e9ff9d05f58bcb3dec9dbb7d25b169b0
tharindu-bandara <bandara14@live.com>
2019-05-03 12:52:33 +0530

    Add migration script changes

--- modules/migration/migration-resources/5.8.0/dbscripts/step1/uma/oracle_rac.sql
+++ modules/migration/migration-resources/5.8.0/dbscripts/step1/uma/oracle_rac.sql
@@ -1,2 +1,20 @@
-CREATE INDEX IDX_USER_RID ON IDN_UMA_RESOURCE (RESOURCE_ID, RESOURCE_OWNER_NAME, USER_DOMAIN, CLIENT_ID)
-/
+CREATE OR REPLACE PROCEDURE skip_index_if_exists (query IN VARCHAR2)
+IS
+BEGIN
+declare
+  already_exists  exception;
+  columns_indexed exception;
+  pragma exception_init( already_exists, -955 );
+  pragma exception_init(columns_indexed, -1408);
+begin
+  execute immediate query;
+  dbms_output.put_line( 'created' );
+exception
+  when already_exists or columns_indexed then
+  dbms_output.put_line( 'skipped' );
+end;
+END;
+
+BEGIN
+skip_index_if_exists('CREATE INDEX IDX_USER_RID ON IDN_UMA_RESOURCE (RESOURCE_ID, RESOURCE_OWNER_NAME, USER_DOMAIN, CLIENT_ID)')
+END;
