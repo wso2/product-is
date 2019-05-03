f815eae67f0afc2121b6002d71527d3ccf29c35e
DMHP <hasanthidisanayakka@yahoo.com>
2019-05-03 18:46:21 +0530

    Add temporary logs for the debugging purpose

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/provisioning/JustInTimeProvisioningTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/provisioning/JustInTimeProvisioningTestCase.java
@@ -33,6 +33,7 @@ import org.wso2.carbon.integration.common.utils.mgt.ServerConfigurationManager;
 import org.wso2.carbon.um.ws.api.stub.RemoteUserStoreManagerServiceUserStoreExceptionException;
 import org.wso2.carbon.user.core.UserCoreConstants;
 import org.wso2.identity.integration.common.clients.UserManagementClient;
+import org.wso2.identity.integration.common.clients.authorization.mgt.RemoteAuthorizationManagerServiceClient;
 import org.wso2.identity.integration.common.clients.user.store.config.UserStoreConfigAdminServiceClient;
 import org.wso2.identity.integration.common.clients.usermgt.remote.RemoteUserStoreManagerServiceClient;
 import org.wso2.identity.integration.common.utils.UserStoreConfigUtils;
@@ -63,6 +64,26 @@ public class JustInTimeProvisioningTestCase extends SAMLIdentityFederationTestCa
             userMgtClient = new UserManagementClient(backendURL, getSessionCookie());
             userStoreClient = new RemoteUserStoreManagerServiceClient(getBackendURL(), getSessionCookie());
             userStoreConfigAdminServiceClient = new UserStoreConfigAdminServiceClient(getBackendURL(), sessionCookie);
+            RemoteAuthorizationManagerServiceClient remoteAuthorizationManagerServiceClient =
+                    new RemoteAuthorizationManagerServiceClient(getBackendURL(), getSessionCookie());
+
+            log.info("login user name : " + isServer.getSuperTenant().getTenantAdmin().getUserName());
+            log.info("login password : " + isServer.getSuperTenant().getTenantAdmin().getPassword());
+            for (String role : userStoreClient.getRoleListOfUser(isServer.getSuperTenant().getTenantAdmin().getUserName())) {
+                boolean roleAuthorized = remoteAuthorizationManagerServiceClient.isRoleAuthorized(role,
+                        "/permission/admin/manage/identity/rolemgt/create", "ui.execute");
+                log.info("Role Authorization :" + roleAuthorized + " role: " + role);
+            }
+
+            boolean isUserAuthorized = remoteAuthorizationManagerServiceClient.isUserAuthorized(isServer.getSuperTenant().getTenantAdmin()
+                    .getUserName(), "/permission/admin/manage/identity/rolemgt/create", "ui.execute");
+            log.info("User Authorization :" + isUserAuthorized);
+            remoteAuthorizationManagerServiceClient.authorizeUser(isServer.getSuperTenant().getTenantAdmin().
+                    getUserName(), "/permission/admin/", "ui.execute");
+            boolean isAuthorized = remoteAuthorizationManagerServiceClient.isUserAuthorized(isServer.getSuperTenant().getTenantAdmin()
+                    .getUserName(), "/permission/admin/manage/identity/rolemgt/create", "ui.execute");
+            log.info("User Authorization details :" + isAuthorized);
+
             userMgtClient.addInternalRole("loginJIT", null, new String[]{"/permission/admin/login"});
             addSecondaryUserStore();
 
