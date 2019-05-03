462c8e34c277690d4c0c8bfeba1ac2758417cb60
tharindu-bandara <bandara14@live.com>
2019-05-03 12:52:33 +0530

    Add migration script changes

--- modules/migration/migration-resources/5.8.0/dbscripts/step1/uma/postgresql.sql
+++ modules/migration/migration-resources/5.8.0/dbscripts/step1/uma/postgresql.sql
@@ -1,1 +1,12 @@
-CREATE INDEX IDX_USER_RID ON IDN_UMA_RESOURCE (RESOURCE_ID, RESOURCE_OWNER_NAME, USER_DOMAIN, CLIENT_ID);
+CREATE OR REPLACE FUNCTION skip_index_if_exists(indexName varchar(64),tableName varchar(64), tableColumns varchar(64))
+  RETURNS void AS $$
+  declare s varchar(1000);
+  begin
+	  if to_regclass(indexName) IS NULL  then
+	    s :=  CONCAT('CREATE INDEX ' , indexName , ' ON ' , tableName, tableColumns);
+	    execute s;
+	  end if;
+  END;
+$$ LANGUAGE plpgsql;
+
+SELECT skip_index_if_exists('IDX_USER_RID', 'IDN_UMA_RESOURCE', '(RESOURCE_ID, RESOURCE_OWNER_NAME, USER_DOMAIN, CLIENT_ID)');
