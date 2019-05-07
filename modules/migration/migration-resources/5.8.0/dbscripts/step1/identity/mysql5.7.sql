61889c3dfd6f0f430ad7e5479c3c6845214dc031
tharindu-bandara <bandara14@live.com>
2019-05-07 15:32:14 +0530

    Add mysql 5.7 script for issues/5190

--- modules/migration/migration-resources/5.8.0/dbscripts/step1/identity/mysql5.7.sql
+++ modules/migration/migration-resources/5.8.0/dbscripts/step1/identity/mysql5.7.sql
@@ -4,6 +4,24 @@ DROP PROCEDURE IF EXISTS skip_index_if_exists;
 
 CREATE PROCEDURE skip_index_if_exists(indexName varchar(64),tableName varchar(64), tableColumns varchar(64)) BEGIN  IF((SELECT COUNT(*) AS index_exists FROM information_schema.statistics WHERE TABLE_SCHEMA = DATABASE() and table_name = tableName AND index_name = indexName) = 0) THEN SET @s = CONCAT('CREATE INDEX ' , indexName , ' ON ' , tableName, tableColumns); PREPARE stmt FROM @s; EXECUTE stmt; END IF; END;
 
+DROP PROCEDURE IF EXISTS create_index_if_not_column_is_partly_indexed;
+
+CREATE PROCEDURE create_index_if_not_column_is_partly_indexed(partlyIndexedColumn varchar(64), indexName varchar(64), tableName varchar(64), columns varchar(64)) BEGIN
+
+DROP PROCEDURE IF EXISTS add_column_if_not_exists_with_default_val;
+
+CREATE PROCEDURE add_column_if_not_exists_with_default_val(table_name VARCHAR (64), column_name VARCHAR (64),data_type VARCHAR (64), default_val VARCHAR (64)) BEGIN IF EXISTS( SELECT NULL       FROM INFORMATION_SCHEMA.COLUMNS       WHERE table_name = table_name        AND column_name = column_name) THEN  SET @query = CONCAT('ALTER TABLE ', table_name, ' ADD COLUMN ', column_name, ' ', data_type, ' NOT NULL default ', default_val);  PREPARE statement FROM @query;  EXECUTE statement; END IF;END;
+
+CALL add_column_if_not_exists_with_default_val('IDN_OAUTH2_AUTHORIZATION_CODE', 'IDP_ID', 'int', '-1');
+
+CALL add_column_if_not_exists_with_default_val('IDN_OAUTH2_ACCESS_TOKEN', 'IDP_ID', 'INT', '-1');
+
+CALL add_column_if_not_exists_with_default_val('IDN_OAUTH2_ACCESS_TOKEN_AUDIT', 'IDP_ID', 'INT', '-1');
+
+ALTER TABLE IDN_OAUTH2_ACCESS_TOKEN DROP INDEX CON_APP_KEY;
+
+ALTER TABLE IDN_OAUTH2_ACCESS_TOKEN ADD CONSTRAINT CON_APP_KEY UNIQUE (CONSUMER_KEY_ID,AUTHZ_USER,TENANT_ID,USER_DOMAIN,USER_TYPE,TOKEN_SCOPE_HASH,TOKEN_STATE,TOKEN_STATE_ID,IDP_ID);
+
 CREATE TABLE IF NOT EXISTS IDN_AUTH_USER (
 	USER_ID VARCHAR(255) NOT NULL,
 	USER_NAME VARCHAR(255) NOT NULL,
