fca29dbb8c6032fa267372380d62f336792aba09
Farasath Ahamed <farasatha@wso2.com>
2019-06-27 13:57:51 +0530

    Fix travelocity sample to be compatible with latest sso-agent component

--- modules/samples/sso/sso-agent-sample/src/main/java/org/wso2/sample/is/sso/agent/SampleContextEventListener.java
+++ modules/samples/sso/sso-agent-sample/src/main/java/org/wso2/sample/is/sso/agent/SampleContextEventListener.java
@@ -19,9 +19,9 @@ package org.wso2.sample.is.sso.agent;
 
 import org.wso2.carbon.identity.sso.agent.SSOAgentConstants;
 import org.wso2.carbon.identity.sso.agent.bean.SSOAgentConfig;
-import org.wso2.carbon.identity.sso.agent.SSOAgentException;
-import org.wso2.carbon.identity.sso.agent.saml.SSOAgentX509Credential;
-import org.wso2.carbon.identity.sso.agent.saml.SSOAgentX509KeyStoreCredential;
+import org.wso2.carbon.identity.sso.agent.exception.SSOAgentException;
+import org.wso2.carbon.identity.sso.agent.security.SSOAgentX509Credential;
+import org.wso2.carbon.identity.sso.agent.security.SSOAgentX509KeyStoreCredential;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -69,9 +69,7 @@ public class SampleContextEventListener implements ServletContextListener {
             config.getOpenId().setAttributesRequestor(new SampleAttributesRequestor());
             servletContextEvent.getServletContext().
                     setAttribute(SSOAgentConstants.CONFIG_BEAN_NAME, config);
-        } catch (IOException e){
-            LOGGER.log(Level.SEVERE, e.getMessage(), e);
-        } catch (SSOAgentException e) {
+        } catch (IOException | SSOAgentException e){
             LOGGER.log(Level.SEVERE, e.getMessage(), e);
         }
     }
