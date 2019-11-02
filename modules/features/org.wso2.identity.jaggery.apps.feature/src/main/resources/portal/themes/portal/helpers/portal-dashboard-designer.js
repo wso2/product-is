8e6017e1a3a521abbbc3f6b9590a7250a55ffbd7
piraveena <piraveena18@gmail.com>
2019-11-02 11:51:10 +0530

    Fix comments

--- modules/features/org.wso2.identity.jaggery.apps.feature/src/main/resources/portal/themes/portal/helpers/portal-dashboard-designer.js
+++ modules/features/org.wso2.identity.jaggery.apps.feature/src/main/resources/portal/themes/portal/helpers/portal-dashboard-designer.js
@@ -1,7 +1,8 @@
 var resources = function (page, meta) {
     return {
         template: 'portal-dashboard-designer.hbs',
-        js: ['portal-gadgets-js.jag','shindig.js', 'UESContainer.js', 'jquery-ui-1.12.1.custom.min.js', 'jquery.gridster.with-extras.min.js', 'portal-dashboard-designer.js', 'alert.js'],
+        js: ['portal-gadgets-js.jag','shindig.js', 'UESContainer.js', 'jquery-ui-1.12.1.custom.min.js',
+            'jquery.gridster.with-extras.min.js', 'portal-dashboard-designer.js', 'alert.js'],
         css: ['jquery.gridster.min.css', 'portal-dashboard-designer.css'],
         code: []
     };
