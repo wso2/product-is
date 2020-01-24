5e16d9b5fc6f0293fc4a1f8e7277442e228f61b5
Gayashan Bombuwala <gayashanbc@gmail.com>
2020-01-24 09:16:46 +0530

    Add test cases for updating SCIM users

---
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/base/ReadOnlyUserStoreInitializerTestCase.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.identity.integration.test.base;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.wso2.carbon.automation.engine.context.TestUserMode;
+import org.wso2.carbon.integration.common.admin.client.AuthenticatorClient;
+import org.wso2.carbon.integration.common.utils.mgt.ServerConfigurationManager;
+import org.wso2.identity.integration.common.clients.UserManagementClient;
+import org.wso2.identity.integration.common.utils.ISIntegrationTest;
+import org.wso2.identity.integration.test.util.Utils;
+import org.wso2.identity.integration.test.utils.ISTestUtils;
+
+import java.io.File;
+
+public class ReadOnlyUserStoreInitializerTestCase extends ISIntegrationTest {
+
+    private ServerConfigurationManager scm;
+    private File defaultConfigFile;
+    private UserManagementClient userMgtClient;
+    private AuthenticatorClient authenticatorClient;
+    private String newUserName = "ReadOnlyLDAPUserName";
+    private String newUserRole = "ReadOnlyLDAPUserRole";
+    private String newUserPassword = "ReadOnlyLDAPUserPass";
+
+    @BeforeTest(alwaysRun = true)
+    public void initUserStoreConfig() throws Exception {
+
+        super.init();
+        userMgtClient = new UserManagementClient(backendURL, getSessionCookie());
+        authenticatorClient = new AuthenticatorClient(backendURL);
+
+        userMgtClient.addRole(newUserRole, null, new String[]{"/permission/admin/login"});
+        userMgtClient.addUser(newUserName, newUserPassword, new String[]{newUserRole}, null);
+
+        String carbonHome = Utils.getResidentCarbonHome();
+        defaultConfigFile = getDeploymentTomlFile(carbonHome);
+        log.info("Default TOML: " + defaultConfigFile.toString());
+        File userMgtConfigFile = new File(getISResourceLocation() + File.separator + "userMgt"
+                + File.separator + "read_only_ldap_user_mgt_config.toml");
+        scm = new ServerConfigurationManager(isServer);
+        scm.applyConfigurationWithoutRestart(userMgtConfigFile, defaultConfigFile, true);
+        scm.restartGracefully();
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void resetUserStoreConfig() throws Exception {
+
+        super.init();
+        scm.restoreToLastConfiguration(true);
+        super.init(TestUserMode.SUPER_TENANT_ADMIN);
+        userMgtClient = new UserManagementClient(backendURL, getSessionCookie());
+
+        if (ISTestUtils.nameExists(userMgtClient.listAllUsers(newUserName, 10), newUserName)) {
+            userMgtClient.deleteUser(newUserName);
+        }
+
+        if (userMgtClient.roleNameExists(newUserRole)) {
+            userMgtClient.deleteRole(newUserRole);
+        }
+    }
+}
---
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/scim2/rest/api/SCIMUserUpdateReadOnlyTest.java
@@ -0,0 +1,113 @@
+package org.wso2.identity.integration.test.scim2.rest.api;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+import org.wso2.carbon.automation.engine.context.TestUserMode;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import static org.wso2.identity.integration.test.scim2.SCIM2BaseTestCase.USERS_ENDPOINT;
+
+public class SCIMUserUpdateReadOnlyTest extends SCIM2BaseTest {
+
+    private static final Log log = LogFactory.getLog(SCIMUserUpdateReadOnlyTest.class);
+
+    protected String endpointURL;
+    protected String userId = null;
+    private static final String SCIM_CONTENT_TYPE = "application/scim+json";
+
+    @Factory(dataProvider = "restAPIUserConfigProvider")
+    public SCIMUserUpdateReadOnlyTest(TestUserMode userMode) throws Exception {
+
+        super.init(userMode);
+        this.context = isServer;
+        this.authenticatingUserName = context.getContextTenant().getTenantAdmin().getUserName();
+        this.authenticatingCredential = context.getContextTenant().getTenantAdmin().getPassword();
+        this.tenant = context.getContextTenant().getDomain();
+    }
+
+    @DataProvider(name = "restAPIUserConfigProvider")
+    public static Object[][] restAPIUserConfigProvider() {
+
+        return new Object[][]{
+                {TestUserMode.SUPER_TENANT_ADMIN},
+                {TestUserMode.TENANT_ADMIN}
+        };
+    }
+
+    @BeforeClass(alwaysRun = true)
+    public void init() throws Exception {
+
+        super.testInit(swaggerDefinition, tenant);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void testFinish() {
+
+        super.conclude();
+    }
+
+    @BeforeClass(alwaysRun = true)
+    public void testInit() {
+
+        RestAssured.basePath = basePath;
+        super.testInit();
+    }
+
+    @Test
+    public void testGetUsers() {
+
+        endpointURL = USERS_ENDPOINT;
+        Response response = getResponseOfGet(endpointURL, SCIM_CONTENT_TYPE);
+        ExtractableResponse<Response> extractableResponse = response.then()
+                .log().ifValidationFails()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .and()
+                .assertThat()
+                .header(HttpHeaders.CONTENT_TYPE, SCIM_CONTENT_TYPE)
+                .extract();
+        Assert.assertNotNull(extractableResponse);
+
+        Object resourcesAttribute = extractableResponse.path("Resources");
+        userId = (String) ((LinkedHashMap) ((ArrayList) resourcesAttribute).get(0)).get("id");
+        endpointURL = USERS_ENDPOINT + "/" + userId;
+    }
+
+    /*
+     TODO: 1/22/20 Enable the below test once the following issues have been resolved.
+     https://github.com/wso2/product-is/issues/7341
+     https://github.com/wso2/product-is/issues/7342
+    @Test(dependsOnMethods = "testGetUsers")
+    public void testPatchUserIdentityClaim() throws IOException {
+
+        String body = readResource("scim2-patch-replace-identity-claim.json");
+        Response response = getResponseOfPatch(endpointURL, body, SCIM_CONTENT_TYPE);
+        ExtractableResponse extractableResponse = response.then()
+                .log().ifValidationFails()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED)
+                .header(HttpHeaders.LOCATION, notNullValue())
+                .and()
+                .assertThat()
+                .header(HttpHeaders.CONTENT_TYPE, SCIM_CONTENT_TYPE)
+                .extract();
+
+        Assert.assertNotNull(extractableResponse);
+        SCIMUtils.validateSchemasAttribute(extractableResponse.path("schemas"));
+        SCIMUtils.validateMetaAttribute(extractableResponse.path("meta"), response, endpointURL);
+    }
+    */
+}
