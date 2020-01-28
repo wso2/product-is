9ee3521115fcfb59fa0c606572fc5cc07e3d234e
DMHP <hasanthidisanayakka@yahoo.com>
2020-01-28 17:15:41 +0530

    Add test cases for https://github.com/wso2/product-is/issues/7313

--- modules/integration/tests-common/integration-test-utils/src/main/java/org/wso2/identity/integration/common/utils/UserStoreConfigUtils.java
+++ modules/integration/tests-common/integration-test-utils/src/main/java/org/wso2/identity/integration/common/utils/UserStoreConfigUtils.java
@@ -17,13 +17,25 @@
 */
 package org.wso2.identity.integration.common.utils;
 
+import org.wso2.carbon.automation.test.utils.dbutils.H2DataBaseManager;
+import org.wso2.carbon.identity.user.store.configuration.stub.dto.PropertyDTO;
 import org.wso2.carbon.identity.user.store.configuration.stub.dto.UserStoreDTO;
+import org.wso2.carbon.integration.common.utils.mgt.ServerConfigurationManager;
 import org.wso2.identity.integration.common.clients.user.store.config.UserStoreConfigAdminServiceClient;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * The Util class which carries common functionality required by the user store configuration scenarios
  */
 public class UserStoreConfigUtils {
+
+    private static PropertyDTO[] propertyDTOs;
+    private static final String userStoreDBName = "JDBC_USER_STORE_DB";
+    private static final String dbUserName = "wso2automation";
+    private static final String dbUserPassword = "wso2automation";
 
     public boolean waitForUserStoreDeployment(UserStoreConfigAdminServiceClient userStoreConfigAdminServiceClient,
                                               String domain) throws Exception {
@@ -63,5 +75,62 @@ public class UserStoreConfigUtils {
             }
         }
         return true;
+    }
+
+    /**
+     * Sets properties for the JDBC Userstore.
+     * @throws IOException throws if an error occurs when setting JDBC userstore properties.
+     * @throws SQLException throws if an error occurs when perfomring a db action.
+     * @throws ClassNotFoundException throws if it fails to find the relevant java class.
+     */
+    public PropertyDTO[] getJDBCUserStoreProperties() throws IOException, SQLException, ClassNotFoundException {
+
+        propertyDTOs = new PropertyDTO[11];
+        for (int i = 0; i < 11; i++) {
+            propertyDTOs[i] = new PropertyDTO();
+        }
+        //creating database
+        H2DataBaseManager dbmanager = new H2DataBaseManager("jdbc:h2:" + ServerConfigurationManager.getCarbonHome()
+                + "/repository/database/" + userStoreDBName,
+                dbUserName, dbUserPassword);
+        dbmanager.executeUpdate(new File(ServerConfigurationManager.getCarbonHome() + "/dbscripts/h2.sql"));
+        dbmanager.disconnect();
+
+        propertyDTOs[0].setName("driverName");
+        propertyDTOs[0].setValue("org.h2.Driver");
+
+        propertyDTOs[1].setName("url");
+        propertyDTOs[1].setValue("jdbc:h2:./repository/database/" + userStoreDBName);
+
+        propertyDTOs[2].setName("userName");
+        propertyDTOs[2].setValue(dbUserName);
+
+        propertyDTOs[3].setName("password");
+        propertyDTOs[3].setValue(dbUserPassword);
+
+        propertyDTOs[4].setName("PasswordJavaRegEx");
+        propertyDTOs[4].setValue("^[\\S]{5,30}$");
+
+        propertyDTOs[5].setName("UsernameJavaRegEx");
+        propertyDTOs[5].setValue("^[\\S]{5,30}$");
+
+        propertyDTOs[6].setName("Disabled");
+        propertyDTOs[6].setValue("false");
+
+        propertyDTOs[7].setName("PasswordDigest");
+        propertyDTOs[7].setValue("SHA-256");
+
+        propertyDTOs[8].setName("StoreSaltedPassword");
+        propertyDTOs[8].setValue("true");
+
+        propertyDTOs[9].setName("SCIMEnabled");
+        propertyDTOs[9].setValue("true");
+
+        propertyDTOs[9].setName("CountRetrieverClass");
+        propertyDTOs[9].setValue("org.wso2.carbon.identity.user.store.count.jdbc.JDBCUserStoreCountRetriever");
+
+        propertyDTOs[10].setName("UserIDEnabled");
+        propertyDTOs[10].setValue("true");
+        return propertyDTOs;
     }
 }
