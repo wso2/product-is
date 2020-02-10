a9013684b51254e77fbfe22192c7ae65f6bd8ecb
JKAUSHALYA <jayangak@wso2.com>
2020-02-10 17:05:02 +0530

    Improve code quality in the tests.

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/user/mgt/uuid/JDBCUUIDUMTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/user/mgt/uuid/JDBCUUIDUMTestCase.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.identity.integration.test.user.mgt.uuid;
 
 import org.testng.annotations.AfterClass;
@@ -188,6 +204,7 @@ public class JDBCUUIDUMTestCase extends AbstractUUIDUMTestCase {
     @Test(dependsOnMethods = "testAddUser")
     public void testGetUserListConditionWithID() throws Exception {
 
+        super.testGetUserListConditionWithID();
     }
 
     @Test(dependsOnMethods = "testAddUser")
@@ -217,7 +234,7 @@ public class JDBCUUIDUMTestCase extends AbstractUUIDUMTestCase {
     @AfterClass
     public void deInit() throws IOException, AutomationUtilException {
 
-        scm.restoreToLastConfiguration();
+        scm.restoreToLastConfiguration(false);
     }
 
 }
