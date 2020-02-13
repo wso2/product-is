555c660e41909876cd409920a7d5e31a0b0f7548
JKAUSHALYA <jayangak@wso2.com>
2020-02-13 14:00:19 +0530

    Improve code quality.

--- modules/integration/tests-common/admin-services/src/main/java/org/wso2/carbon/identity/test/integration/service/UUIDUserStoreManagerService.java
+++ modules/integration/tests-common/admin-services/src/main/java/org/wso2/carbon/identity/test/integration/service/UUIDUserStoreManagerService.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.carbon.identity.test.integration.service;
 
 import org.apache.commons.logging.Log;
@@ -36,6 +52,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Admin service to call methods using unique user id. This service currently only used for integration tests.
+ */
 public class UUIDUserStoreManagerService {
 
     private static final Log log = LogFactory.getLog(UUIDUserStoreManagerService.class);
