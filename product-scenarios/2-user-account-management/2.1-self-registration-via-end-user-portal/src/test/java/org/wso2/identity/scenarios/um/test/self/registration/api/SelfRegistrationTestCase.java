3cb844b304ec6d830bff325f4afbe1da0370367f
sachiniWettasinghe <swettasinghe23@gmail.com>
2020-10-07 11:18:00 +0530

    FIx intermittent test failure in SelfRegistrationTestCase

--- product-scenarios/2-user-account-management/2.1-self-registration-via-end-user-portal/src/test/java/org/wso2/identity/scenarios/um/test/self/registration/api/SelfRegistrationTestCase.java
+++ product-scenarios/2-user-account-management/2.1-self-registration-via-end-user-portal/src/test/java/org/wso2/identity/scenarios/um/test/self/registration/api/SelfRegistrationTestCase.java
@@ -207,7 +207,7 @@ public class SelfRegistrationTestCase extends ScenarioTestBase {
             }
         }
         // To sync the local caches.
-        Thread.sleep(5000);
+        Thread.sleep(10000);
     }
 
     @Test(description = "2.1.2",
