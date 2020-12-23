e4112fc17c5edf867f4a028ccb9c5467cc4224e2
ashendes <desilvaashen.15@cse.mrt.ac.lk>
2020-12-23 11:45:55 +0530

    Config check and server restarts eliminated.

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/identityServlet/ExtendSessionEndpointAuthCodeGrantTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/identityServlet/ExtendSessionEndpointAuthCodeGrantTestCase.java
@@ -1,20 +1,44 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.identity.integration.test.identityServlet;
 
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import com.nimbusds.oauth2.sdk.AuthorizationCode;
 import com.nimbusds.oauth2.sdk.AuthorizationCodeGrant;
+import com.nimbusds.oauth2.sdk.AuthorizationGrant;
+import com.nimbusds.oauth2.sdk.RefreshTokenGrant;
+import com.nimbusds.oauth2.sdk.Scope;
 import com.nimbusds.oauth2.sdk.TokenErrorResponse;
 import com.nimbusds.oauth2.sdk.TokenRequest;
 import com.nimbusds.oauth2.sdk.TokenResponse;
+import com.nimbusds.oauth2.sdk.auth.ClientAuthentication;
 import com.nimbusds.oauth2.sdk.auth.ClientSecretBasic;
 import com.nimbusds.oauth2.sdk.auth.Secret;
 import com.nimbusds.oauth2.sdk.http.HTTPResponse;
 import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.token.RefreshToken;
 import com.nimbusds.openid.connect.sdk.OIDCTokenResponse;
 import com.nimbusds.openid.connect.sdk.OIDCTokenResponseParser;
 import com.nimbusds.openid.connect.sdk.token.OIDCTokens;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang.StringUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
@@ -24,6 +48,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
@@ -38,7 +63,6 @@ import org.wso2.carbon.identity.application.common.model.xsd.ServiceProvider;
 import org.wso2.carbon.identity.oauth.stub.dto.OAuthConsumerAppDTO;
 import org.wso2.identity.integration.test.oauth2.OAuth2ServiceAbstractIntegrationTest;
 import org.wso2.identity.integration.test.utils.DataExtractUtil;
-import org.wso2.identity.integration.test.utils.OAuth2Constant;
 
 import java.io.IOException;
 import java.net.URI;
@@ -47,6 +71,24 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.ACCESS_TOKEN_ENDPOINT;
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.AUTHORIZE_ENDPOINT_URL;
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.OAUTH2_CLIENT_ID;
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.OAUTH2_GRANT_TYPE_CODE;
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.OAUTH2_REDIRECT_URI;
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.OAUTH2_RESPONSE_TYPE;
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.OAUTH2_SCOPE;
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.OAUTH2_SCOPE_OPENID;
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.SESSION_DATA_KEY;
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.HTTP_RESPONSE_HEADER_LOCATION;
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.AUTHORIZATION_CODE_NAME;
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.SESSION_DATA_KEY_CONSENT;
+import static org.wso2.identity.integration.test.utils.OAuth2Constant.USER_AGENT;
+
+/**
+ * Test for Session Extender endpoint functionality with claims obtained via an Authorization Code Grant and Refresh
+ * Grant.
+ */
 public class ExtendSessionEndpointAuthCodeGrantTestCase extends OAuth2ServiceAbstractIntegrationTest {
 
     private static final String CALLBACK_URL = "https://localhost/callback";
@@ -61,6 +103,7 @@ public class ExtendSessionEndpointAuthCodeGrantTestCase extends OAuth2ServiceAbs
     private String sessionDataKeyConsent;
     private AuthorizationCode authorizationCode;
     private String idToken;
+    private RefreshToken refreshToken;
     private String idpSessionKey;
     private String authenticatingUserName;
     private String authenticatingCredential;
@@ -79,6 +122,7 @@ public class ExtendSessionEndpointAuthCodeGrantTestCase extends OAuth2ServiceAbs
 
     @AfterClass(alwaysRun = true)
     public void atEnd() throws Exception {
+
         deleteApplication();
         removeOAuthApplicationData();
 
@@ -104,22 +148,20 @@ public class ExtendSessionEndpointAuthCodeGrantTestCase extends OAuth2ServiceAbs
 
         // Send a direct auth code request to IS instance.
         List<NameValuePair> urlParameters = new ArrayList<>();
-        urlParameters.add(new BasicNameValuePair(OAuth2Constant.OAUTH2_RESPONSE_TYPE,
-                OAuth2Constant.OAUTH2_GRANT_TYPE_CODE));
-        urlParameters.add(new BasicNameValuePair(OAuth2Constant.OAUTH2_CLIENT_ID, consumerKey));
-        urlParameters.add(new BasicNameValuePair(OAuth2Constant.OAUTH2_REDIRECT_URI, CALLBACK_URL));
-        urlParameters.add(new BasicNameValuePair(OAuth2Constant.OAUTH2_SCOPE, OAuth2Constant.OAUTH2_SCOPE_OPENID));
+        urlParameters.add(new BasicNameValuePair(OAUTH2_RESPONSE_TYPE, OAUTH2_GRANT_TYPE_CODE));
+        urlParameters.add(new BasicNameValuePair(OAUTH2_CLIENT_ID, consumerKey));
+        urlParameters.add(new BasicNameValuePair(OAUTH2_REDIRECT_URI, CALLBACK_URL));
+        urlParameters.add(new BasicNameValuePair(OAUTH2_SCOPE, OAUTH2_SCOPE_OPENID));
 
-        HttpResponse response = sendPostRequestWithParameters(firstPartyClient, urlParameters,
-                        OAuth2Constant.AUTHORIZE_ENDPOINT_URL);
+        HttpResponse response = sendPostRequestWithParameters(firstPartyClient, urlParameters, AUTHORIZE_ENDPOINT_URL);
         Assert.assertNotNull(response, "Authorization request failed. Authorized response is null");
 
         String locationValue = getLocationHeaderValue(response);
-        Assert.assertTrue(locationValue.contains(OAuth2Constant.SESSION_DATA_KEY),
+        Assert.assertTrue(locationValue.contains(SESSION_DATA_KEY),
                 "sessionDataKey not found in response.");
 
         // Extract sessionDataKey from the location value.
-        sessionDataKey = DataExtractUtil.getParamFromURIString(locationValue, OAuth2Constant.SESSION_DATA_KEY);
+        sessionDataKey = DataExtractUtil.getParamFromURIString(locationValue, SESSION_DATA_KEY);
         Assert.assertNotNull(sessionDataKey, "Session data key is null.");
         EntityUtils.consume(response.getEntity());
     }
@@ -140,65 +182,38 @@ public class ExtendSessionEndpointAuthCodeGrantTestCase extends OAuth2ServiceAbs
         Assert.assertNotNull(response, "Approval request failed. response is invalid.");
 
         Header locationHeader =
-                response.getFirstHeader(OAuth2Constant.HTTP_RESPONSE_HEADER_LOCATION);
+                response.getFirstHeader(HTTP_RESPONSE_HEADER_LOCATION);
         Assert.assertNotNull(locationHeader, "Approval request failed. Location header is null.");
 
         String locationValue = getLocationHeaderValue(response);
-        Assert.assertTrue(locationValue.contains(OAuth2Constant.AUTHORIZATION_CODE_NAME),
+        Assert.assertTrue(locationValue.contains(AUTHORIZATION_CODE_NAME),
                 "Authorization code not found in the response.");
 
         // Extract authorization code from the location value.
         authorizationCode = new AuthorizationCode(DataExtractUtil.getParamFromURIString(locationValue,
-                OAuth2Constant.AUTHORIZATION_CODE_NAME));
+                AUTHORIZATION_CODE_NAME));
         Assert.assertNotNull(authorizationCode, "Authorization code is null.");
         EntityUtils.consume(response.getEntity());
     }
 
     @Test(groups = "wso2.is", description = "Send get access token request.",
             dependsOnMethods = "testAuthCodeGrantSendApprovalPost")
-    public void testAuthCodeGrantSendGetTokensPost() throws Exception {
-
-        ClientID clientID = new ClientID(consumerKey);
-        Secret clientSecret = new Secret(consumerSecret);
-        ClientSecretBasic clientSecretBasic = new ClientSecretBasic(clientID, clientSecret);
+    public void testAuthCodeGrantIdTokenClaimAvailability() throws Exception {
 
         URI callbackURI = new URI(CALLBACK_URL);
         AuthorizationCodeGrant authorizationCodeGrant = new AuthorizationCodeGrant(authorizationCode, callbackURI);
-
-        TokenRequest tokenReq = new TokenRequest(new URI(OAuth2Constant.ACCESS_TOKEN_ENDPOINT), clientSecretBasic,
-                authorizationCodeGrant);
-
-        HTTPResponse tokenHTTPResp = tokenReq.toHTTPRequest().send();
-        Assert.assertNotNull(tokenHTTPResp, "Access token http response is null.");
-
-        TokenResponse tokenResponse = OIDCTokenResponseParser.parse(tokenHTTPResp);
-        Assert.assertNotNull(tokenResponse, "Access token response is null.");
-
-        Assert.assertFalse(tokenResponse instanceof TokenErrorResponse,
-                "Access token response contains errors.");
-
-        OIDCTokenResponse oidcTokenResponse = (OIDCTokenResponse) tokenResponse;
-        OIDCTokens oidcTokens = oidcTokenResponse.getOIDCTokens();
-        Assert.assertNotNull(oidcTokens, "OIDC Tokens object is null.");
+        OIDCTokens oidcTokens = makeTokenRequest(authorizationCodeGrant, ACCESS_TOKEN_ENDPOINT,null);
 
         idToken = oidcTokens.getIDTokenString();
+        refreshToken = oidcTokens.getRefreshToken();
         Assert.assertNotNull(idToken, "ID token is null");
-    }
+        Assert.assertNotNull(refreshToken, "Refresh token is null");
 
-    @Test(groups = "wso2.is",
-            description = "Checks whether the IDP session key is available as a claim in the ID token",
-            dependsOnMethods = "testAuthCodeGrantSendGetTokensPost")
-    public void testIdTokenClaimAvailability() throws Exception {
-
-        JWTClaimsSet claims = SignedJWT.parse(idToken).getJWTClaimsSet();
-        Assert.assertNotNull(claims, "ID token claim set is null");
-
-        idpSessionKey = (String) claims.getClaim(IDP_SESSION_KEY_CLAIM_NAME);
-        Assert.assertNotNull(idpSessionKey, "IDP session key not available in ID token.");
+        testIdTokenClaimAvailability();
     }
 
     @Test(groups = "wso2.is", description = "Sends a request for session extension with a valid cookie.",
-            dependsOnMethods = "testIdTokenClaimAvailability")
+            dependsOnMethods = "testAuthCodeGrantIdTokenClaimAvailability")
     public void testSessionExtensionWithValidCookie() throws Exception {
 
         Long lastAccessedTimeBeforeExtension = getLastAccessedTimeOfSession();
@@ -249,8 +264,22 @@ public class ExtendSessionEndpointAuthCodeGrantTestCase extends OAuth2ServiceAbs
                 "Session has not been extended.");
     }
 
-    @Test(groups = "wso2.is", description = "Sends a valid request for session extension.",
+    @Test(groups = "wso2.is", description = "Checks whether the IDP session key is available as a claim in the" +
+            " ID token obtained from a Refresh Grant",
             dependsOnMethods = "testSessionExtensionWithValidParameters")
+    public void testRefreshGrantIdTokenClaimAvailability() throws Exception {
+
+        RefreshTokenGrant refreshGrant = new RefreshTokenGrant(refreshToken);
+        OIDCTokens oidcTokens = makeTokenRequest(refreshGrant, ACCESS_TOKEN_ENDPOINT, OAUTH2_SCOPE_OPENID);
+
+        idToken = oidcTokens.getIDTokenString();
+        Assert.assertNotNull(idToken, "ID token is null");
+
+        testIdTokenClaimAvailability();
+    }
+
+    @Test(groups = "wso2.is", description = "Sends a valid request for session extension.",
+            dependsOnMethods = "testRefreshGrantIdTokenClaimAvailability")
     public void testSessionExtensionWithInValidParameters() throws Exception {
 
         // Using the fresh third party client without any cookies as the firstPartyClient holds a valid cookie,
@@ -261,6 +290,54 @@ public class ExtendSessionEndpointAuthCodeGrantTestCase extends OAuth2ServiceAbs
         String invalidKeyValueUrl =  String.format(SESSION_EXTENDER_ENDPOINT_GET_URL, "idpSessionKey", "12345");
         testSessionExtensionResponse(thirdPartyClient, invalidKeyValueUrl, HttpServletResponse.SC_BAD_REQUEST,
                 "ISE-60004");
+    }
+
+    /**
+     * Checks whether the isk claim is available in the ID token.
+     *
+     * @throws Exception Exception.
+     */
+    private void testIdTokenClaimAvailability() throws Exception {
+
+        JWTClaimsSet claims = SignedJWT.parse(idToken).getJWTClaimsSet();
+        Assert.assertNotNull(claims, "ID token claim set is null");
+
+        idpSessionKey = (String) claims.getClaim(IDP_SESSION_KEY_CLAIM_NAME);
+        Assert.assertNotNull(idpSessionKey, "IDP session key not available in ID token.");
+    }
+
+    /**
+     * To make a token request with specified grant.
+     *
+     * @param authorizationGrant    Relevant authorization grant.
+     * @return                      OIDC tokens coming from request.
+     * @throws Exception            Exception.
+     */
+    private OIDCTokens makeTokenRequest(AuthorizationGrant authorizationGrant, String uriString, String scopeString)
+            throws Exception {
+
+        ClientID clientID = new ClientID(consumerKey);
+        Secret clientSecret = new Secret(consumerSecret);
+        ClientAuthentication clientAuth = new ClientSecretBasic(clientID, clientSecret);
+        URI uri = new URI(uriString);
+        Scope scope = null;
+        if (StringUtils.isNotBlank(scopeString)) {
+            scope = new Scope(scopeString);
+        }
+        TokenRequest request = new TokenRequest(uri, clientAuth, authorizationGrant, scope);
+
+        HTTPResponse tokenHTTPResp = request.toHTTPRequest().send();
+        Assert.assertNotNull(tokenHTTPResp, "Token http response is null.");
+
+        TokenResponse tokenResponse = OIDCTokenResponseParser.parse(tokenHTTPResp);
+        Assert.assertNotNull(tokenResponse, "Token response of access token response is null.");
+
+        Assert.assertFalse(tokenResponse instanceof TokenErrorResponse, "JWT access token response contains errors.");
+
+        OIDCTokenResponse oidcTokenResponse = (OIDCTokenResponse) tokenResponse;
+        OIDCTokens oidcTokens = oidcTokenResponse.getOIDCTokens();
+        Assert.assertNotNull(oidcTokens, "OIDC Tokens object is null.");
+        return oidcTokens;
     }
 
     /**
@@ -296,7 +373,7 @@ public class ExtendSessionEndpointAuthCodeGrantTestCase extends OAuth2ServiceAbs
      */
     private String getLocationHeaderValue(HttpResponse response) {
 
-        Header location = response.getFirstHeader(OAuth2Constant.HTTP_RESPONSE_HEADER_LOCATION);
+        Header location = response.getFirstHeader(HTTP_RESPONSE_HEADER_LOCATION);
         Assert.assertNotNull(location);
         return location.getValue();
     }
@@ -316,7 +393,7 @@ public class ExtendSessionEndpointAuthCodeGrantTestCase extends OAuth2ServiceAbs
         HttpResponse response = sendLoginPost(client, sessionDataKey);
         Assert.assertNotNull(response, "Login request failed. response is null.");
 
-        Header locationHeader = response.getFirstHeader(OAuth2Constant.HTTP_RESPONSE_HEADER_LOCATION);
+        Header locationHeader = response.getFirstHeader(HTTP_RESPONSE_HEADER_LOCATION);
         Assert.assertNotNull(locationHeader, "Login response header is null");
         EntityUtils.consume(response.getEntity());
 
@@ -324,18 +401,29 @@ public class ExtendSessionEndpointAuthCodeGrantTestCase extends OAuth2ServiceAbs
         response = sendGetRequest(client, locationHeader.getValue());
 
         String locationValue = getLocationHeaderValue(response);
-        Assert.assertTrue(locationValue.contains(OAuth2Constant.SESSION_DATA_KEY_CONSENT),
+        Assert.assertTrue(locationValue.contains(SESSION_DATA_KEY_CONSENT),
                 "sessionDataKeyConsent not found in response.");
 
         EntityUtils.consume(response.getEntity());
 
         // Extract sessionDataKeyConsent from the location value.
-        return DataExtractUtil.getParamFromURIString(locationValue, OAuth2Constant.SESSION_DATA_KEY_CONSENT);
+        return DataExtractUtil.getParamFromURIString(locationValue, SESSION_DATA_KEY_CONSENT);
     }
 
+    /**
+     * Retrieves the last accessed time of the existing session.
+     *
+     * @return              Last accessed time of the session.
+     * @throws Exception    Exception.
+     */
     private Long getLastAccessedTimeOfSession() throws Exception {
 
-        HttpResponse response = sendGetRequestToRestApi(firstPartyClient, SESSIONS_ENDPOINT_URI);
+        String encodedCredentials =
+                Base64.encodeBase64String((authenticatingUserName + ":" + authenticatingCredential).getBytes());
+        BasicHeader authorizationHeader = new BasicHeader(HttpHeaders.AUTHORIZATION, "Basic " + encodedCredentials);
+        List<Header> headers = new ArrayList<>();
+        headers.add(authorizationHeader);
+        HttpResponse response = sendGetRequestWithCustomHeaders(firstPartyClient, SESSIONS_ENDPOINT_URI, headers);
         Assert.assertNotNull(response.getEntity(), "Session extension validation request failed. Response is invalid.");
 
         HttpEntity entity = response.getEntity();
@@ -352,14 +440,27 @@ public class ExtendSessionEndpointAuthCodeGrantTestCase extends OAuth2ServiceAbs
         return Long.parseLong(lastAccessTime);
     }
 
-    private HttpResponse sendGetRequestToRestApi(HttpClient client, String locationURL)
-            throws IOException {
+    /**
+     * Returns the response of a GET request to a REST API.
+     *
+     * @param client        Http client.
+     * @param locationURL   URL of the request.
+     * @param headerList    List of headers.
+     * @return              Http response.
+     * @throws Exception    Exception.
+     */
+    private HttpResponse sendGetRequestWithCustomHeaders(HttpClient client,
+                                                         String locationURL,
+                                                         List<Header> headerList)
+            throws Exception {
 
-        String encodedCredentials =
-                Base64.encodeBase64String((authenticatingUserName + ":" + authenticatingCredential).getBytes());
         HttpGet getRequest = new HttpGet(locationURL);
-        getRequest.setHeader(HttpHeaders.USER_AGENT, OAuth2Constant.USER_AGENT);
-        getRequest.setHeader(HttpHeaders.AUTHORIZATION, "Basic " + encodedCredentials);
+        Header[] headers = new Header[headerList.size()];
+        for (int i = 0; i < headerList.size(); i++) {
+            headers[i] = headerList.get(i);
+        }
+        getRequest.setHeaders(headers);
+        getRequest.setHeader(HttpHeaders.USER_AGENT, USER_AGENT);
         HttpResponse response = client.execute(getRequest);
 
         return response;
