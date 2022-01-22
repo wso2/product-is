6215fdfab95c193b90112d9a983ea3bd0113ee46
sahandilshan <sahandilshan222@gmail.com>
2022-01-22 15:07:10 +0530

    Fix formatting of the OIDCScopeValidationTest.java

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/oidc/OIDCScopeValidationTest.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/oidc/OIDCScopeValidationTest.java
@@ -13,9 +13,7 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.wso2.carbon.identity.application.common.model.xsd.ServiceProvider;
@@ -66,12 +64,9 @@ public class OIDCScopeValidationTest extends OIDCAbstractIntegrationTest {
         createUser(user);
         userInfo.setUserName(user.getUsername());
         userInfo.setPassword(user.getPassword());
-
         playgroundApp = initApplication();
         serviceProvider = createApplication(new ServiceProvider(), playgroundApp);
         client = HttpClientBuilder.create().setDefaultCookieStore(cookieStore).build();
-
-
     }
 
     @AfterMethod(alwaysRun = true)
@@ -86,14 +81,16 @@ public class OIDCScopeValidationTest extends OIDCAbstractIntegrationTest {
 
     @Test(groups = "wso2.is", description = "Test scope validation with a user which has 'internal/everyone' role.")
     public void testScopeValidationWithNormalUser() throws Exception {
-        testSendAuthenticationRequest(playgroundApp, client);
-        testAuthentication(playgroundApp);
+
+        sendAuthenticationRequest(playgroundApp, client);
+        authentication(playgroundApp);
         Assert.assertEquals(claimsToGetConsent, "0_Email,1_First Name",
                 "Requested claims were not prompted to ask the consent.");
         Assert.assertNotNull(scopes);
         Assert.assertEquals(INTERNAL_LOGIN_SCOPE, scopes, "'" + INTERNAL_LOGIN_SCOPE + "' should return " +
                 "with the redirect URL");
-        Assert.assertFalse(scopes.contains(SYSTEM_SCOPE), "'" + SYSTEM_SCOPE + "' scopes should not exist after the validation.");
+        Assert.assertFalse(scopes.contains(SYSTEM_SCOPE), "'" + SYSTEM_SCOPE + "' scopes should not exist " +
+                "after the validation.");
         Assert.assertFalse(scopes.contains(INTERNAL_USER_MGT_UPDATE_SCOPE),
                 "'" + INTERNAL_USER_MGT_UPDATE_SCOPE + "' scopes should not exist after the validation. " +
                         "Since user does not have permission for this scope.");
@@ -105,8 +102,9 @@ public class OIDCScopeValidationTest extends OIDCAbstractIntegrationTest {
     @Test(groups = "wso2.is", dependsOnMethods = {"testScopeValidationWithNormalUser"}, description = "Test scope" +
             " validation with an admin user.")
     public void testScopeValidationWithAdminUser() throws Exception {
-        testSendAuthenticationRequest(playgroundApp, client);
-        testAuthentication(playgroundApp);
+
+        sendAuthenticationRequest(playgroundApp, client);
+        authentication(playgroundApp);
         Assert.assertEquals(claimsToGetConsent, "0_Email,1_First Name",
                 "Requested claims were not prompted to ask the consent.");
         Assert.assertNotNull(scopes);
@@ -134,14 +132,13 @@ public class OIDCScopeValidationTest extends OIDCAbstractIntegrationTest {
     private OIDCApplication initApplication() {
 
         playgroundApp = new OIDCApplication(OIDCUtilTest.playgroundAppOneAppName,
-                OIDCUtilTest.playgroundAppOneAppContext,
-                OIDCUtilTest.playgroundAppOneAppCallBackUri);
+                OIDCUtilTest.playgroundAppOneAppContext, OIDCUtilTest.playgroundAppOneAppCallBackUri);
         playgroundApp.addRequiredClaim(OIDCUtilTest.emailClaimUri);
         playgroundApp.addRequiredClaim(OIDCUtilTest.firstNameClaimUri);
         return playgroundApp;
     }
 
-    private void testSendAuthenticationRequest(OIDCApplication application, HttpClient client)
+    private void sendAuthenticationRequest(OIDCApplication application, HttpClient client)
             throws Exception {
 
         List<NameValuePair> urlParameters = getNameValuePairs(application);
@@ -150,46 +147,38 @@ public class OIDCScopeValidationTest extends OIDCAbstractIntegrationTest {
                         OAuth2Constant.PlaygroundAppPaths.appUserAuthorizePath));
         Assert.assertNotNull(response, "Authorization request failed for " + application.getApplicationName() +
                 ". Authorized response is null.");
-
         Header locationHeader = response.getFirstHeader(OAuth2Constant.HTTP_RESPONSE_HEADER_LOCATION);
-
         Assert.assertNotNull(locationHeader, "Authorization request failed for " +
                 application.getApplicationName() + ". Authorized response header is null.");
         EntityUtils.consume(response.getEntity());
         response = sendGetRequest(client, locationHeader.getValue());
         Assert.assertNotNull(response, "Authorization request failed for " +
                 application.getApplicationName() + ". Authorized user response is null.");
-
         Map<String, Integer> keyPositionMap = new HashMap<>(1);
         keyPositionMap.put("name=\"sessionDataKey\"", 1);
         List<DataExtractUtil.KeyValue> keyValues = DataExtractUtil.extractDataFromResponse(response,
                 keyPositionMap);
         Assert.assertNotNull(keyValues, "The sessionDataKey value is null for " +
                 application.getApplicationName());
-
         sessionDataKey = keyValues.get(0).getValue();
         Assert.assertNotNull(sessionDataKey, "Invalid sessionDataKey for " + application.getApplicationName());
-
         EntityUtils.consume(response.getEntity());
     }
 
-    private void testAuthentication(OIDCApplication application) throws Exception {
+    private void authentication(OIDCApplication application) throws Exception {
 
         HttpResponse response = sendLoginPost(client, sessionDataKey);
         Assert.assertNotNull(response, "Login request failed for " + application.getApplicationName() +
                 ". response is null.");
-
         Header locationHeader = response.getFirstHeader(OAuth2Constant.HTTP_RESPONSE_HEADER_LOCATION);
         Assert.assertNotNull(locationHeader, "Login response header is null for " +
                 application.getApplicationName());
         EntityUtils.consume(response.getEntity());
-
         HttpClient httpClientWithoutAutoRedirections = HttpClientBuilder.create().disableRedirectHandling()
                 .setDefaultCookieStore(cookieStore).build();
         HttpGet getRequest = new HttpGet(locationHeader.getValue());
         getRequest.setHeader("User-Agent", OAuth2Constant.USER_AGENT);
         response = httpClientWithoutAutoRedirections.execute(getRequest);
-
         claimsToGetConsent = claimsToGetConsent(response);
         scopes = getScopes(response);
         consentParameters.addAll(Utils.getConsentRequiredClaimsFromResponse(response));
@@ -203,7 +192,6 @@ public class OIDCScopeValidationTest extends OIDCAbstractIntegrationTest {
                 keyPositionMap);
         Assert.assertNotNull(keyValues, "SessionDataKeyConsent key value is null for " + application
                 .getApplicationName());
-
         sessionDataKeyConsent = keyValues.get(0).getValue();
         Assert.assertNotNull(sessionDataKeyConsent, "Invalid sessionDataKeyConsent for " + application
                 .getApplicationName());
@@ -226,6 +214,7 @@ public class OIDCScopeValidationTest extends OIDCAbstractIntegrationTest {
     }
 
     private static String getScopes(HttpResponse response) throws Exception {
+
         String redirectUrl = Utils.getRedirectUrl(response);
         Map<String, String> queryParams = Utils.getQueryParams(redirectUrl);
         Assert.assertTrue(queryParams.containsKey("scope"));
