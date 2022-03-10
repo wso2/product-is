627803ae38800bdf127a1c95b1eed6bc6535b64c
nilasini <nilathiru.12@cse.mrt.ac.lk>
2022-03-10 21:57:43 +0530

    Fix intermittent issue

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/rest/api/user/authorized/apps/v2/MeAuthorizedAppsScopeTest.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/rest/api/user/authorized/apps/v2/MeAuthorizedAppsScopeTest.java
@@ -241,6 +241,7 @@ public class MeAuthorizedAppsScopeTest extends UserAuthorizedAppsBaseTest {
                 .assertThat()
                 .statusCode(HttpStatus.SC_NO_CONTENT)
                 .log().ifValidationFails();
+        Thread.sleep(1000);
     }
 
     @Test(groups = "wso2.is", dependsOnMethods = {"testDeleteAuthorizedApps"}, description = " The expected " +
