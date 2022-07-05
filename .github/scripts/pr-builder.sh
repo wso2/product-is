76c1e352e6abfafa0adb82ef44891c370143093c
Dhanushka samarasinghe <43103902+nirmal101@users.noreply.github.com>
2022-07-05 12:49:56 +0530

    changing jdk versions

--- .github/scripts/pr-builder.sh
+++ .github/scripts/pr-builder.sh
@@ -7,11 +7,13 @@ SCIM2_REPO_CLONE_LINK=https://github.com/wso2-extensions/identity-inbound-provis
 echo ""
 echo "=========================================================="
 PR_LINK=${PR_LINK%/}
+JDK_VERSION=${JDK_VERSION%/}
 JAVA_8_HOME=${JAVA_8_HOME%/}
 JAVA_11_HOME=${JAVA_11_HOME%/}
 echo "    PR_LINK: $PR_LINK"
 echo "    JAVA 8 Home: $JAVA_8_HOME"
 echo "    JAVA 11 Home: $JAVA_11_HOME"
+echo "    User Input: $JDK_VERSION"
 echo "::warning::Build ran for PR $PR_LINK"
 
 USER=$(echo $PR_LINK | awk -F'/' '{print $4}')
