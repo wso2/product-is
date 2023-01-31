1f8b1f54b7e028a1eb0c4af4c9d38ebdacc6a07d
sahandilshan <sahandilshan222@gmail.com>
2023-01-31 14:25:24 +0530

    Add integration tests to check the SYSTEM scope behvaiour

---
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/oauth2/SystemScopePermissionValidationTestCase.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.identity.integration.test.oauth2;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+import org.wso2.carbon.automation.engine.context.AutomationContext;
+import org.wso2.carbon.automation.engine.context.TestUserMode;
+import org.wso2.carbon.identity.oauth.stub.dto.OAuthConsumerAppDTO;
+import org.wso2.carbon.integration.common.admin.client.AuthenticatorClient;
+import org.wso2.identity.integration.common.clients.application.mgt.ApplicationManagementServiceClient;
+import org.wso2.identity.integration.common.clients.oauth.OauthAdminClient;
+import org.wso2.identity.integration.test.utils.DataExtractUtil;
+import org.wso2.identity.integration.test.utils.OAuth2Constant;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.wso2.identity.integration.test.utils.DataExtractUtil.KeyValue;
+
+public class SystemScopePermissionValidationTestCase extends OAuth2ServiceAbstractIntegrationTest {
+
+    private AuthenticatorClient logManger;
+    private String accessToken;
+    private String consumerKey;
+    private String consumerSecret;
+    private CloseableHttpClient client;
+    private final String username;
+    private final String userPassword;
+    private final String activeTenant;
+    private final AutomationContext context;
+
+    private static final String SYSTEM_SCOPE = "SYSTEM";
+
+    @DataProvider(name = "configProvider")
+    public static Object[][] configProvider() {
+
+        return new Object[][]{{TestUserMode.SUPER_TENANT_ADMIN}, {TestUserMode.TENANT_ADMIN}};
+    }
+
+    @Factory(dataProvider = "configProvider")
+    public SystemScopePermissionValidationTestCase(TestUserMode userMode) throws Exception {
+
+        context = new AutomationContext("IDENTITY", userMode);
+        this.username = context.getContextTenant().getTenantAdmin().getUserName();
+        this.userPassword = context.getContextTenant().getTenantAdmin().getPassword();
+        this.activeTenant = context.getContextTenant().getDomain();
+    }
+
+    @BeforeClass(alwaysRun = true)
+    public void testInit() throws Exception {
+
+        backendURL = context.getContextUrls().getBackEndUrl();
+        logManger = new AuthenticatorClient(backendURL);
+        sessionCookie = logManger.login(username, userPassword, context.getInstance().getHosts().get("default"));
+        tenantInfo = context.getContextTenant();
+        userInfo = tenantInfo.getContextUser();
+        appMgtclient = new ApplicationManagementServiceClient(sessionCookie, backendURL, null);
+        adminClient = new OauthAdminClient(backendURL, sessionCookie);
+
+        setSystemproperties();
+        client = HttpClientBuilder.create().build();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void atEnd() throws Exception {
+
+        appMgtclient.deleteApplication(SERVICE_PROVIDER_NAME);
+        adminClient.removeOAuthApplicationData(consumerKey);
+        client.close();
+        logManger = null;
+        consumerKey = null;
+        accessToken = null;
+    }
+
+    @Test(groups = "wso2.is", description = "Check Oauth2 application registration")
+    public void testRegisterApplication() throws Exception {
+
+        OAuthConsumerAppDTO appDto = createApplication();
+        Assert.assertNotNull(appDto, "Application creation failed.");
+
+        consumerKey = appDto.getOauthConsumerKey();
+        Assert.assertNotNull(consumerKey, "Application creation failed.");
+
+        consumerSecret = appDto.getOauthConsumerSecret();
+        Assert.assertNotNull(consumerSecret, "Application creation failed.");
+    }
+
+    @Test(groups = "wso2.is", description = "Send authorize user request and get access token", dependsOnMethods = "testRegisterApplication")
+    public void testGetAccessToken() throws Exception {
+
+        List<NameValuePair> urlParameters = new ArrayList<>();urlParameters.add(new BasicNameValuePair("grantType",
+                OAuth2Constant.OAUTH2_GRANT_TYPE_CLIENT_CREDENTIALS));
+        urlParameters.add(new BasicNameValuePair("consumerKey", consumerKey));
+        urlParameters.add(new BasicNameValuePair("consumerSecret", consumerSecret));
+        urlParameters.add(new BasicNameValuePair("accessEndpoint",
+                OAuth2Constant.ACCESS_TOKEN_ENDPOINT));
+        urlParameters.add(new BasicNameValuePair("authorize", OAuth2Constant.AUTHORIZE_PARAM));
+        urlParameters.add(new BasicNameValuePair("scope", SYSTEM_SCOPE));
+        HttpResponse response =
+                sendPostRequestWithParameters(client, urlParameters,
+                        OAuth2Constant.AUTHORIZED_USER_URL);
+        Assert.assertNotNull(response, "Authorization request failed. Authorized response is null");
+        EntityUtils.consume(response.getEntity());
+
+        response = sendPostRequest(client, OAuth2Constant.AUTHORIZED_URL);
+
+        Map<String, Integer> keyPositionMap = new HashMap<String, Integer>(1);
+        keyPositionMap.put("name=\"accessToken\"", 1);
+
+        List<KeyValue> keyValues =
+                DataExtractUtil.extractInputValueFromResponse(response,
+                        keyPositionMap);
+        Assert.assertNotNull(keyValues, "Access token Key value is null.");
+        accessToken = keyValues.get(0).getValue();
+
+        EntityUtils.consume(response.getEntity());
+        Assert.assertNotNull(accessToken, "Access token is null.");
+    }
+
+    @Test(groups = "wso2.is", description = "Test introspection endpoint", dependsOnMethods = "testGetAccessToken")
+    public void testIntrospectionEndpoint() throws Exception {
+
+        String introspectionUrl = getIntrospectionUrl(activeTenant);
+        org.json.simple.JSONObject introspectionResponse =
+                introspectTokenWithTenant(client, accessToken, introspectionUrl, username, userPassword);
+        Assert.assertTrue(introspectionResponse.containsKey("scope"));
+        String scope = introspectionResponse.get("scope").toString();
+
+        if (activeTenant.equals("carbon.super")) {
+            Assert.assertTrue(scope.contains("internal_server_admin"), "Scope should contain " +
+                    "`internal_server_admin` scope");
+            Assert.assertTrue(scope.contains("internal_modify_tenants"), "Scope should contain " +
+                    "`internal_modify_tenants` scope");
+        } else {
+            Assert.assertFalse(scope.contains("internal_server_admin"), "Scope should not contain " +
+                    "`internal_server_admin` scope");
+            Assert.assertFalse(scope.contains("internal_modify_tenants"), "Scope should not contain " +
+                    "`internal_modify_tenants` scope");
+        }
+    }
+
+    private String getIntrospectionUrl(String tenantDomain) {
+
+        return "carbon.super".equalsIgnoreCase(tenantDomain) ?
+                OAuth2Constant.INTRO_SPEC_ENDPOINT : OAuth2Constant.TENANT_INTRO_SPEC_ENDPOINT;
+    }
+}
