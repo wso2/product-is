681f334faad7dc77bb8ff26d1d2eb65ddf4b9a99
ashanthamara <ashanthamara98@gmail.com>
2023-05-24 13:08:23 +0530

    Created OAuth2RestClient class f

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/oauth2/OAuth2RestClient.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/oauth2/OAuth2RestClient.java
@@ -2,46 +2,44 @@ package org.wso2.identity.integration.test.oauth2;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import io.restassured.http.ContentType;
 import org.apache.commons.codec.binary.Base64;
-import org.apache.http.HttpHeaders;
+import org.apache.http.Header;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.*;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
+import org.testng.Assert;
 import org.wso2.carbon.automation.engine.context.beans.Tenant;
-import org.wso2.identity.integration.test.rest.api.server.application.management.v1.model.*;
+import org.wso2.identity.integration.test.rest.api.server.application.management.v1.model.ApplicationListResponse;
+import org.wso2.identity.integration.test.rest.api.server.application.management.v1.model.ApplicationModel;
+import org.wso2.identity.integration.test.rest.api.server.application.management.v1.model.ApplicationPatchModel;
+import org.wso2.identity.integration.test.rest.api.server.application.management.v1.model.ApplicationResponseModel;
+import org.wso2.identity.integration.test.rest.api.server.application.management.v1.model.OpenIDConnectConfiguration;
 import org.wso2.identity.integration.test.utils.OAuth2Constant;
 
-import java.io.BufferedReader;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.List;
 
 public class OAuth2RestClient {
 
-    //    static final String APPLICATION_MANAGEMENT_API_BASE_PATH = "https://localhost:9853/api/server/v1/applications";
-    public static final String NAME_ATTRIBUTE = "name";
-    public static final String GRANT_TYPE_ATTRIBUTE = "grantTypes";
-    public static final String CALL_BACK_URLS_ATTRIBUTE = "callbackURLs";
-    public static final String OIDC_ATTRIBUTE = "oidc";
-    public static final String INBOUND_PROTOCOL_CONFIG_ATTRIBUTE = "inboundProtocolConfiguration";
-    private static final String PUBLIC_CLIENT_ATTRIBUTE = "publicClient";
-    private final CloseableHttpClient client;
-    private final String APPLICATION_MANAGEMENT_API_BASE_PATH;
+    public static final String LOCATION_HEADER = "Location";
+    public static final String BASIC_AUTHORIZATION_ATTRIBUTE = "Basic ";
+    public static final String CONTENT_TYPE_ATTRIBUTE = "Content-Type";
+    public static final String AUTHORIZATION_ATTRIBUTE = "Authorization";
+    private static final String USER_AGENT_ATTRIBUTE = "User-Agent";
     protected static final String TENANT_PATH = "t/%s";
     protected static final String API_SERVER_BASE_PATH = "/api/server/v1";
     protected static final String APPLICATION_MANAGEMENT_PATH = "/applications";
     protected static final String INBOUND_PROTOCOLS_OIDC_CONTEXT_PATH = "/inbound-protocols/oidc";
+    private final CloseableHttpClient client;
+    private final String APPLICATION_MANAGEMENT_API_BASE_PATH;
     private final String USERNAME;
     private final String PASSWORD;
 
@@ -53,100 +51,150 @@ public class OAuth2RestClient {
 
         String tenantDomain = tenantInfo.getContextUser().getUserDomain();
 
-        APPLICATION_MANAGEMENT_API_BASE_PATH = backendURL + String.format(TENANT_PATH, tenantDomain) + API_SERVER_BASE_PATH + APPLICATION_MANAGEMENT_PATH;
+        APPLICATION_MANAGEMENT_API_BASE_PATH = backendURL + String.format(TENANT_PATH, tenantDomain) +
+                API_SERVER_BASE_PATH + APPLICATION_MANAGEMENT_PATH;
     }
 
     public String createApplication(ApplicationModel application) throws IOException, JSONException {
 
-        HashMap<String, Object> rootMap = new HashMap<>();
+        String jsonRequest = toJSONString(application);
 
-        JSONArray grantTypes = new JSONArray(application.getInboundProtocolConfiguration().getOidc().getGrantTypes());
-        JSONArray callBackUrls = new JSONArray(application.getInboundProtocolConfiguration().getOidc().getCallbackURLs());
-
-        HashMap<String, Object> oidcMap = new HashMap<>();
-        oidcMap.put(GRANT_TYPE_ATTRIBUTE, grantTypes);
-        oidcMap.put(CALL_BACK_URLS_ATTRIBUTE, callBackUrls);
-        oidcMap.put(PUBLIC_CLIENT_ATTRIBUTE, application.getInboundProtocolConfiguration().getOidc().getPublicClient());
-
-        HashMap<String, Object> inboundConfigMap = new HashMap<>();
-        inboundConfigMap.put(OIDC_ATTRIBUTE, new JSONObject(oidcMap));
-
-        rootMap.put(NAME_ATTRIBUTE, application.getName());
-        rootMap.put(INBOUND_PROTOCOL_CONFIG_ATTRIBUTE, new JSONObject(inboundConfigMap));
-
-        JSONObject rootObject = new JSONObject(rootMap);
-
-        HttpPost request = new HttpPost(APPLICATION_MANAGEMENT_API_BASE_PATH);
-        request.setHeader("User-Agent", OAuth2Constant.USER_AGENT);
-        request.setHeader("Authorization", "Basic " + Base64.encodeBase64String((USERNAME + ":" + PASSWORD).getBytes()).trim());
-        request.setHeader("Content-Type", String.valueOf(ContentType.JSON));
-
-        StringEntity entity = new StringEntity(rootObject.toString());
-        request.setEntity(entity);
-        HttpResponse response = client.execute(request);
+        HttpResponse response = getResponseOfHttpPost(APPLICATION_MANAGEMENT_API_BASE_PATH, jsonRequest);
         EntityUtils.consume(response.getEntity());
-        String[] self = response.getHeaders("Location")[0].toString().split("/");
+
+        String[] self = response.getHeaders(LOCATION_HEADER)[0].toString().split("/");
 
         return self[self.length-1];
     }
 
-    public ApplicationListItem[] getAllApplications() throws IOException {
+    public ApplicationResponseModel getApplication(String appId) throws IOException {
 
-        HttpGet request = new HttpGet(APPLICATION_MANAGEMENT_API_BASE_PATH);
-        request.setHeader("User-Agent", OAuth2Constant.USER_AGENT);
-        request.setHeader("Authorization", "Basic " + Base64.encodeBase64String((USERNAME + ":" + PASSWORD).getBytes()).trim());
-        request.setHeader("Content-Type", String.valueOf(ContentType.JSON));
-        HttpResponse response = client.execute(request);
+        HttpResponse response = getResponseOfHttpGet(APPLICATION_MANAGEMENT_API_BASE_PATH + "/" + appId);
 
         String responseBody = EntityUtils.toString(response.getEntity());
         EntityUtils.consume(response.getEntity());
 
         ObjectMapper jsonWriter = new ObjectMapper(new JsonFactory());
-        ApplicationListResponse listResponse = jsonWriter.readValue(responseBody, ApplicationListResponse.class);
 
-        List<ApplicationListItem> applicationsList = listResponse.getApplications();
-
-        return applicationsList.toArray(new ApplicationListItem[0]);
+        return jsonWriter.readValue(responseBody, ApplicationResponseModel.class);
     }
 
-    public ApplicationModel getOAuthInboundDetailsOfApp(String appId) throws Exception {
+    public void updateApplication(String appId, ApplicationPatchModel application) throws IOException {
 
-        String path = APPLICATION_MANAGEMENT_API_BASE_PATH + "/" + appId + INBOUND_PROTOCOLS_OIDC_CONTEXT_PATH;
+        String jsonRequest = toJSONString(application);
 
-        ApplicationModel resultApp = new ApplicationModel();
-        resultApp.setId(appId);
+        HttpResponse response = getResponseOfHttpPatch(APPLICATION_MANAGEMENT_API_BASE_PATH + "/" + appId,
+                jsonRequest);
 
-        HttpGet request = new HttpGet(path);
-        request.setHeader("User-Agent", OAuth2Constant.USER_AGENT);
-        request.setHeader("Authorization", "Basic " + Base64.encodeBase64String((USERNAME + ":" + PASSWORD).getBytes()).trim());
-        request.setHeader("Content-Type", String.valueOf(ContentType.JSON));
-        HttpResponse response = client.execute(request);
+        Assert.assertEquals(response.getStatusLine().getStatusCode(), HttpServletResponse.SC_OK,
+                "Application update failed");
+        EntityUtils.consume(response.getEntity());
+    }
 
-        BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+    public String toJSONString(java.lang.Object object) {
 
-        Object obj = JSONValue.parse(rd);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(object);
+    }
+
+    public HttpResponse getResponseOfHttpPost(String endPointUrl, String jsonRequest) throws IOException {
+
+        HttpPost request = new HttpPost(endPointUrl);
+        request.setHeaders(getHeaders());
+        request.setEntity(new StringEntity(jsonRequest));
+
+        return client.execute(request);
+    }
+
+    public HttpResponse getResponseOfHttpGet(String endPointUrl) throws IOException {
+
+        HttpGet request = new HttpGet(endPointUrl);
+        request.setHeaders(getHeaders());
+
+        return client.execute(request);
+    }
+
+    public HttpResponse getResponseOfHttpPatch(String endPointUrl, String jsonRequest) throws IOException {
+
+        HttpPatch request = new HttpPatch(endPointUrl);
+        request.setHeaders(getHeaders());
+        request.setEntity(new StringEntity(jsonRequest));
+
+        return client.execute(request);
+    }
+
+    public HttpResponse getResponseOfHttpDelete(String endPointUrl) throws IOException {
+
+        HttpDelete request = new HttpDelete(endPointUrl);
+        request.setHeaders(getHeaders());
+
+        return client.execute(request);
+    }
+
+    public HttpResponse getResponseOfHttpPut(String endPointUrl, String jsonRequest) throws IOException {
+
+        HttpPut request = new HttpPut(endPointUrl);
+        request.setHeaders(getHeaders());
+        request.setEntity(new StringEntity(jsonRequest));
+
+        return client.execute(request);
+    }
+
+    public Header[] getHeaders() {
+
+        Header[] headerList = new Header[3];
+        headerList[0] = new BasicHeader(USER_AGENT_ATTRIBUTE, OAuth2Constant.USER_AGENT);
+        headerList[1] = new BasicHeader(AUTHORIZATION_ATTRIBUTE, BASIC_AUTHORIZATION_ATTRIBUTE +
+                Base64.encodeBase64String((USERNAME + ":" + PASSWORD).getBytes()).trim());
+        headerList[2] = new BasicHeader(CONTENT_TYPE_ATTRIBUTE, String.valueOf(ContentType.JSON));
+
+        return headerList;
+    }
+
+    public ApplicationListResponse getAllApplications() throws IOException {
+
+        HttpResponse response = getResponseOfHttpGet(APPLICATION_MANAGEMENT_API_BASE_PATH);
+
+        String responseBody = EntityUtils.toString(response.getEntity());
         EntityUtils.consume(response.getEntity());
 
-        OpenIDConnectConfiguration oidcConfig = new OpenIDConnectConfiguration();
-        oidcConfig.setClientId(((JSONObject) obj).get("clientId").toString());
-        oidcConfig.setClientSecret(((JSONObject) obj).get("clientSecret").toString());
+        ObjectMapper jsonWriter = new ObjectMapper(new JsonFactory());
 
-        InboundProtocols inboundProtocolsConfig = new InboundProtocols();
-        inboundProtocolsConfig.setOidc(oidcConfig);
+        return jsonWriter.readValue(responseBody, ApplicationListResponse.class);
+    }
 
-        resultApp.setInboundProtocolConfiguration(inboundProtocolsConfig);
+    public OpenIDConnectConfiguration getOIDCInboundDetails(String appId) throws Exception {
 
-        return resultApp;
+        HttpResponse response = getResponseOfHttpGet(APPLICATION_MANAGEMENT_API_BASE_PATH + "/" +
+                appId + INBOUND_PROTOCOLS_OIDC_CONTEXT_PATH);
+
+        String responseBody = EntityUtils.toString(response.getEntity());
+        EntityUtils.consume(response.getEntity());
+
+        ObjectMapper jsonWriter = new ObjectMapper(new JsonFactory());
+
+        return jsonWriter.readValue(responseBody, OpenIDConnectConfiguration.class);
+    }
+
+    public void updateOIDCInboundDetailsOfApplication(String appId, OpenIDConnectConfiguration oidcInboundConfig)
+            throws IOException {
+
+        String jsonRequest = toJSONString(oidcInboundConfig);
+
+        HttpResponse response = getResponseOfHttpPut(APPLICATION_MANAGEMENT_API_BASE_PATH + "/" + appId +
+                INBOUND_PROTOCOLS_OIDC_CONTEXT_PATH, jsonRequest);
+
+        Assert.assertEquals(response.getStatusLine().getStatusCode(), HttpServletResponse.SC_OK,
+                "Application oidc inbound config update failed");
+        EntityUtils.consume(response.getEntity());
     }
 
     public void deleteApplication(String appId) throws IOException {
 
-        HttpDelete request = new HttpDelete(APPLICATION_MANAGEMENT_API_BASE_PATH + "/" + appId);
+        HttpResponse response = getResponseOfHttpDelete(APPLICATION_MANAGEMENT_API_BASE_PATH + "/" + appId);
 
-        request.setHeader(HttpHeaders.USER_AGENT, OAuth2Constant.USER_AGENT);
-        request.setHeader(HttpHeaders.AUTHORIZATION, "Basic " + Base64.encodeBase64String((USERNAME + ":" + PASSWORD).getBytes()).trim());
-        request.setHeader(HttpHeaders.CONTENT_TYPE, "application/scim+json");
-
-        client.execute(request);
+        Assert.assertEquals(response.getStatusLine().getStatusCode(), HttpServletResponse.SC_NO_CONTENT,
+                "Application deletion failed");
+        EntityUtils.consume(response.getEntity());
     }
 }
