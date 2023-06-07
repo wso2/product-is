8c5c28104212c5c2fe5f581e4938babb1c473453
ashanthamara <ashanthamara98@gmail.com>
2023-06-07 15:16:20 +0530

    Updated SAML2Configuration object

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/rest/api/server/application/management/v1/model/SAML2Configuration.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/rest/api/server/application/management/v1/model/SAML2Configuration.java
@@ -1,18 +1,20 @@
 /*
-* Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2019, WSO2 LLC. (https://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 package org.wso2.identity.integration.test.rest.api.server.application.management.v1.model;
 
@@ -26,7 +28,7 @@ public class SAML2Configuration  {
   
     private String metadataFile;
     private String metadataURL;
-    private SAML2ServiceProvider serviceProvider;
+    private SAML2ServiceProvider manualConfiguration;
 
     /**
     **/
@@ -66,20 +68,20 @@ public class SAML2Configuration  {
 
     /**
     **/
-    public SAML2Configuration serviceProvider(SAML2ServiceProvider serviceProvider) {
+    public SAML2Configuration manualConfiguration(SAML2ServiceProvider serviceProvider) {
 
-        this.serviceProvider = serviceProvider;
+        this.manualConfiguration = serviceProvider;
         return this;
     }
     
     @ApiModelProperty(value = "")
     @JsonProperty("serviceProvider")
     @Valid
-    public SAML2ServiceProvider getServiceProvider() {
-        return serviceProvider;
+    public SAML2ServiceProvider getManualConfiguration() {
+        return manualConfiguration;
     }
-    public void setServiceProvider(SAML2ServiceProvider serviceProvider) {
-        this.serviceProvider = serviceProvider;
+    public void setManualConfiguration(SAML2ServiceProvider manualConfiguration) {
+        this.manualConfiguration = manualConfiguration;
     }
 
 
@@ -96,12 +98,12 @@ public class SAML2Configuration  {
         SAML2Configuration saML2Configuration = (SAML2Configuration) o;
         return Objects.equals(this.metadataFile, saML2Configuration.metadataFile) &&
             Objects.equals(this.metadataURL, saML2Configuration.metadataURL) &&
-            Objects.equals(this.serviceProvider, saML2Configuration.serviceProvider);
+            Objects.equals(this.manualConfiguration, saML2Configuration.manualConfiguration);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(metadataFile, metadataURL, serviceProvider);
+        return Objects.hash(metadataFile, metadataURL, manualConfiguration);
     }
 
     @Override
@@ -112,7 +114,7 @@ public class SAML2Configuration  {
 
         sb.append("    metadataFile: ").append(toIndentedString(metadataFile)).append("\n");
         sb.append("    metadataURL: ").append(toIndentedString(metadataURL)).append("\n");
-        sb.append("    serviceProvider: ").append(toIndentedString(serviceProvider)).append("\n");
+        sb.append("    serviceProvider: ").append(toIndentedString(manualConfiguration)).append("\n");
         sb.append("}");
         return sb.toString();
     }
