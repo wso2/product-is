586abc28be7c3f60f49991ccb36751c5bba12b3c
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-06-24 07:27:38 +0530

    Update migration-script-ubuntu.sh

--- .github/migration-tester/migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ .github/migration-tester/migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -36,7 +36,7 @@ currentVersion=$(echo $currentVersion | xargs)
 combinedCurrentVersion="wso2is-${currentVersion}"
 
 # Replace all instances of "CurrentVersion" with the combined version value
-sed -i "s/CurrentVersion/${combinedCurrentVersion}/g" /home/runner/work/product-is/migration-tester/migration-automation/env.s
+sed -i "s/CurrentVersion/${combinedCurrentVersion}/g" /home/runner/work/product-is/product-is/migration-tester/migration-automation/env.s
 
 # Remove spaces from the beginning and end of the migratingVersion variable
 migratingVersion=$(echo $migratingVersion | xargs)
