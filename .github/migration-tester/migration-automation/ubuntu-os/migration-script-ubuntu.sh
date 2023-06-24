2fbc06d5bb2d2ec97201857fe93f8fdc4209c5fa
JayanaGunaweera01 <jayana.20200003@iit.ac.lk>
2023-06-24 13:22:47 +0530

    Changed path of migration-tester folder

--- .github/migration-tester/migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ .github/migration-tester/migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -38,7 +38,7 @@ combinedCurrentVersion="wso2is-${currentVersion}"
 # Replace all instances of "CurrentVersion" with the combined version value
 sed -i "s/CurrentVersion/${combinedCurrentVersion}/g" /home/runner/work/product-is/product-is/.github/migration-tester/migration-automation/env.sh
 pwd
-echo "Paths updated.."
+echo "Paths updated...."
 
 # Remove spaces from the beginning and end of the migratingVersion variable
 migratingVersion=$(echo $migratingVersion | xargs)
