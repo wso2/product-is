f671ddee08f04316921e15756daecf3e6389abc2
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-06-26 14:52:49 +0530

    Update migration-script-mac.sh

--- .github/migration-tester/migration-automation/mac-os/migration-script-mac.sh
+++ .github/migration-tester/migration-automation/mac-os/migration-script-mac.sh
@@ -90,9 +90,15 @@ cd "./IS_HOME_OLD"
 echo "${GREEN}==> Navigated to home folder successfully${RESET}"
 
 # Download needed wso2IS zip
-wget -qq --waitretry=5 --retry-connrefused "$urlOld"
-ls -a
-echo "${GREEN}==> Downloaded needed wso2IS zip${RESET}"
+if [ "$currentVersion" = "5.9.0" ]; then
+
+    response=$(curl -k -L -o wso2is.zip "https://drive.google.com/u/0/uc?id=1GU32FtPGvvB2WsmQoPnHr5yn1M6ddL-h&amp;amp;export=download&amp;amp;confirm=t&amp;amp;uuid=712b27d8-ea10-4e0b-bbbd-3cde24b1d92e&amp;amp;at=AKKF8vzpFDL5XdIrNRv6KFY0ZvPr:1687251333945&amp;confirm=t&amp;uuid=efe88210-d059-4968-84c6-7a1236bb6ef9&amp;at=AKKF8vxFWIDReSULenUtKrASKULT:1687251490306&confirm=t&uuid=7017f976-902b-4050-a3a6-22b26bb46d88&at=AKKF8vyia4DpEk742C_FTMCmJDE9:1687251582718")
+    wait $!
+    echo "$response"
+else
+    wget -qq --waitretry=5 --retry-connrefused "$urlOld"
+    wait $!
+fi
 
 # Unzip IS archive
 unzip -qq *.zip &
