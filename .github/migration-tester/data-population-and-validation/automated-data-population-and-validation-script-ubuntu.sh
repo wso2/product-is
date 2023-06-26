4a86b2bd7856c68cd1279019773556a75d442725
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-06-26 12:26:14 +0530

    Update automated-data-population-and-validation-script-ubuntu.sh

--- .github/migration-tester/data-population-and-validation/automated-data-population-and-validation-script-ubuntu.sh
+++ .github/migration-tester/data-population-and-validation/automated-data-population-and-validation-script-ubuntu.sh
@@ -15,6 +15,7 @@ currentVersion=$1
 data_population_dir="/home/runner/work/product-is/product-is/.github/migration-tester/data-population-and-validation"
 
 echo -e "${GREEN}==> Running data population scripts${NC}"
+
 if [ "$currentVersion" = "5.9" ]; then
   # execute scripts in order
   for script in \
@@ -42,37 +43,36 @@ if [ "$currentVersion" = "5.9" ]; then
       echo "${GREEN}==> Script '$script_path' does not exist.${RESET}"
     fi
   done
-else
-    # execute scripts in alternative order
-    for script in \
-      "1-user-creation/create-user.sh" \
-      "1-user-creation/create-bulk-users.sh" \
-      "2-tenant-creation/create-tenant.sh" \
-      "2-tenant-creation/create-tenant-soapAPI.sh" \
-      "3-userstore-creation/create-userstore.sh" \
-      "3-userstore-creation/create-user-in-userstore.sh" \
-      "4-service-provider-creation/register-a-service-provider.sh" \
-      "4-service-provider-creation/create-user-in-a-service-provider.sh" \
-      "4-service-provider-creation/register-a-service-provider-get-access-token-ubuntu.sh" \
-      "5-group-creation/create-group.sh" \
-      "5-group-creation/create-groups-with-users.sh"
-    do
-      # construct the full path of the script
-      script_path="$data_population_dir/$script"
+fi
 
-      # check if script exists and is executable
-      if [ -f "$script_path" ]; then
-        chmod +x "$script_path" # make the script executable
-        printf "${ORANGE}Running script: %s${RESET}\n" "$script_path"
-        # execute script
-        "$script_path"
-      else
-        echo "${GREEN}==> Script '$script_path' does not exist.${RESET}"
-      fi
-    done
-  else
-    echo "${GREEN}==> Directory '$data_population_dir' does not exist.${RESET}"
-  fi
+if [ "$currentVersion" != "5.9" ]; then
+  # execute scripts in alternative order
+  for script in \
+    "1-user-creation/create-user.sh" \
+    "1-user-creation/create-bulk-users.sh" \
+    "2-tenant-creation/create-tenant.sh" \
+    "2-tenant-creation/create-tenant-soapAPI.sh" \
+    "3-userstore-creation/create-userstore.sh" \
+    "3-userstore-creation/create-user-in-userstore.sh" \
+    "4-service-provider-creation/register-a-service-provider.sh" \
+    "4-service-provider-creation/create-user-in-a-service-provider.sh" \
+    "4-service-provider-creation/register-a-service-provider-get-access-token-ubuntu.sh" \
+    "5-group-creation/create-group.sh" \
+    "5-group-creation/create-groups-with-users.sh"
+  do
+    # construct the full path of the script
+    script_path="$data_population_dir/$script"
+
+    # check if script exists and is executable
+    if [ -f "$script_path" ]; then
+      chmod +x "$script_path" # make the script executable
+      printf "${ORANGE}Running script: %s${RESET}\n" "$script_path"
+      # execute script
+      "$script_path"
+    else
+      echo "${GREEN}==> Script '$script_path' does not exist.${RESET}"
+    fi
+  done
 fi
 
 # execute scripts in any other subdirectories
