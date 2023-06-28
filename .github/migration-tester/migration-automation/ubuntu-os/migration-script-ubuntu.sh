bbd7ec834e716b047712dd064b9e8b096b82cd2e
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-06-29 02:21:27 +0530

    Update migration-script-ubuntu.sh

--- .github/migration-tester/migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ .github/migration-tester/migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -101,13 +101,15 @@ response=$(curl --location --request POST 'https://oauth2.googleapis.com/token' 
 access_token=$(echo "$response" | jq -r '.access_token')
 
 # Specify the Google Drive file URL
- #file_url="$urlOld"
+file_url="$urlOld"
 
-# Extract the file ID from the URL
- #file_id=$(echo "$file_url" | awk -F'/' '{print $NF}' | awk -F'=' '{print $2}')
+# Extract the file ID from the URL using grep
+file_id=$(echo "$file_url" | grep -o -E '[^/]+$' | grep -o -E '[^?=]+$')
+
+echo "File ID: $file_id"
 
 # Specify the Google Drive file URL
-file_url="https://www.googleapis.com/drive/v3/files/1WSQwh2aizd-nhrQQEc_PqnTJJxiRufUv?alt=media"
+file_url="https://www.googleapis.com/drive/v3/files/$file_id?alt=media"
 # https://drive.google.com/file/d/1WSQwh2aizd-nhrQQEc_PqnTJJxiRufUv/view?usp=sharing  - is 5.10.0
 # https://drive.google.com/file/d/1tn6GYCzJtMBdwleQ2fVy1icUlItdm4w5/view?usp=sharing   - is 5.11.0
 # https://drive.google.com/file/d/1eAcSLBD-UoYI84SefGQKokOhpmU9bO_a/view?usp=sharing -is 6.0
