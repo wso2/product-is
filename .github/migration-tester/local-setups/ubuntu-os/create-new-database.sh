37d7a02d94059f9ed93e86644c2349b5b28826b0
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-06-28 06:54:40 +0530

    Update create-new-database.sh

--- .github/migration-tester/local-setups/ubuntu-os/create-new-database.sh
+++ .github/migration-tester/local-setups/ubuntu-os/create-new-database.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-chmod +x $Home/Downloads/Automating-Product-Migration-Testing/utils/mysql.sql
-docker exec -i mysql_eight sh -c 'exec mysql -uroot -proot' < $Home/Downloads/Automating-Product-Migration-Testing/utils/mysql.sql
+chmod +x $Home/Downloads/Automating-Product-Migration-Testing/local-setups/utils/mysql.sql
+docker exec -i mysql_eight sh -c 'exec mysql -uroot -proot' < $Home/Downloads/Automating-Product-Migration-Testing/local-setups/utils/mysql.sql
 echo "Database created successfully!!"
 
-chmod +x  $Home/Downloads/Automating-Product-Migration-Testing/utils/db-scripts/IS-5.11/mysql.sql
-docker exec -i mysql_eight sh -c 'exec mysql -uroot -proot -D '$DATABASE'' < $Home/Downloads/Automating-Product-Migration-Testing/utils/db-scripts/IS-5.11/mysql.sql
-docker exec -i mysql_eight sh -c 'exec mysql -uroot -proot -D '$DATABASE'' < $Home/Downloads/Automating-Product-Migration-Testing/utils/db-scripts/IS-5.11/identity/mysql.sql
-docker exec -i mysql_eight sh -c 'exec mysql -uroot -proot -D '$DATABASE'' < $Home/Downloads/Automating-Product-Migration-Testing/utils/db-scripts/IS-5.11/identity/uma/mysql.sql
-docker exec -i mysql_eight sh -c 'exec mysql -uroot -proot -D '$DATABASE'' < $Home/Downloads/Automating-Product-Migration-Testing/utils/db-scripts/IS-5.11/consent/mysql.sql
-docker exec -i mysql_eight sh -c 'exec mysql -uroot -proot -D '$DATABASE'' < $Home/Downloads/Automating-Product-Migration-Testing/utils/db-scripts/IS-5.11/metrics/mysql.sql
+chmod +x  $Home/Downloads/Automating-Product-Migration-Testing/local-setups/utils/db-scripts/IS-5.11/mysql.sql
+docker exec -i mysql_eight sh -c 'exec mysql -uroot -proot -D '$DATABASE'' < $Home/Downloads/Automating-Product-Migration-Testing/local-setups/utils/db-scripts/IS-5.11/mysql.sql
+docker exec -i mysql_eight sh -c 'exec mysql -uroot -proot -D '$DATABASE'' < $Home/Downloads/Automating-Product-Migration-Testing/local-setups/utils/db-scripts/IS-5.11/identity/mysql.sql
+docker exec -i mysql_eight sh -c 'exec mysql -uroot -proot -D '$DATABASE'' < $Home/Downloads/Automating-Product-Migration-Testing/local-setups/utils/db-scripts/IS-5.11/identity/uma/mysql.sql
+docker exec -i mysql_eight sh -c 'exec mysql -uroot -proot -D '$DATABASE'' < $Home/Downloads/Automating-Product-Migration-Testing/local-setups/utils/db-scripts/IS-5.11/consent/mysql.sql
+docker exec -i mysql_eight sh -c 'exec mysql -uroot -proot -D '$DATABASE'' < $Home/Downloads/Automating-Product-Migration-Testing/local-setups/utils/db-scripts/IS-5.11/metrics/mysql.sql
