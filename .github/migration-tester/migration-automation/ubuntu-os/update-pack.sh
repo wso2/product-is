e84a1afc59f3a53172ebb737e9e50712bd503dff
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-06-29 03:41:11 +0530

    Update update-pack.sh

--- .github/migration-tester/migration-automation/ubuntu-os/update-pack.sh
+++ .github/migration-tester/migration-automation/ubuntu-os/update-pack.sh
@@ -9,28 +9,44 @@ NC='\033[0m' # No Color
 
 email=$1
 password=$2
+startServer=$3
+
+
+# Source env file
+. "/home/runner/work/product-is/product-is/.github/migration-tester/migration-automation/env.sh"
+echo -e "${GREEN}==> Env file for Ubuntu sourced successfully${NC}"
 
 # Copy update tool from utils to bin folder
 cd "/home/runner/work/product-is/product-is/.github/migration-tester/utils/update-tools"
 
-cp -r $UPDATE_TOOL_UBUNTU $BIN_ISOLD
+
+if [ "$startServer" = "currentVersion" ]; then
+  cp -r $UPDATE_TOOL_UBUNTU $BIN_ISOLD
+elif [ "$startServer" = "migratingVersion" ]; then
+  cp -r $UPDATE_TOOL_UBUNTU $BIN_ISNEW
+fi
 copy_exit_code=$?
 if [ $copy_exit_code -eq 0 ]; then
-    echo "${GREEN}==> Update tool successfully copied to $currentVersion${RESET}"
+    echo "${GREEN}==> Update tool successfully copied to "$startServer"${RESET}"
 else
     echo "${RED}==> Failed to copy the update tool.${RESET}"
 fi
 
-cd "$BIN_ISOLD"
+if [ "$startServer" = "currentVersion" ]; then
+  cd "$BIN_ISOLD"
+elif [ "$startServer" = "migratingVersion" ]; then
+  cd "$BIN_ISNEW"
+fi
+
 
 sudo apt-get install expect -y
-
-# Set executable permissions for the expect script
-chmod +x ./wso2update_linux
 
 # Create an expect script file
 cat >wso2update_script.expect <<EOF
 #!/usr/bin/expect -f
+email=$1
+password=$2
+
 spawn ./wso2update_linux
 expect "Please enter your credentials to continue."
 sleep 5
@@ -56,13 +72,15 @@ EOF
 # Set executable permissions for the expect script
 chmod +x wso2update_script.expect
 # Run the expect script
-./wso2update_script.expect
+./wso2update_script.expect "$email" "$password"
 
 echo "${GREEN}==> Updated the Client Tool successfully${RESET}" &
 wait $!
 
 # Update Product Pack
-./wso2update_linux
+./wso2update_linux 
 echo "${GREEN}==> Updated the Product Pack successfully${RESET}" &
 wait $!
+
+
 
