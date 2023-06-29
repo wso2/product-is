bf8c35c11d8a314c2c3215766751804db9f185b5
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-06-29 16:39:16 +0530

    Delete .github/migration-tester/data-population-and-validation/windows-os directory

--- .github/migration-tester/data-population-and-validation/windows-os/3-userstore-creation/create-user-in-userstore.sh
+++
@@ -1,7 +0,0 @@
-curl -k --location --request POST "$SCIM_USER_EP_USERSTORE" \
---header 'Content-Type: application/json' \
---header 'Authorization: Basic YWRtaW46YWRtaW4=' \
---data-raw '{"schemas":[],"userName":"'$USERSTORE_USER_NAME'","password":"'$USERSTORE_USER_PASSWORD'","wso2Extension":{"employeeNumber":"000111","costCenter":"111111","organization":"WSO2Org","division":"'$USERSTORE_GROUP_NAME'","department":"Intigration","manager":{"managerId":"111000","displayName":"'$USERSTORE_USER_NAME'"}}}'
-
-echo "\033[1;35mA user has been created in userstore. User name=$USERSTORE_USER_NAME, Group name=$USERSTORE_GROUP_NAME\033[0m"
-echo
