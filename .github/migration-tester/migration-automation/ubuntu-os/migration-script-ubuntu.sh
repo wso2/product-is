5d16ce79f9aa2cba18e389ce264e7129d63f7d35
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-06-30 10:15:33 +0530

    Update migration-script-ubuntu.sh

--- .github/migration-tester/migration-automation/ubuntu-os/migration-script-ubuntu.sh
+++ .github/migration-tester/migration-automation/ubuntu-os/migration-script-ubuntu.sh
@@ -98,7 +98,7 @@ response=$(curl --location --request POST 'https://oauth2.googleapis.com/token' 
 
 # Extract the access token from the response using jq
 access_token=$(echo "$response" | jq -r '.access_token')
-
+#https://drive.google.com/file/d/1tn6GYCzJtMBdwleQ2fVy1icUlItdm4w5/view?usp=sharing
 # Initialize file_id variable
 file_id=""
 
@@ -128,7 +128,7 @@ esac
 echo "file_id: $file_id"
 
 # Specify the Google Drive file URL
-file_url="https://www.googleapis.com/drive/v3/files/"$file_id"?alt=media"
+file_url="https://www.googleapis.com/drive/v3/files/1tn6GYCzJtMBdwleQ2fVy1icUlItdm4w5?alt=media"
 
 # Download the file using the access token
 response=$(curl "$file_url" \
