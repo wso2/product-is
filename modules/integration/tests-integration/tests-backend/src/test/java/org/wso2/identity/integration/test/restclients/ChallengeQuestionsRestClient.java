51a3f8acf6991d8cc7f0c726c16fa1721e69f1ad
ashanthamara <ashanthamara98@gmail.com>
2023-07-17 09:14:52 +0530

    Created ChallengeQuestionsRestClient

---
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/restclients/ChallengeQuestionsRestClient.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.identity.integration.test.restclients;
+
+import io.restassured.http.ContentType;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.Header;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.message.BasicHeader;
+import org.testng.Assert;
+import org.wso2.carbon.automation.engine.context.beans.Tenant;
+import org.wso2.identity.integration.test.rest.api.server.challenge.v1.model.UserChallengeAnswer;
+import org.wso2.identity.integration.test.utils.OAuth2Constant;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ChallengeQuestionsRestClient extends RestBaseClient {
+    private final String serverUrl;
+    private static final String CHALLENGE_QUESTION_BASE_PATH = "api/users/v1/%s/challenge-answers";
+    private final String tenantDomain;
+    private final String username;
+    private final String password;
+
+    public ChallengeQuestionsRestClient(String serverUrl, Tenant tenantInfo) {
+
+        this.serverUrl = serverUrl;
+        this.tenantDomain = tenantInfo.getContextUser().getUserDomain();
+        this.username = tenantInfo.getContextUser().getUserName();
+        this.password = tenantInfo.getContextUser().getPassword();
+    }
+
+    /**
+     * Set Answers for the user challenge questions
+     *
+     * @param userId userId.
+     * @param questionSetId Challenge Question Set id.
+     * @param challengeAsnwerObj Challenge Question request object.
+     */
+    public void setChallengeQuestionAnswer(String userId, String questionSetId,
+                                           UserChallengeAnswer challengeAsnwerObj) throws Exception {
+
+        String jsonRequest = toJSONString(challengeAsnwerObj);
+        String endPointUrl = serverUrl + TENANT_PATH + tenantDomain + PATH_SEPARATOR +
+                String.format(CHALLENGE_QUESTION_BASE_PATH, userId) + PATH_SEPARATOR +  questionSetId;
+
+        try (CloseableHttpResponse response = getResponseOfHttpPost(endPointUrl, jsonRequest, getHeaders())) {
+            Assert.assertEquals(response.getStatusLine().getStatusCode(), HttpServletResponse.SC_CREATED,
+                    "Setting Challenge Answer is failed");
+        }
+    }
+
+    private Header[] getHeaders() {
+        Header[] headerList = new Header[3];
+        headerList[0] = new BasicHeader(USER_AGENT_ATTRIBUTE, OAuth2Constant.USER_AGENT);
+        headerList[1] = new BasicHeader(AUTHORIZATION_ATTRIBUTE, BASIC_AUTHORIZATION_ATTRIBUTE +
+                Base64.encodeBase64String((username + ":" + password).getBytes()).trim());
+        headerList[2] = new BasicHeader(CONTENT_TYPE_ATTRIBUTE, String.valueOf(ContentType.JSON));
+
+        return headerList;
+    }
+
+    /**
+     * Close the HTTP client.
+     *
+     */
+    public void closeHttpClient() throws IOException {
+        client.close();
+    }
+}
