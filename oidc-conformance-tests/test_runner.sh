f3285024b91505148a0f7f63f8bfd0f5c5f445d0
Anju Chamantha <chamantha97anju@gmail.com>
2023-09-12 15:47:55 +0530

    FAPI suite - add fapi test

--- oidc-conformance-tests/test_runner.sh
+++ oidc-conformance-tests/test_runner.sh
@@ -44,7 +44,7 @@ then
   echo "-----------------------------"
   echo
   sed -i '/^.*all_test_modules\ =.*/a \ \ \ \ print("All available OIDC test modules:")\n\ \ \ \ print("==============================================")\n\ \ \ \ print(sorted(all_test_modules.keys()))' $CONFORMANCE_SUITE_PATH/scripts/run-test-plan.py
-  python3 $CONFORMANCE_SUITE_PATH/scripts/run-test-plan.py fapi1-advanced-final[server_metadata=static][client_registration=static_client] $PATH_TO_SCRIPTS/fapi/IS_config_fapi.json 2>&1 | tee fapi-certification-test-plan-log.txt
+  python3 $CONFORMANCE_SUITE_PATH/scripts/run-test-plan.py fapi1-advanced-final-test-plan[client_auth_type=mtls][fapi_profile=plain_fapi][fapi_response_mode=plain_response][fapi_auth_request_method=by_value] $PATH_TO_SCRIPTS/fapi/IS_config_fapi.json 2>&1 | tee fapi-certification-test-plan-log.txt
   echo
 else
   echo "Basic certification test plan"
