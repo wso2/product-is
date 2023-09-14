860aa43764cd60b630d812b67ea24ef1dfae261e
Jayana  Gunaweera <79576139+JayanaGunaweera01@users.noreply.github.com>
2023-09-14 16:38:57 +0530

    Update download-GA-pack.sh
    Changes focus on refining the way the script accesses and uses the JSON credentials, with an emphasis on security and efficiency.
    
    Previously, the value for keyJsonFile was derived from the first positional argument ($1) which was written for a file in the workflow.
    In the new code, it is derived from the environment variable MIGRATION_JSON_ENCODED. After fetching the data from the variable, it gets base64-decoded to retrieve the JSON contents. This is a significant change as the way credentials are passed to the script has been altered for added security.

--- .github/migration-tester/migration-automation/download-GA-pack.sh
+++ .github/migration-tester/migration-automation/download-GA-pack.sh
@@ -81,16 +81,15 @@ base64stream() {
     base64 | tr '/+' '_-' | tr -d '=\n'
 }
 
-keyJsonFile=$1
+keyJsonFile=$(echo "$MIGRATION_JSON_ENCODED" | base64 -d)
 scope="https://www.googleapis.com/auth/drive.readonly"
 valid_for_sec="${3:-3600}"
-valid_for_sec_numeric=$(echo "$valid_for_sec" | tr -d -c '[:digit:]')
-private_key=$(jq -r .private_key "$keyJsonFile")
-sa_email=$(jq -r .client_email "$keyJsonFile") || { echo "Error extracting client_email from the JSON file."; exit 1; }
+private_key=$(jq -r .private_key <<< "$keyJsonFile")
+sa_email=$(jq -r .client_email <<< "$keyJsonFile") || { echo "Error extracting client_email from the JSON file."; exit 1; }
 
 header='{"alg":"RS256","typ":"JWT"}'
 # Calculate the expiration time as 'valid_for_sec' seconds from now
-exp=$(($(date +%s) + 60))  # Set the token to expire in 60 seconds
+exp=$(($(date +%s) + 60))
 # The issued at time should be the current time
 iat=$(date +%s)
 claim=$(cat <<EOF | jq -c
@@ -107,8 +106,6 @@ EOF
 request_body="$(base64var "$header").$(base64var "$claim")"
 signature=$(echo -n "$request_body" | openssl dgst -sha256 -sign <(echo "$private_key") -binary | base64stream)
 
-echo "$request_body.$signature"
-
 jwt_token=$(printf "%s.%s" "$request_body" "$signature")
 
 # Manually construct the POST data for token request
@@ -116,8 +113,6 @@ data="grant_type=urn:ietf:params:oauth:grant-type:jwt-bearer&assertion=$jwt_toke
 
 # Make the token request to get the access token
 token_response=$(curl -s -X POST -d "$data" "https://www.googleapis.com/oauth2/v4/token")
-
-# Extract the access token from the response
 access_token=$(echo "$token_response" | jq -r .access_token)
 
 # Use the file_id variable in downloading the IS zip
