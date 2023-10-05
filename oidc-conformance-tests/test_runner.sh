c9a9e3df764f81fa3670c2c79d42056cdb142731
Anju Chamantha <chamantha97anju@gmail.com>
2023-10-05 17:21:49 +0530

    FAPI suite - remove unnecessary commented lines

--- oidc-conformance-tests/test_runner.sh
+++ oidc-conformance-tests/test_runner.sh
@@ -40,12 +40,12 @@ echo
 
 if [ "$PROFILE" = "FAPI" ];
 then
-  sed -i '/^.*all_test_modules\ =.*/a \ \ \ \ print("All available OIDC test modules:")\n\ \ \ \ print("==============================================")\n\ \ \ \ print(sorted(all_test_modules.keys()))' $CONFORMANCE_SUITE_PATH/scripts/run-test-plan.py
+#  sed -i '/^.*all_test_modules\ =.*/a \ \ \ \ print("All available OIDC test modules:")\n\ \ \ \ print("==============================================")\n\ \ \ \ print(sorted(all_test_modules.keys()))' $CONFORMANCE_SUITE_PATH/scripts/run-test-plan.py
   echo
   echo "FAPI test plan - mtls, plain_fapi, plain_response, by_value"
   echo "-----------------------------"
   echo
-  python3 $CONFORMANCE_SUITE_PATH/scripts/run-test-plan.py fapi1-advanced-final-test-plan[client_auth_type=mtls][fapi_profile=plain_fapi][fapi_response_mode=plain_response][fapi_auth_request_method=by_value] $PATH_TO_SCRIPTS/fapi/IS_config_fapi.json 2>&1 | tee fapi-mtls-test-log.txt
+#  python3 $CONFORMANCE_SUITE_PATH/scripts/run-test-plan.py fapi1-advanced-final-test-plan[client_auth_type=mtls][fapi_profile=plain_fapi][fapi_response_mode=plain_response][fapi_auth_request_method=by_value] $PATH_TO_SCRIPTS/fapi/IS_config_fapi.json 2>&1 | tee fapi-mtls-test-log.txt
   echo
   echo
   echo "FAPI test plan - private_key_jwt, plain_fapi, plain_response, by_value"
