086600feb501ee3a62545439bbba63d61c429839
Anju Chamantha <chamantha97anju@gmail.com>
2023-11-13 12:53:09 +0530

    [FAPI suite] both profiles

--- oidc-fapi-conformance-tests/test_runner_fapi.sh
+++ oidc-fapi-conformance-tests/test_runner_fapi.sh
@@ -10,12 +10,12 @@ echo "========================"
 echo "Running Tests"
 echo "========================"
 
-# echo
-# echo "FAPI Basic test plan - private_key_jwt, plain_fapi, plain_response, by_value"
-# echo "-----------------------------"
-# echo
-# python3 $CONFORMANCE_SUITE_PATH/scripts/run-test-plan.py fapi1-advanced-final-test-plan[client_auth_type=private_key_jwt][fapi_profile=plain_fapi][fapi_response_mode=plain_response][fapi_auth_request_method=by_value] $PATH_TO_SCRIPTS/config/IS_config_fapi.json 2>&1 | tee fapi-testplan-pvtkeyjwt-log.txt
-# echo
+echo
+echo "FAPI Basic test plan - private_key_jwt, plain_fapi, plain_response, by_value"
+echo "-----------------------------"
+echo
+python3 $CONFORMANCE_SUITE_PATH/scripts/run-test-plan.py fapi1-advanced-final-test-plan[client_auth_type=private_key_jwt][fapi_profile=plain_fapi][fapi_response_mode=plain_response][fapi_auth_request_method=by_value] $PATH_TO_SCRIPTS/config/IS_config_fapi.json 2>&1 | tee fapi-testplan-pvtkeyjwt-log.txt
+echo
 
 echo
 echo "FAPI Basic test plan - mtls, plain_fapi, plain_response, by_value"
