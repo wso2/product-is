5ffeed6f715fa442a46f4f148567f1dcff4c7583
Anju Chamantha <chamantha97anju@gmail.com>
2023-11-13 12:27:30 +0530

    [FAPI suite] only mtls

--- oidc-fapi-conformance-tests/test_runner_fapi.sh
+++ oidc-fapi-conformance-tests/test_runner_fapi.sh
@@ -6,12 +6,16 @@ IS_SUCCESSFUL=false
 
 #  sed -i '/^.*all_test_modules\ =.*/a \ \ \ \ print("All available OIDC test modules:")\n\ \ \ \ print("==============================================")\n\ \ \ \ print(sorted(all_test_modules.keys()))' $CONFORMANCE_SUITE_PATH/scripts/run-test-plan.py
 
-echo
-echo "FAPI Basic test plan - private_key_jwt, plain_fapi, plain_response, by_value"
-echo "-----------------------------"
-echo
-python3 $CONFORMANCE_SUITE_PATH/scripts/run-test-plan.py fapi1-advanced-final-test-plan[client_auth_type=private_key_jwt][fapi_profile=plain_fapi][fapi_response_mode=plain_response][fapi_auth_request_method=by_value] $PATH_TO_SCRIPTS/config/IS_config_fapi.json 2>&1 | tee fapi-testplan-pvtkeyjwt-log.txt
-echo
+echo "========================"
+echo "Running Tests"
+echo "========================"
+
+# echo
+# echo "FAPI Basic test plan - private_key_jwt, plain_fapi, plain_response, by_value"
+# echo "-----------------------------"
+# echo
+# python3 $CONFORMANCE_SUITE_PATH/scripts/run-test-plan.py fapi1-advanced-final-test-plan[client_auth_type=private_key_jwt][fapi_profile=plain_fapi][fapi_response_mode=plain_response][fapi_auth_request_method=by_value] $PATH_TO_SCRIPTS/config/IS_config_fapi.json 2>&1 | tee fapi-testplan-pvtkeyjwt-log.txt
+# echo
 
 echo
 echo "FAPI Basic test plan - mtls, plain_fapi, plain_response, by_value"
