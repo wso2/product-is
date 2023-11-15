9a22199bcc8ac519f112d4ecdaf61c4bb6e1caaf
Anju Chamantha <chamantha97anju@gmail.com>
2023-11-15 09:55:47 +0530

    [FAPI suite] export results

--- oidc-fapi-conformance-tests/export_results_fapi.py
+++ oidc-fapi-conformance-tests/export_results_fapi.py
@@ -7,9 +7,9 @@ conformance_suite_url = sys.argv[1]
 
 
 # export test results of a given test plan
-def save_results(plan):
+def save_results(plan, i):
     response = requests.get(url=conformance_suite_url + "/api/plan/exporthtml/" + plan['_id'], stream=True, verify=False)
-    with open("./" + plan['planName'] + "_test_results.zip", 'wb') as fileDir:
+    with open("./" + plan['planName_'] + i + "_test_results.zip", 'wb') as fileDir:
         for chunk in response.iter_content(chunk_size=128):
             fileDir.write(chunk)
 
@@ -40,8 +40,10 @@ if __name__ == '__main__':
     warnings.filterwarnings("ignore")
     plan_list = json.loads(requests.get(url=conformance_suite_url + "/api/plan?length=50", verify=False).content)
     print("======================\nExporting test results\n======================")
+    i = 0
     for test_plan in plan_list['data']:
-        save_results(test_plan)
+        i += 1
+        save_results(test_plan, i)
         failed_tests_list = get_failed_tests(test_plan)
         if len(failed_tests_list['fails']) > 0 or len(failed_tests_list['warnings']) > 0:
             failed_plan_details[test_plan['planName']] = failed_tests_list
