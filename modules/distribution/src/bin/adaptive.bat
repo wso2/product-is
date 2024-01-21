1fa0f3d699c5acff860c13c8823e63098d4815ae
Sachin-Mamoru <sachinm@wso2.com>
2024-01-21 20:53:31 +0530

    Update the adaptive.bat script to add missing JARs

--- modules/distribution/src/bin/adaptive.bat
+++ modules/distribution/src/bin/adaptive.bat
@@ -25,11 +25,13 @@ rem    CARBON_HOME       Home of WSO2 Carbon installation. If not set I will  tr
 rem                      to figure it out.
 rem    NASHORN_VERSION   OpenJDK Nashorn Version        	
 rem    ASM_VERSION       ASM Util, Commons Version.
+rem    JMS_VERSION       Geronimo Spec JMS Version.
 rem
 rem -----------------------------------------------------------------------------
 
 set NASHORN_VERSION=15.3
 set ASM_VERSION=9.2
+set JMS_VERSION=1.1.0.rc4-wso2v1
 
 set SERVER_RESTART_REQUIRED="false"
 
@@ -67,18 +69,25 @@ goto abortDisableAdaptiveAuth
 :proceedDisableAdaptiveAuth
 set LOCAL_NASHORN_VERSION=""
 set LOCAL_ASM_VERSION=""
+set LOCAL_JMS_VERSION=""
 
 call :removeLibrary "Nashorn", "lib", "%CARBON_HOME%\repository\components\lib\nashorn-core-*.jar"
 call :removeLibrary "Nashorn", "dropins", "%CARBON_HOME%\repository\components\dropins\nashorn_core_!LOCAL_NASHORN_VERSION!*.jar"
 call :removeLibrary "ASM Util", "lib", "%CARBON_HOME%\repository\components\lib\asm-util-*.jar"
 call :removeLibrary "ASM Util", "dropins", "%CARBON_HOME%\repository\components\dropins\asm_util_!LOCAL_ASM_VERSION!*.jar"
+call :removeLibrary "ASM Commons", "lib", "%CARBON_HOME%\repository\components\lib\asm-commons-*.jar"
+call :removeLibrary "ASM Commons", "dropins", "%CARBON_HOME%\repository\components\dropins\asm_commons_!LOCAL_ASM_VERSION!*.jar"
+call :removeLibrary "ASM Tree", "lib", "%CARBON_HOME%\repository\components\lib\asm-tree-*.jar"
+call :removeLibrary "ASM Tree", "dropins", "%CARBON_HOME%\repository\components\dropins\asm_tree_!LOCAL_ASM_VERSION!*.jar"
+call :removeLibrary "Geronimo Spec Jms", "lib", "%CARBON_HOME%\repository\components\lib\geronimo-spec-jms-*.jar"
+call :removeLibrary "Geronimo Spec Jms", "dropins", "%CARBON_HOME%\repository\components\dropins\geronimo_spec_jms_!LOCAL_JMS_VERSION!*.jar"
 
 echo Adaptive authentication successfully disabled.
 goto printRestartMsg
 
 rem function to remove a jar matching a given file path pattern.
 :removeLibrary
-rem should be one of 'Nashorn' or 'ASM-Util'.
+rem should be one of 'Nashorn', 'ASM-Util', 'ASM-Commons', 'ASM-Tree' or 'Geronimo-Spec-Jms'.
 set jar_name=%~1
 rem should be one of 'lib' or 'dropins'.
 set folder=%~2
@@ -102,6 +111,15 @@ if exist "%file_pattern%" (
     ) else if "%jar_name%"=="ASM Util" (
       rem extracts the a.b (i.e version) and sets it as local version
       for /f "tokens=1,2 delims=." %%l in ("!artifact_name!") do set "LOCAL_ASM_VERSION=%%l.%%m"
+    ) else if "%jar_name%"=="ASM Commons" (
+      rem extracts the a.b (i.e version) and sets it as local version
+      for /f "tokens=1,2 delims=." %%l in ("!artifact_name!") do set "LOCAL_ASM_VERSION=%%l.%%m"
+    ) else if "%jar_name%"=="ASM Tree" (
+      rem extracts the a.b (i.e version) and sets it as local version
+      for /f "tokens=1,2 delims=." %%l in ("!artifact_name!") do set "LOCAL_ASM_VERSION=%%l.%%m"
+    ) else if "%jar_name%"=="Geronimo Spec Jms" (
+      rem extracts the a.b (i.e version) and sets it as local version
+      for /f "tokens=1,2 delims=." %%l in ("!artifact_name!") do set "LOCAL_JMS_VERSION=%%l.%%m"
     )
   )
 
@@ -183,6 +201,111 @@ if exist "%CARBON_HOME%\repository\components\lib\asm-util-*.jar" (
     exit /b !ERRORLEVEL!
   )
   echo ASM-Util download completed. Downloaded version : asm-util-%ASM_VERSION%%
+)
+
+if exist "%CARBON_HOME%\repository\components\lib\asm-commons-*.jar" (
+  for /f "delims=" %%i in ('dir /s /b %CARBON_HOME%\repository\components\lib\asm-commons-*.jar') do set "location=%%i"
+  for %%j in (!location!!) do set "full_artifact_name=%%~nxj"
+  for /f "tokens=3 delims=-" %%k in ("!full_artifact_name!") do set "artifact_name=%%k"
+  for /f "tokens=1,2 delims=." %%l in ("!artifact_name!") do set "LOCAL_ASM_VERSION=%%l.%%m"
+
+  if %ASM_VERSION%==!LOCAL_ASM_VERSION!  (
+    echo ASM-Commons library exists. No need to download.
+  ) else (
+    set SERVER_RESTART_REQUIRED="true"
+    echo Required ASM-Commons library not found. Remove existing library : !full_artifact_name!
+    del !location!
+    call :removeLibrary "ASM Commons", "dropins", "%CARBON_HOME%\repository\components\dropins\asm_commons_!LOCAL_ASM_VERSION!*.jar"
+    echo Downloading required ASM-Commons library : asm-commons-%ASM_VERSION%
+	  curl https://repo1.maven.org/maven2/org/ow2/asm/asm-commons/%ASM_VERSION%/asm-commons-%ASM_VERSION%.jar -o %CARBON_HOME%/repository/components/lib/asm-commons-%ASM_VERSION%.jar
+	rem if download failed
+    if !ERRORLEVEL! neq 0 (
+      echo ASM-Commons library update failed with exit code: !ERRORLEVEL!
+      exit /b !ERRORLEVEL!
+    )
+    echo ASM-Commons library updated.
+  )
+) else (
+  set SERVER_RESTART_REQUIRED="true"
+  echo ASM-Commons library not found. Starting to download.....
+  curl https://repo1.maven.org/maven2/org/ow2/asm/asm-commons/%ASM_VERSION%/asm-commons-%ASM_VERSION%.jar -o %CARBON_HOME%/repository/components/lib/asm-commons-%ASM_VERSION%.jar
+  rem if download failed
+  set exit_code_a=!ERRORLEVEL!
+  if !ERRORLEVEL! neq 0 (
+    echo ASM-Commons library download failed with exit code: !ERRORLEVEL!
+    exit /b !ERRORLEVEL!
+  )
+  echo ASM-Commons download completed. Downloaded version : asm-commons-%ASM_VERSION%%
+)
+
+if exist "%CARBON_HOME%\repository\components\lib\asm-tree-*.jar" (
+  for /f "delims=" %%i in ('dir /s /b %CARBON_HOME%\repository\components\lib\asm-tree-*.jar') do set "location=%%i"
+  for %%j in (!location!!) do set "full_artifact_name=%%~nxj"
+  for /f "tokens=3 delims=-" %%k in ("!full_artifact_name!") do set "artifact_name=%%k"
+  for /f "tokens=1,2 delims=." %%l in ("!artifact_name!") do set "LOCAL_ASM_VERSION=%%l.%%m"
+
+  if %ASM_VERSION%==!LOCAL_ASM_VERSION!  (
+    echo ASM-Tree library exists. No need to download.
+  ) else (
+    set SERVER_RESTART_REQUIRED="true"
+    echo Required ASM-Tree library not found. Remove existing library : !full_artifact_name!
+    del !location!
+    call :removeLibrary "ASM Tree", "dropins", "%CARBON_HOME%\repository\components\dropins\asm_tree_!LOCAL_ASM_VERSION!*.jar"
+    echo Downloading required ASM-Tree library : asm-tree-%ASM_VERSION%
+	  curl https://repo1.maven.org/maven2/org/ow2/asm/asm-tree/%ASM_VERSION%/asm-tree-%ASM_VERSION%.jar -o %CARBON_HOME%/repository/components/lib/asm-tree-%ASM_VERSION%.jar
+	rem if download failed
+    if !ERRORLEVEL! neq 0 (
+      echo ASM-Tree library update failed with exit code: !ERRORLEVEL!
+      exit /b !ERRORLEVEL!
+    )
+    echo ASM-Tree library updated.
+  )
+) else (
+  set SERVER_RESTART_REQUIRED="true"
+  echo ASM-Tree library not found. Starting to download.....
+  curl https://repo1.maven.org/maven2/org/ow2/asm/asm-tree/%ASM_VERSION%/asm-tree-%ASM_VERSION%.jar -o %CARBON_HOME%/repository/components/lib/asm-tree-%ASM_VERSION%.jar
+  rem if download failed
+  set exit_code_a=!ERRORLEVEL!
+  if !ERRORLEVEL! neq 0 (
+    echo ASM-Tree library download failed with exit code: !ERRORLEVEL!
+    exit /b !ERRORLEVEL!
+  )
+  echo ASM-Tree download completed. Downloaded version : asm-tree-%ASM_VERSION%%
+)
+
+if exist "%CARBON_HOME%\repository\components\lib\geronimo-spec-jms-*.jar" (
+  for /f "delims=" %%i in ('dir /s /b %CARBON_HOME%\repository\components\lib\geronimo-spec-jms-*.jar') do set "location=%%i"
+  for %%j in (!location!!) do set "full_artifact_name=%%~nxj"
+  for /f "tokens=3 delims=-" %%k in ("!full_artifact_name!") do set "artifact_name=%%k"
+  for /f "tokens=1,2 delims=." %%l in ("!artifact_name!") do set "LOCAL_JMS_VERSION=%%l.%%m"
+
+  if %JMS_VERSION%==!LOCAL_JMS_VERSION!  (
+    echo Geronimo-Spec-Jms library exists. No need to download.
+  ) else (
+    set SERVER_RESTART_REQUIRED="true"
+    echo Required Geronimo-Spec-Jms library not found. Remove existing library : !full_artifact_name!
+    del !location!
+    call :removeLibrary "Geronimo Spec Jms", "dropins", "%CARBON_HOME%\repository\components\dropins\geronimo_spec_jms_!LOCAL_JMS_VERSION!*.jar"
+    echo Downloading required Geronimo-Spec-Jms library : geronimo-spec-jms-%JMS_VERSION%
+	  curl https://dist.wso2.org/maven2/geronimo-spec/wso2/geronimo-spec-jms/%JMS_VERSION%/geronimo-spec-jms-%JMS_VERSION%.jar -o %CARBON_HOME%/repository/components/lib/geronimo-spec-jms-%JMS_VERSION%.jar
+	rem if download failed
+    if !ERRORLEVEL! neq 0 (
+      echo Geronimo-Spec-Jms library update failed with exit code: !ERRORLEVEL!
+      exit /b !ERRORLEVEL!
+    )
+    echo Geronimo-Spec-Jms library updated.
+  )
+) else (
+  set SERVER_RESTART_REQUIRED="true"
+  echo Geronimo-Spec-Jms library not found. Starting to download.....
+  curl https://dist.wso2.org/maven2/geronimo-spec/wso2/geronimo-spec-jms/%JMS_VERSION%/geronimo-spec-jms-%JMS_VERSION%.jar -o %CARBON_HOME%/repository/components/lib/geronimo-spec-jms-%JMS_VERSION%.jar
+  rem if download failed
+  set exit_code_a=!ERRORLEVEL!
+  if !ERRORLEVEL! neq 0 (
+    echo Geronimo-Spec-Jms library download failed with exit code: !ERRORLEVEL!
+    exit /b !ERRORLEVEL!
+  )
+  echo Geronimo-Spec-Jms download completed. Downloaded version : geronimo-spec-jms-%JMS_VERSION%%
 )
 echo Adaptive authentication successfully enabled.
 goto printRestartMsg
