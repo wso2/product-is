f96eaeee402ce0cab1449d0796c3c9cee2199698
shanggeeth <shanggeethk@gmail.com>
2024-03-04 10:05:41 +0530

    Modify integrations tests to run with graaljs

---
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/auth/GraalJsAdaptiveScriptInitializerTestCase.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.identity.integration.test.auth;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.wso2.carbon.integration.common.utils.mgt.ServerConfigurationManager;
+import org.wso2.carbon.utils.CarbonUtils;
+import org.wso2.identity.integration.common.utils.ISIntegrationTest;
+
+import java.io.File;
+
+public class GraalJsAdaptiveScriptInitializerTestCase extends ISIntegrationTest {
+
+    private ServerConfigurationManager scm;
+    private File defaultConfigFile;
+
+    @BeforeTest(alwaysRun = true)
+    public void initScriptEngineConfig() throws Exception {
+
+        super.init();
+        String carbonHome = CarbonUtils.getCarbonHome();
+        defaultConfigFile = getDeploymentTomlFile(carbonHome);
+        File scriptEngineConfigFile = new File(
+                getISResourceLocation() + File.separator + "scriptEngine" + File.separator +
+                        "graaljs_script_engine_config.toml");
+        scm = new ServerConfigurationManager(isServer);
+        scm.applyConfiguration(scriptEngineConfigFile, defaultConfigFile, true, true);
+    }
+
+    @AfterTest(alwaysRun = true)
+    public void resetScriptEngineConfig() throws Exception {
+
+        super.init();
+        scm.restoreToLastConfiguration(false);
+        scm.restartGracefully();
+    }
+
+}
