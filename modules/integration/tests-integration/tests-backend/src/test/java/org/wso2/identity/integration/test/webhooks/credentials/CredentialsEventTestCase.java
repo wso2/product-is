26101057d79041a8c0bc8f9e116a2ac5dc923725
Indeewai Wijesiri <indeewari@wso2.com>
2025-08-24 12:17:32 +0530

    Incoorporate the password update payload tests

---
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/webhooks/credentials/CredentialsEventTestCase.java
@@ -0,0 +1,110 @@
+package org.wso2.identity.integration.test.webhooks.credentials;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+import org.wso2.carbon.automation.engine.context.AutomationContext;
+import org.wso2.carbon.automation.engine.context.TestUserMode;
+import org.wso2.identity.integration.common.utils.ISIntegrationTest;
+import org.wso2.identity.integration.test.rest.api.user.common.model.Email;
+import org.wso2.identity.integration.test.rest.api.user.common.model.Name;
+import org.wso2.identity.integration.test.rest.api.user.common.model.PatchOperationRequestObject;
+import org.wso2.identity.integration.test.rest.api.user.common.model.UserItemAddGroupobj;
+import org.wso2.identity.integration.test.rest.api.user.common.model.UserObject;
+import org.wso2.identity.integration.test.restclients.IdentityGovernanceRestClient;
+import org.wso2.identity.integration.test.restclients.SCIM2RestClient;
+import org.wso2.identity.integration.test.webhooks.usermanagement.eventpayloadbuilder.AdminInitUserManagementEventTestExpectedEventPayloadBuilder;
+import org.wso2.identity.integration.test.webhooks.util.WebhookEventTestManager;
+
+import java.util.Arrays;
+
+import static org.testng.Assert.assertNotNull;
+
+/**
+ * This class tests the credential related events in the webhook.
+ */
+public class CredentialsEventTestCase extends ISIntegrationTest {
+
+    private String userId;
+    private final AutomationContext automationContext;
+    private final TestUserMode userMode;
+    private WebhookEventTestManager webhookEventTestManager;
+    private SCIM2RestClient scim2RestClient;
+    private IdentityGovernanceRestClient identityGovernanceRestClient;
+
+    @BeforeClass(alwaysRun = true)
+    public void testInit() throws Exception {
+
+        super.init(userMode);
+
+        scim2RestClient = new SCIM2RestClient(serverURL, tenantInfo);
+        identityGovernanceRestClient = new IdentityGovernanceRestClient(serverURL, tenantInfo);
+
+        UserObject userInfo = new UserObject()
+                .userName("test-user")
+                .password("TestPassword@123")
+                .name(new Name().givenName("test-user-given-name").familyName("test-user-last-name"))
+                .addEmail(new Email().value("test-user@test.com"));
+        userId = scim2RestClient.createUser(userInfo);
+
+        webhookEventTestManager = new WebhookEventTestManager("/scim2/webhook", "WSO2",
+                Arrays.asList("https://schemas.identity.wso2.org/events/user",
+                        "https://schemas.identity.wso2.org/events/credential"),
+                "CredentialsEventTestCase",
+                automationContext);
+
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void atEnd() throws Exception {
+
+        scim2RestClient.closeHttpClient();
+        identityGovernanceRestClient.closeHttpClient();
+        webhookEventTestManager.teardown();
+    }
+
+    @Factory(dataProvider = "testExecutionContextProvider")
+    public CredentialsEventTestCase(TestUserMode userMode) throws Exception {
+
+        this.userMode = userMode;
+        automationContext = new AutomationContext("IDENTITY", userMode);
+    }
+
+    @DataProvider(name = "testExecutionContextProvider")
+    public static Object[][] getTestExecutionContext() {
+
+        return new Object[][]{
+                {TestUserMode.SUPER_TENANT_USER},
+                {TestUserMode.TENANT_USER}
+        };
+    }
+
+    @Test
+    public void testUserProfileUpdateWithoutAnyAccountStateManagementClaims() throws Exception {
+
+        assertNotNull(userId);
+
+        PatchOperationRequestObject patchRequest = new PatchOperationRequestObject();
+
+        UserItemAddGroupobj newPassword = new UserItemAddGroupobj().op(UserItemAddGroupobj.OpEnum.REPLACE);
+        newPassword.setPath("password");
+        newPassword.setValue("NewSecurePassword123!");
+        patchRequest.addOperations(newPassword);
+
+        scim2RestClient.updateUser(patchRequest, userId);
+
+        webhookEventTestManager.stackExpectedEventPayload(
+                "https://schemas.identity.wso2.org/events/credential/event-type/credentialUpdated",
+                AdminInitUserManagementEventTestExpectedEventPayloadBuilder.buildExpectedUserCredentialUpdatedEventPayloadWithoutAnyOtherClaims(
+                        userId, automationContext.getContextTenant().getDomain()));
+        webhookEventTestManager.validateStackedEventPayloads();
+    }
+
+    @Test(dependsOnMethods = {"testUserProfileUpdateWithoutAnyAccountStateManagementClaims"})
+    public void testDeleteUser() throws Exception {
+
+        scim2RestClient.deleteUser(userId);
+    }
+}
--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/webhooks/usermanagement/eventpayloadbuilder/AdminInitUserManagementEventTestExpectedEventPayloadBuilder.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/webhooks/usermanagement/eventpayloadbuilder/AdminInitUserManagementEventTestExpectedEventPayloadBuilder.java
@@ -102,6 +102,32 @@ public class AdminInitUserManagementEventTestExpectedEventPayloadBuilder {
     }
 
     /**
+     * Builds the expected payload for a user credential updated event when neither account state management claims
+     * nor other profile claim gets updated in the context of test case.
+     *
+     * @param userId       ID of the user whose credential is being updated
+     * @param tenantDomain Tenant domain of the user
+     * @return JSONObject representing the expected user profile updated event payload
+     * @throws Exception if an error occurs while building the payload
+     */
+    public static JSONObject buildExpectedUserCredentialUpdatedEventPayloadWithoutAnyOtherClaims(
+            String userId, String tenantDomain) throws Exception {
+
+        JSONObject userProfileUpdateEvent = new JSONObject();
+
+        userProfileUpdateEvent.put("initiatorType", "ADMIN");
+        userProfileUpdateEvent.put("user",
+                createUserObjectForUserCredentialUpdateWithoutAnyOtherClaims(userId, tenantDomain));
+        userProfileUpdateEvent.put("tenant", createTenantObject(tenantDomain));
+        userProfileUpdateEvent.put("organization", createOrganizationObject(tenantDomain));
+        userProfileUpdateEvent.put("userStore", createUserStoreObject());
+        userProfileUpdateEvent.put("action", "CREDENTIAL_UPDATE");
+        userProfileUpdateEvent.put("credentialType", "PASSWORD");
+
+        return userProfileUpdateEvent;
+    }
+
+    /**
      * Builds the expected payload for a user profile updated event when account lock claim gets updated
      * along with profile in the context of the test case.
      *
@@ -362,6 +388,33 @@ public class AdminInitUserManagementEventTestExpectedEventPayloadBuilder {
         removedClaims.put(
                 new JSONObject().put("uri", "http://wso2.org/claims/givenname").put("value", "test-user-given-name"));
         user.put("removedClaims", removedClaims);
+
+        return user;
+    }
+
+    private static JSONObject createUserObjectForUserCredentialUpdateWithoutAnyOtherClaims(String userId,
+                                                                                           String tenantDomain)
+            throws JSONException {
+
+        JSONObject user = new JSONObject();
+        user.put("id", userId);
+
+        JSONObject userOrganization = new JSONObject();
+        userOrganization.put("id", "dummy-org-id");
+        if (tenantDomain.equals(MultitenantConstants.SUPER_TENANT_DOMAIN_NAME)) {
+            userOrganization.put("name", "Super");
+        } else {
+            userOrganization.put("name", tenantDomain);
+        }
+        userOrganization.put("orgHandle", tenantDomain);
+        userOrganization.put("depth", 0);
+        user.put("organization", userOrganization);
+
+        if (tenantDomain.equals(MultitenantConstants.SUPER_TENANT_DOMAIN_NAME)) {
+            user.put("ref", "https://localhost:9853/scim2/Users/" + userId);
+        } else {
+            user.put("ref", "https://localhost:9853/t/" + tenantDomain + "/scim2/Users/" + userId);
+        }
 
         return user;
     }
