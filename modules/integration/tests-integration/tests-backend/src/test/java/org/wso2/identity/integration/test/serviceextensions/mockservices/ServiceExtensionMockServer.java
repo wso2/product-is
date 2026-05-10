1d60c7f06ba1664ebd46c30e8e2a138e5df99211
UdeshAthukorala <udeshathukorala@gmail.com>
2026-05-10 11:05:26 +0530

    Add integration tests for OAuth 2.0 password grant based authentication for actions

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/serviceextensions/mockservices/ServiceExtensionMockServer.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/identity/integration/test/serviceextensions/mockservices/ServiceExtensionMockServer.java
@@ -26,6 +26,7 @@ import org.apache.commons.lang.StringUtils;
 import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.containing;
 import static com.github.tomakehurst.wiremock.client.WireMock.matching;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
@@ -87,5 +88,56 @@ public class ServiceExtensionMockServer {
 
     public void resetRequests() {
         wireMockServer.resetRequests();
+    }
+
+    /**
+     * Stub a token endpoint that returns a Bearer access token only when the request has the
+     * expected Basic auth header and a form body containing the password grant fields.
+     *
+     * @param url             URL of the token endpoint (path).
+     * @param basicAuthHeader Expected Authorization header value (regex-matched).
+     * @param accessToken     Access token to return on a successful match.
+     * @param username        Expected username form value.
+     * @param password        Expected password form value.
+     */
+    public void setupTokenEndpointStub(String url, String basicAuthHeader, String accessToken,
+                                       String username, String password) {
+
+        String responseBody = "{\"access_token\":\"" + accessToken +
+                "\",\"token_type\":\"Bearer\",\"expires_in\":3600}";
+
+        wireMockServer.stubFor(post(urlEqualTo(url))
+                .withHeader("Authorization", matching(basicAuthHeader))
+                .withRequestBody(containing("grant_type=password"))
+                .withRequestBody(containing("username=" + username))
+                .withRequestBody(containing("password=" + password))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withHeader("Connection", "Close")
+                        .withBody(responseBody)));
+    }
+
+    /**
+     * Stub a token endpoint that responds with the given status code and body for any POST.
+     * Used in failure tests to simulate an unhealthy IdP.
+     */
+    public void setupTokenEndpointStubWithError(String url, int statusCode, String responseBody) {
+
+        wireMockServer.stubFor(post(urlEqualTo(url))
+                .willReturn(aResponse()
+                        .withStatus(statusCode)
+                        .withHeader("Content-Type", "application/json")
+                        .withHeader("Connection", "Close")
+                        .withBody(responseBody)));
+    }
+
+    /**
+     * Count the number of POST requests received at the given URL.
+     */
+    public int getReceivedRequestCount(String url) {
+
+        List<LoggedRequest> requestList = wireMockServer.findAll(postRequestedFor(urlEqualTo(url)));
+        return requestList == null ? 0 : requestList.size();
     }
 }
